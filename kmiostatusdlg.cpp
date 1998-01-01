164c8c1cdcc11327064c3246712536cce48cfc7f
Stefan Taferner <taferner@kde.org>
1998-01-01 18:11:15 +0000

    first draft of a I/O status dialog for sending / receiving mails
    svn path=/trunk/kdenetwork/kmail/; revision=3769

---
+++ kmiostatusdlg.cpp
@@ -0,0 +1,62 @@
+// kmiostatusdlg.cpp
+// Author: Stefan Taferner <taferner@kde.org>
+// This code is under GPL
+
+#include "kmiostatusdlg.h"
+
+#include <qlabel.h>
+#include <qpushbt.h>
+#include <klocale.h>
+#include <kapp.h>
+
+//-----------------------------------------------------------------------------
+KMIOStatusDlg::KMIOStatusDlg(const char* aCap):
+  KMIOStatusDlgInherited(NULL, aCap, TRUE)
+{
+  int y, h, w;
+
+  initMetaObject();
+
+  mLblTask = new QLabel("M", this);
+  mLblTask->adjustSize();
+  h = mLblTask->size().height();
+  w = mLblTask->size().width() * 30;
+  y = 5+h;
+  mLblTask->setGeometry(10, y, w, h);
+  y += y+2;
+
+  mLblStatus = new QLabel(this);
+  mLblStatus->setGeometry(10, y, w, h);
+  y += y+2;
+
+  mBtnAbort = new QPushButton(klocale->translate("&Abort"), this);
+  mBtnAbort->adjustSize();
+  mBtnAbort->move(10, y);
+  connect(mBtnAbort,SIGNAL(clicked()),this,SLOT(reject()));
+}
+
+
+//-----------------------------------------------------------------------------
+void KMIOStatusDlg::done(void)
+{
+  accept();
+}
+
+
+//-----------------------------------------------------------------------------
+void KMIOStatusDlg::setTask(const QString aMsg)
+{
+  mLblTask->setText(aMsg);
+}
+
+
+//-----------------------------------------------------------------------------
+void KMIOStatusDlg::setStatus(const QString aMsg)
+{
+  mLblStatus->setText(aMsg);
+}
+
+
+//-----------------------------------------------------------------------------
+#include "kmiostatusdlg.moc"
+
