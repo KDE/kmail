069e2b915226c49f42fd935c0962f68293bd21c3
Juraj Bednar <bednar@isternet.sk>
1998-11-25 12:51:20 +0000

    Added support for adding PGP 2.6.x keys. It should be now fairly easy to implement ,,Add PGP key'' function, since there should be no low-level programming (functions which interact with pgpk (5.x) and pgp (2.6.x) are now written).
    svn path=/trunk/kdenetwork/kmail/; revision=12929

--- kpgpbase.cpp
+++ kpgpbase.cpp
@@ -781,19 +781,21 @@ QString KpgpBase5::getAsciiPublicKey(QString _person) {
   QString toexec;
   toexec.sprintf("pgpk -xa \"%s\"", _person.data());
 
-  //debug("running %s", toexec.data());
   status = run(toexec.data());
   if(status == RUN_ERR) return 0;
-  //debug("okay");
 
   return output;
 }
 
 QString KpgpBase2::getAsciiPublicKey(QString _person) {
 
-  // FIX ME, HOW DO I EXTRACT PGP PUBLIC KEY ?
+  QString toexec;
+  toexec.sprintf("pgp -kxaf \"%s\"", _person.data());
 
-  return NULL;
+  status = run(toexec.data());
+  if(status == RUN_ERR) return 0;
+
+  return output;
 }
 
 int
