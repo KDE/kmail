24a88af9fef6b112dc11712d704e68a7e7b889ba
Don Sanders <don@sanders.org>
2000-07-17 18:31:40 +0000

    Try to fix non-reproduceable crash that occurs sometimes when deleting many messages consecutively.
    svn path=/trunk/kdenetwork/kmail/; revision=57575

--- kmheaders.cpp
+++ kmheaders.cpp
@@ -858,7 +858,7 @@ void KMHeaders::deleteMsg (int msgId)
     // We are in the trash folder -> really delete messages
     moveMsgToFolder(NULL, msgId);
   }
-  triggerUpdate();
+  //  triggerUpdate();
 }
 
 
