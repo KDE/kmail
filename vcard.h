4a3ab83a54fbb9b607e5fd8c538a58ed8134b044
George Staikos <staikos@kde.org>
2000-09-06 05:46:27 +0000

    more licence fixups
    svn path=/trunk/kdenetwork/kmail/; revision=62853

--- vcard.h
+++ vcard.h
@@ -1,19 +1,24 @@
+/**
+ * vcard.h
+ *
+ * Copyright (c) 2000 George Staikos <staikos@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 #ifndef _KDE_VCARD_H
 #define _KDE_VCARD_H
-
-//
-// VCard class to handle creation and parsing of Netscape
-// vcards as per RFC 2426 (and possibly more)
-//
-// Copyright 2000, George Staikos <staikos@kde.org>
-//
-// Released under GPL.
-//
-// Also freely useable under the QPL.
-//
-// Requires QT 2.1+
-//
-
 
 // required
 #define VCARD_BEGIN          "begin:vcard"
