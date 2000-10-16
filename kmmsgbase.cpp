77c738508059e89f1c4e206b78c5e15ac7acb3f8
Jacek Stolarczyk <jacek@mer.chemia.polsl.gliwice.pl>
2000-10-16 05:12:06 +0000

    Fix for truncating headers in non-latin encoding. It's a conversion from char* to QCString to avoid latin1() call. It should not change any behaviour for latin1 users. Jacek Stolarczyk. Has benn posted for testing on kmail@kde.org for more than a week. No objections there.
    svn path=/trunk/kdenetwork/kmail/; revision=67938

--- kmmsgbase.cpp
+++ kmmsgbase.cpp
@@ -368,67 +368,66 @@ const char especials[17] = "()<>@,;:\"/[]?.= ";
 const QString KMMsgBase::encodeRFC2047String(const QString& _str)
 {
   if (_str.isEmpty()) return _str;
-  char *latin = (char *)calloc(1, _str.length() + 1);
-  strcpy(latin, _str.latin1());
-  char *latinStart = latin, *l, *start, *stop;
+  QCString latin = _str.local8Bit();
+  int cr, start, stop, pos = 0;
+  int latinLen = latin.length();
   char hexcode;
   int numQuotes, i;
   QString result = QString();
-  while (*latin)
+  while (pos < latinLen)
   {
-    l = latin;
-    start = latin;
-    while (*l)
+    cr = pos;
+    start = pos;
+    while (cr < latinLen)
     {
-      if (*l == 32) start = l + 1;
-      if (*l < 0) break;
-      l++;
+      if (latin[cr] == 32) start = cr + 1;
+      if (latin[cr] < 0) break;
+      cr++;
     }
-    if (*l)
+    if (cr < latinLen)
     {
       numQuotes = 1;
-      while (*l)
+      while (cr < latinLen)
       {
         /* The encoded word must be limited to 75 character */
-        for (i = 0; i < 16; i++) if (*l == especials[i]) numQuotes++;
-        if (*l < 0) numQuotes++;
+        for (i = 0; i < 16; i++) if (latin[cr] == especials[i]) numQuotes++;
+        if (latin[cr] < 0) numQuotes++;
         /* Stop after 58 = 75 - 17 characters or at "<user@host..." */
-        if (l - start + 2 * numQuotes >= 58 || *l == 60) break;
-        l++;
+        if (cr - start + 2 * numQuotes >= 58 || latin[cr] == 60) break;
+        cr++;
       }
-      if (*l)
+      if (cr < latinLen)
       {
-        stop = l - 1;
-        while (stop >= start && *stop != 32) stop--;
-        if (stop <= start) stop = l;
-      } else stop = l;
-      while (latin < start) { result += *latin; latin++; }
+        stop = cr - 1;
+        while (stop >= start && latin[stop] != 32) stop--;
+        if (stop <= start) stop = cr;
+      } else stop = cr;
+      while (pos < start) { result += latin[pos]; pos++; }
       result += QString("=?iso-8859-1?q?");
-      while (latin < stop)
+      while (pos < stop)
       {
         numQuotes = 0;
-        for (i = 0; i < 16; i++) if (*latin == especials[i]) numQuotes = 1;
-        if (*latin < 0) numQuotes = 1;
+        for (i = 0; i < 16; i++) if (latin[pos] == especials[i]) numQuotes = 1;
+        if (latin[pos] < 0) numQuotes = 1;
         if (numQuotes)
         {
           result += "=";
-          hexcode = ((*latin & 0xF0) >> 4) + 48;
+          hexcode = ((latin[pos] & 0xF0) >> 4) + 48;
           if (hexcode >= 58) hexcode += 7;
           result += hexcode;
-          hexcode = (*latin & 0x0F) + 48;
+          hexcode = (latin[pos] & 0x0F) + 48;
           if (hexcode >= 58) hexcode += 7;
           result += hexcode;
         } else {
-          result += *latin;
+          result += latin[pos];
         }
-        latin++;
+        pos++;
       }
       result += "?=";
     } else {
-      while (*latin) { result += *latin; latin++; }
+      while (pos < latinLen) { result += latin[pos]; pos++; }
     }
   }
-  free(latinStart);
   return result;
 }
 
