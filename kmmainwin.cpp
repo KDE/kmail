1c0ce48dab9d3031bbb99814e985fe8d8791788b
Daniel Naber <daniel.naber@t-online.de>
2001-05-07 23:51:17 +0000

    Show a warning if someone wants to activate HTML. If you don't want the warning, just check the useful "don't ask again" box.
    svn path=/trunk/kdenetwork/kmail/; revision=95745

--- kmmainwin.cpp
+++ kmmainwin.cpp
@@ -850,6 +850,19 @@ void KMMainWin::slotCompactAll()
 //-----------------------------------------------------------------------------
 void KMMainWin::slotOverrideHtml()
 {
+  if( !mFolderHtmlPref ) {
+    int result = KMessageBox::warningContinueCancel( this, 
+      // the warning text is taken from configuredialog.cpp:
+      i18n( "Use of HTML in mail will make you more vulnerable to\n"
+        "\"spam\" and may increase the likelihood that your system will be\n"
+        "compromised by other present and anticipated security exploits." ),
+      i18n( "Security Warning" ),
+      i18n( "Continue" ),
+      "OverrideHtmlWarning", false);
+    if( result == KMessageBox::Cancel ) {
+      return;
+    }
+  }
   mFolderHtmlPref = !mFolderHtmlPref;
   mMsgView->setHtmlOverride(mFolderHtmlPref);
   mMsgView->setMsg( mMsgView->msg(), TRUE );
