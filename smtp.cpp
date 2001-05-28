4a5421d8a9bf66450f28024688b68d7465b75058
Igor Janssen <alaves17@gmail.com>
2001-05-28 16:00:49 +0000

    bug...cutting 8th bit when sending message('??????' for non-latin1 charsets) ... bug at Qt, QTextStream::Locale effect as QTextStream::Latin1 and Latin1 as Locale
    svn path=/trunk/kdenetwork/kmail/; revision=99186

--- smtp.cpp
+++ smtp.cpp
@@ -41,6 +41,7 @@ Smtp::Smtp( const QString &from, const QStringList &to,
 
     mSocket->connectToHost( server, port );
     t = new QTextStream( mSocket );
+    t->setEncoding(QTextCoding::Latin1);
 }
 
 
