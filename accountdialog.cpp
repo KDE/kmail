3dfeff40dc51aac15a35685a6cd261ac72a0ba99
Zack Rusin <zack@kde.org>
2002-04-03 05:27:38 +0000

    Adding the QToolTip and QWhatsThis to misunderstood Login label
    svn path=/trunk/kdenetwork/kmail/; revision=146763

--- accountdialog.cpp
+++ accountdialog.cpp
@@ -27,6 +27,8 @@
 #include <qvalidator.h>
 #include <qlabel.h>
 #include <qpushbutton.h>
+#include <qwhatsthis.h>
+#include <qtooltip.h>
 
 #include <kfiledialog.h>
 #include <klocale.h>
@@ -477,6 +479,8 @@ void AccountDialog::makePopAccountPage()
   grid->addWidget( mPop.nameEdit, 0, 1 );
 
   label = new QLabel( i18n("&Login:"), page1 );
+  QToolTip::add( label, i18n("User name used for authentication purposes") );
+  QWhatsThis::add( label, i18n("Your Internet Service Provider gave you a <em>user name</em> which is used to authenticate you with their servers. It usually is the first part of your email address ( the part before <em>@</em> ).") );
   grid->addWidget( label, 1, 0 );
   mPop.loginEdit = new QLineEdit( page1 );
   label->setBuddy( mPop.loginEdit );
@@ -637,6 +641,8 @@ void AccountDialog::makeImapAccountPage()
   grid->addWidget( mImap.nameEdit, 0, 1 );
 
   label = new QLabel( i18n("&Login:"), page1 );
+  QToolTip::add( label, i18n("User name used for authentication purposes") );
+  QWhatsThis::add( label, i18n("Your Internet Service Provider gave you a <em>user name</em> which is used to authenticate you with their servers. It usually is the first part of your email address ( the part before <em>@</em> ).") );
   grid->addWidget( label, 1, 0 );
   mImap.loginEdit = new QLineEdit( page1 );
   label->setBuddy( mImap.loginEdit );
