1227a29b823ae54d740efca85df0536001ab9700
Karl-Heinz Zimmer <khz@klaralvdalens-datakonsult.se>
2002-04-24 15:54:36 +0000

    addition to Bugfix #41471: We now show some comment before displaying the actual HTML source code for HTML-only mails.
    svn path=/trunk/kdenetwork/kmail/; revision=151672

--- kmreaderwin.cpp
+++ kmreaderwin.cpp
@@ -223,8 +223,18 @@ kdDebug(5006) << "html" << endl;
               }
               // ---Sven's strip </BODY> and </HTML> from end of attachment end-
 
-              writeHTMLStr(mCodec->toUnicode(
-                htmlMail() ? cstr : KMMessage::html2source( cstr ) ) );
+              if( !htmlMail() ) {
+                writeHTMLStr(     "<table border=1 cellpadding=20><tr><td align=center>");
+                writeHTMLStr(i18n("Data is shown in <b>HTML source mode</b> because<br>"
+                                  "mail contains <i>no plain text version</i> of it."));
+                writeHTMLStr(     "<br>&nbsp;<br><b>");
+                writeHTMLStr(i18n("This is the default - and save - behavior of KMail."));
+                writeHTMLStr(     "</b><br>&nbsp;<br><font size=-1>");
+                writeHTMLStr(i18n("To enable HTML rendering at your own risk<br>"
+                                  "use respective &quot;Folder&quot; menu option."));
+                writeHTMLStr(     " </font></td></tr></table>&nbsp;<br>&nbsp;<br>");
+              }
+              writeHTMLStr(mCodec->toUnicode( htmlMail() ? cstr : KMMessage::html2source( cstr )));
               bDone = true;
             }
             break;
