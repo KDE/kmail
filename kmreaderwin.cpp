284992c4195b78bd1b67b0f4a23080526f4678ac
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2002-05-01 14:14:16 +0000

    No space before punctuation please
    svn path=/trunk/kdenetwork/kmail/; revision=153160

--- kmreaderwin.cpp
+++ kmreaderwin.cpp
@@ -1739,7 +1739,7 @@ bool KMReaderWin::writeOpaqueOrMultipartSignedData( partNode* data, partNode& si
         bIsOpaqueSigned = true;
       } else {
         txt = "<hr><b><h2>";
-        txt.append( i18n( "The crypto engine returned no cleartext data !" ) );
+        txt.append( i18n( "The crypto engine returned no cleartext data!" ) );
         txt.append( "</h2></b>" );
         txt.append( "<br>&nbsp;<br>" );
         txt.append( i18n( "Status: " ).local8Bit() );
