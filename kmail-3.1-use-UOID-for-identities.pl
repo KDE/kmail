e7aff7089211ce0042b0e55992597b3283a79fbb
Marc Mutz <mutz@kde.org>
2002-05-24 20:32:34 +0000

    scipt to create UOIDs for identities and make all of kmailrc use them instead of the identity anme to refer to a particular identity.
    Not yet included in build and not installed.
    
    svn path=/trunk/kdenetwork/kmail/; revision=157459

---
+++ kmail-3.1-use-UOID-for-identities.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+# this script goes through all the config keys that deal with
+# identities and replaces identies referenced by name to be referenced
+# by UOIDs. To this end, adds uoid keys to the identity groups.
+
+# read the whole config file:
+while ( <> ) {
+    chomp;
+    next if ( /^$/ ); # skip empty lines
+    next if ( /^\#/ ); # skip comments
+    if ( /^\[/ ) { # group begin
+	$currentGroup = $_;
+	next;
+    } elsif ( defined($currentGroup) ) { # normal entry
+	local ($key,$value) = split /=/;
+	$configFile{$currentGroup}{$key}=$value;
+    }
+}
+
+# filter out identity groups:
+@identityGroups = grep { /^\[Identity \#\d+\]/ } keys %configFile;
+
+# create UOIDs for each identity:
+my %nameToUOID;
+foreach $identityGroup (@identityGroups) {
+    local $uoid = int(rand 0x7fFFffFF);
+    local $name = $configFile{$identityGroup}{'Identity'};
+    $nameToUOID{$name} = $uoid;
+    # create the uoid entries of [Identity #n] groups:
+    print "${identityGroup}\nuoid=$uoid\n";
+}
+
+# change the default identity value:
+print "# DELETE [General]Default Identity\n[General]\nDefault Identity="
+    . $nameToUOID{$configFile{'[General]'}{'Default Identity'}} . "\n";
+
+# [Composer]previous-identity
+print "# DELETE [Composer]previous-identity\n[Composer]\nprevious-identity="
+    . $nameToUOID{$configFile{'[Composer]'}{'previous-identity'}} . "\n";
+
+# Now, go through all [Folder-*] groups and replace the Identity value
+# with the UOID. Also, move MailingListIdentity entries to Identity entries:
+@folderGroups = grep { /^\[Folder-.*\]/ } keys %configFile;
+
+foreach $folderGroup ( @folderGroups ) {
+    local $identity = "";
+    # delete the (MailingList)Identity keys:
+    print "# DELETE ${folderGroup}MailingListIdentity\n";
+    print "# DELETE ${folderGroup}Identity\n";
+    # extract the identity name:
+    if ( exists ($configFile{$folderGroup}{'Identity'}) ) {
+	$identity = $configFile{$folderGroup}{'Identity'};
+    }
+    if ( $identity eq ""
+	 and exists($configFile{$folderGroup}{'MailingListIdentity'}) ) {
+	$identity = $configFile{$folderGroup}{'MailingListIdentity'};
+    }
+    # write the new Identity=<uoid> key if identity is not empty or default:
+    if ( $identity ne "" and $identity ne "Default" ) {
+	print "$folderGroup\nIdentity=" . $nameToUOID{$identity} . "\n";
+    }
+}
+
+# Now, go through all [Filter #n] groups and change arguments to the
+# 'set identity' filter action to use UOIDs:
+
+@filterGroups = grep { /^\[Filter \#\d+\]/ } keys %configFile;
+
+foreach $filterGroup (@filterGroups) {
+    local $numActions = +$configFile{$filterGroup}{'actions'};
+    # go through all actions in search for "set identity":
+    for ( $i = 0 ; $i < $numActions ; ++$i ) {
+	local $actionName = "action-name-$i";
+	local $actionArgs = "action-args-$i";
+	if ( $configFile{$filterGroup}{$actionName} eq "set identity" ) {
+	    # found one: replace it's argument with the UOID:
+	    print "# DELETE $filterGroup$actionArgs\n$filterGroup\n$actionArgs="
+		. $nameToUOID{$configFile{$filterGroup}{$actionArgs}} . "\n";
+	}
+    }
+}
+
