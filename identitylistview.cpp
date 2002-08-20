d7a66019fab5ddb7066e301fc71aa17498a9296f
Marc Mutz <mutz@kde.org>
2002-08-20 21:20:49 +0000

    Adding IdentityListView, a listview for KMidentities, to be used in ConfigureDialog::IdentityPage
    svn path=/trunk/kdenetwork/kmail/; revision=172942

---
+++ identitylistview.cpp
@@ -0,0 +1,102 @@
+/*  -*- c++ -*-
+    identitylistview.cpp
+
+    KMail, the KDE mail client.
+    Copyright (c) 2002 the KMail authors.
+    See file AUTHORS for details
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License,
+    version 2.0, as published by the Free Software Foundation.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, US
+*/
+
+#include "identitylistview.h"
+
+#include "kmidentity.h"
+#include "identitydrag.h"
+#include "identitymanager.h"
+#include "kmkernel.h"
+
+#include <klocale.h> // i18n
+#include <kiconloader.h> // SmallIcon
+
+#include <cassert>
+
+namespace KMail {
+
+  //
+  //
+  // IdentityListViewItem
+  //
+  //
+
+  IdentityListViewItem::IdentityListViewItem( IdentityListView * parent, const KMIdentity & ident )
+    : KListViewItem( parent ), mUOID( ident.uoid() ) {
+    init( ident );
+  }
+
+  IdentityListViewItem::IdentityListViewItem( IdentityListView * parent, QListViewItem * after, const KMIdentity & ident )
+    : KListViewItem( parent, after ), mUOID( ident.uoid() ) {
+    init( ident );
+  }
+
+  KMIdentity & IdentityListViewItem::identity() const {
+    IdentityManager * im = kernel->identityManager();
+    assert( im );
+    return im->identityForUoid( uoid() );
+  }
+
+  void IdentityListViewItem::setIdentity( const KMIdentity & ident ) {
+    mUOID = ident.uoid();
+    init( ident );
+  }
+
+  void IdentityListViewItem::init( const KMIdentity & ident ) {
+    setText( 0, ident.identityName() );
+    setText( 1, ident.fullEmailAddr() );
+  }
+
+  //
+  //
+  // IdentityListView
+  //
+  //
+
+  IdentityListView::IdentityListView( QWidget * parent, const char * name )
+    : KListView( parent, name )
+  {
+    setDragEnabled( true );
+    setAcceptDrops( true );
+    setDropVisualizer( true );
+    addColumn( i18n("Identity Name") );
+    addColumn( i18n("EMail Address") );
+    setRootIsDecorated( false );
+    setRenameable( 0 );
+    setItemsRenameable( true );
+    // setShowToolTips( true );
+    setItemsMovable( true );
+    setAllColumnsShowFocus( true );
+    setSorting( -1 ); // disabled
+    setSelectionModeExt( Single ); // ### Extended would be nicer...
+  }
+
+  bool IdentityListView::acceptDrag( QDropEvent * e ) const {
+    return e->source() == viewport() || IdentityDrag::canDecode( e );
+  }
+
+  QDragObject * IdentityListView::dragObject() {
+    IdentityListViewItem * item = dynamic_cast<IdentityListViewItem*>( currentItem() );
+    if ( !item ) return 0;
+
+    IdentityDrag * drag = new IdentityDrag( item->identity(), viewport() );
+    drag->setPixmap( SmallIcon("identity") );
+    return drag;
+  }
+
+}; // namespace KMail
+
+
+#include "identitylistview.moc"
---
+++ identitylistview.h
@@ -0,0 +1,66 @@
+/*  -*- c++ -*-
+    identitylist.h
+
+    KMail, the KDE mail client.
+    Copyright (c) 2002 the KMail authors.
+    See file AUTHORS for details
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License,
+    version 2.0, as published by the Free Software Foundation.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, US
+*/
+
+#ifndef __KMAIL_IDENTITYLIST_H__
+#define __KMAIL_IDENTITYLIST_H__
+
+#include <klistview.h>
+
+class KMIdentity;
+class QDropEvent;
+class QDragEvent;
+
+namespace KMail {
+
+  class IdentityListView;
+
+  /** @short A @ref QListViewItem for use in @ref IdentityListView
+      @author Marc Mutz <mutz@kde.org>
+  **/
+  class IdentityListViewItem : public KListViewItem {
+  public:
+    IdentityListViewItem( IdentityListView * parent,
+			  const KMIdentity & ident );
+    IdentityListViewItem( IdentityListView * parent, QListViewItem * after,
+			  const KMIdentity & ident );
+
+    uint uoid() const { return mUOID; }
+    KMIdentity & identity() const;
+    virtual void setIdentity( const KMIdentity & ident );
+  private:
+    void init( const KMIdentity & ident );
+
+  protected:
+    uint mUOID;
+  };
+
+  /** @short A listview for @ref KMIdentity
+      @author Marc Mutz <mutz@kde.org>
+  **/
+  class IdentityListView : public KListView {
+    Q_OBJECT
+  public:
+    IdentityListView( QWidget * parent=0, const char * name=0 );
+    virtual ~IdentityListView();
+
+  protected:
+    bool acceptDrag( QDropEvent * ) const;
+    QDragObject * dragObject();
+  };
+
+
+}; // namespace KMail
+
+#endif // __KMAIL_IDENTITYLISTVIEW_H__
