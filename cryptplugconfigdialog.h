1d5ebd5e1340cbdbe2dc96ffca92f758a9384121
Marc Mutz <mutz@kde.org>
2002-09-05 20:54:37 +0000

    CCMAIL: jan@intevation.de Removing traces of "aegypten" from the source
    svn path=/trunk/kdenetwork/kmail/; revision=175931

---
+++ cryptplugconfigdialog.h
@@ -0,0 +1,38 @@
+// -*- c++ -*-
+#ifndef _CRYPT_PLUG_CONFIG_DIALOG_H_
+#define _CRYPT_PLUG_CONFIG_DIALOG_H_
+
+#include <kdialogbase.h>
+
+class CryptPlugWrapper;
+
+class SignatureConfigurationDialogImpl;
+class EncryptionConfigurationDialogImpl;
+class DirectoryServicesConfigurationDialogImpl;
+
+class CryptPlugConfigDialog : public KDialogBase {
+  Q_OBJECT
+public:
+  CryptPlugConfigDialog( CryptPlugWrapper * wrapper, int plugno,
+		      const QString & caption,
+		      QWidget * parent=0, const char * name=0,
+		      bool modal=true );
+  virtual ~CryptPlugConfigDialog() {}
+
+protected slots:
+  void slotOk();
+  void slotStartCertManager();
+
+protected:
+  void setPluginInformation();
+
+protected:
+  SignatureConfigurationDialogImpl * mSignatureTab;
+  EncryptionConfigurationDialogImpl * mEncryptionTab;
+  DirectoryServicesConfigurationDialogImpl * mDirServiceTab;
+
+  CryptPlugWrapper * mWrapper;
+  int mPluginNumber;
+};
+
+#endif // _CRYPT_PLUG_CONFIG_DIALOG_H_
