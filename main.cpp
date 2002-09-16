c5ef5df549f8ae6b878ff005de9866ae6e319430
Don Sanders <don@sanders.org>
2002-09-16 06:55:00 +0000

    Restore my position as a maintainer. Because:
    My kmcommands patch and other work has been committed to the HEAD branch.
    
    Because of the work I'm doing in the make_it_cool branch.
    
    Because my title was changed in my absence, without consulting me and
    definitely without my permission.
    
    And most importantly because code changes of mine that are both
    technically good and very much needed are being rejected because
    I wasn't listed in the about box as a maintainer anymore.
    
    I think this whole about box thing is very silly but I guess it's
    important for me to take credit for my work.
    
    svn path=/trunk/kdenetwork/kmail/; revision=178153

--- main.cpp
+++ main.cpp
@@ -234,7 +234,7 @@ int main(int argc, char *argv[])
 		   0,
 		   "http://kmail.kde.org");
   about.addAuthor( "Michael H\303\244ckel", I18N_NOOP("Current maintainer"), "haeckel@kde.org" );
-  about.addAuthor( "Don Sanders", I18N_NOOP("Core developer and former maintainer"), "sanders@kde.org" );
+  about.addAuthor( "Don Sanders", I18N_NOOP("Core developer and maintainer"), "sanders@kde.org" );
   about.addAuthor( "Stefan Taferner ", I18N_NOOP("Original author"), "taferner@kde.org" );
   about.addAuthor( "Ingo Kl\303\266cker", I18N_NOOP("Encryption"), "kloecker@kde.de" );
   about.addAuthor( "Marc Mutz", I18N_NOOP("Core developer"), "mutz@kde.org" );
@@ -319,7 +319,7 @@ int main(int argc, char *argv[])
   config.writeEntry("pid", getpid());
   config.writeEntry("hostname", hostName);
   config.sync();
-  
+
   kapp->dcopClient()->suspend(); // Don't handle DCOP requests yet
 
   //local, do the init
