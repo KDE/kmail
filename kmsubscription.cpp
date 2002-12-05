a412017ce7fb80e3d2166514bf6429ff11e3bb50
Carsten Burghardt <burghardt@kde.org>
2002-12-05 18:22:34 +0000

    Add the kmail-specific subscription classes. They're not activated yet.
    svn path=/trunk/kdenetwork/kmail/; revision=192257

---
+++ kmsubscription.cpp
@@ -0,0 +1,171 @@
+/*
+    kmsubscription.cpp
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+*/
+
+#include "kmsubscription.h"
+#include "kmmessage.h"
+
+#include <klocale.h>
+#include <kdebug.h>
+
+#include <qpushbutton.h>
+
+KMSubscription::KMSubscription( QWidget *parent, const QString &caption,
+    KAccount * acct )
+  : KSubscription( parent, caption, acct, User1, QString::null, false )
+{
+  // hide unneeded checkboxes
+  hideTreeCheckbox();
+  hideNewOnlyCheckbox();
+
+  // connect to folderlisting
+  KMAcctImap* ai = static_cast<KMAcctImap*>(acct);
+  connect(ai, SIGNAL(receivedFolders(QStringList,QStringList,
+          QStringList,KMAcctImap::jobData)),
+      this, SLOT(slotListDirectory(QStringList,QStringList,
+          QStringList,KMAcctImap::jobData)));
+
+  // ok-button
+  connect(this, SIGNAL(okClicked()), SLOT(slotSave()));
+  
+  // reload-list button
+  connect(this, SIGNAL(user1Clicked()), SLOT(slotLoadFolders()));
+
+  // get the folders
+  slotLoadFolders();
+}
+
+//------------------------------------------------------------------------------
+void KMSubscription::slotListDirectory( QStringList mSubfolderNames, 
+                                        QStringList mSubfolderPaths, 
+                                        QStringList mSubfolderMimeTypes,
+                                        KMAcctImap::jobData jobData )
+{
+  if (mSubfolderPaths.size() <= 0) return;
+  bool onlySubscribed = jobData.onlySubscribed;
+  GroupItem *parent = 0;
+  mLoading = true;
+
+  if (!onlySubscribed)
+  {
+    /* we need to find our root-item
+       remove the name (and the separator) from the path */
+    int start = mSubfolderPaths[0].length() - (mSubfolderNames[0].length()+2);
+    int length = mSubfolderNames[0].length()+1;
+    if (start < 0) start = 0;
+    QString compare = mSubfolderPaths[0];
+    compare.remove(start, length);
+
+    QListViewItemIterator it( groupView );
+    GroupItem *tmp;
+    while ( it.current() != 0 ) 
+    {
+      // compare it with each item to find the current root
+      tmp = static_cast<GroupItem*>(it.current());
+      if (tmp->info().path == compare) {
+        parent = tmp;
+        break;
+      }
+      ++it;
+    }
+  }
+  GroupItem *item = 0;
+  QString text;
+  for (uint i = 0; i < mSubfolderNames.count(); i++)
+  {
+    if (!onlySubscribed)
+    {
+      bool create = true;
+      if (parent)
+      {
+        for ( QListViewItem *it = parent->firstChild() ;
+            it ; it = it->nextSibling() ) 
+        {
+          // check if this item already exists in this hierarchy
+          item = static_cast<GroupItem*>(it);
+          if (item->info().path == mSubfolderPaths[i])
+            create = false;
+        }
+      }
+      if (create) 
+      {
+        KGroupInfo info(mSubfolderNames[i]);
+        if (mSubfolderNames[i].upper() == "INBOX") 
+          info.name = i18n("inbox");
+        info.subscribed = false;
+        info.path = mSubfolderPaths[i];
+        // create a new checkable item
+        if (parent)
+          item = new GroupItem(parent, info, this, true); 
+        else
+          item = new GroupItem(folderTree(), info, this, true); 
+      }
+      if (item) 
+      {
+        // reset
+        item->setOn(false);
+      }
+    } else {
+      // find the item
+      QListViewItemIterator it( groupView );
+      while ( it.current() != 0 ) 
+      {
+        item = static_cast<GroupItem*>(it.current());
+        if (item->info().path == mSubfolderPaths[i]) 
+        {
+          // subscribed
+          item->setOn(true);
+        }
+        ++it;
+      }
+    }
+    if (mSubfolderMimeTypes[i] == "message/directory" ||
+        mSubfolderMimeTypes[i] == "inode/directory")
+    {
+      // descend
+      static_cast<KMAcctImap*>(mAcct)->listDirectory(mSubfolderPaths[i], 
+          onlySubscribed);
+    }
+  }
+  slotLoadingComplete();
+}
+
+//------------------------------------------------------------------------------
+void KMSubscription::slotSave()
+{
+  // subscribe
+  QListViewItemIterator it(subView);
+  for ( ; it.current(); ++it)
+  {
+    static_cast<KMAcctImap*>(account())->changeSubscription(true, 
+        static_cast<GroupItem*>(it.current())->info().path);
+  }
+
+  // unsubscribe
+  QListViewItemIterator it2(unsubView);
+  for ( ; it2.current(); ++it2)
+  {
+    static_cast<KMAcctImap*>(account())->changeSubscription(false, 
+        static_cast<GroupItem*>(it2.current())->info().path);
+  }
+}
+
+//------------------------------------------------------------------------------
+void KMSubscription::slotLoadFolders()
+{
+  folderTree()->clear();
+  KMAcctImap* ai = static_cast<KMAcctImap*>(account());
+  // get folders
+  ai->listDirectory(ai->prefix(), false);
+  ai->listDirectory(ai->prefix(), true);
+}
+
+#include "kmsubscription.moc"
---
+++ kmsubscription.h
@@ -0,0 +1,54 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2002 Carsten Burghardt <burghardt@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+#ifndef __KMSUBSCRIPTION
+#define __KMSUBSCRIPTION
+
+#include <ksubscription.h>
+
+#include "kmacctimap.h"
+
+class KMMessage;
+
+class KMSubscription : public KSubscription
+{
+  Q_OBJECT
+
+  public:
+    KMSubscription( QWidget *parent, const QString &caption, KAccount* acct );
+
+  public slots:
+    /**
+     * get the listing from the imap-server
+     */ 
+    void slotListDirectory(QStringList, QStringList,
+        QStringList, KMAcctImap::jobData);
+
+    /** 
+     * called by Ok-button, saves the changes
+     */ 
+    void slotSave();
+
+  protected slots:
+    /**
+     * Loads the folders
+     */ 
+    void slotLoadFolders();
+
+};
+
+#endif
