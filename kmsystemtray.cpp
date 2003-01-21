2f6e4aa9b184aae5abab26c02753597410bce30d
Marc Mutz <mutz@kde.org>
2003-01-21 19:29:58 +0000

    Add a space between folder name and (count).
    svn path=/trunk/kdepim/; revision=201783

--- kmsystemtray.cpp
+++ kmsystemtray.cpp
@@ -252,7 +252,7 @@ void KMSystemTray::mousePressEvent(QMouseEvent *e)
       kdDebug(5006) << "Adding folder" << endl;
       if(i > mPopupFolders.size()) mPopupFolders.resize(i * 2);
       mPopupFolders.insert(i, it.key());
-      QString item = prettyName(it.key()) + "(" + QString::number(it.data()) + ")";
+      QString item = prettyName(it.key()) + " (" + QString::number(it.data()) + ")";
       popup->insertItem(item, this, SLOT(selectedAccount(int)), 0, i);
       ++it;
     }
