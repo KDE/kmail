dcc996101968e12f165f58165c1a723560579199
Wilco Greven <greven@kde.org>
2003-02-18 02:31:32 +0000

    Make use of this temporary var a bit more.
    svn path=/trunk/kdepim/; revision=207587

--- kmmsgbase.cpp
+++ kmmsgbase.cpp
@@ -478,7 +478,7 @@ QCString KMMsgBase::encodeRFC2047Quoted(const QCString& aStr, bool base64)
   for (unsigned int i = 0; i < aStr.length(); i++)
   {
     ch = aStr.at(i);
-    if (ch >= 128 || ch == '_' || especials.find(aStr.at(i)) != -1)
+    if (ch >= 128 || ch == '_' || especials.find(ch) != -1)
     {
       result += '=';
       hex = ((ch & 0xF0) >> 4) + 48;
@@ -488,7 +488,7 @@ QCString KMMsgBase::encodeRFC2047Quoted(const QCString& aStr, bool base64)
       if (hex >= 58) hex += 7;
       result += hex;
     } else {
-      result += aStr.at(i);
+      result += ch;
     }
   }
   return result;
