0c976e957f5942f6b621295d49f85a74dfb04ac8
Stephan Kulow <coolo@kde.org>
2003-04-24 07:16:18 +0000

    the bare minimum of documentation
    svn path=/trunk/kdepim/; revision=221803

--- kcursorsaver.h
+++ kcursorsaver.h
@@ -4,23 +4,33 @@
 #include <qcursor.h>
 #include <qapplication.h>
 
+/**
+ * @short sets a cursor and makes sure it's restored on destruction
+ * Create a KCursorSaver object when you want to set the cursor.
+ * As soon as it gets out of scope, it will restore the original
+ * cursor.
+ */
 class KCursorSaver : public Qt
 {
 public:
+    /// constructor taking QCursor shapes
     KCursorSaver(Qt::CursorShape shape) {
         QApplication::setOverrideCursor( QCursor(shape) );
         inited = true;
     }
 
+    /// copy constructor. The right side won't restore the cursor
     KCursorSaver( const KCursorSaver &rhs ) {
         *this = rhs;
     }
 
+    /// restore the cursor
     ~KCursorSaver() {
         if (inited)
             QApplication::restoreOverrideCursor();
     }
 
+    /// call this to explitly restore the cursor
     inline void restoreCursor(void) {
         QApplication::restoreOverrideCursor();
         inited = false;
@@ -36,6 +46,9 @@ private:
     mutable bool inited;
 };
 
+/**
+ * convencience functions
+ */
 namespace KBusyPtr {
     inline KCursorSaver idle() {
         return KCursorSaver(QCursor::ArrowCursor);
