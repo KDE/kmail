f996f5a62ffc9cd0139518f4079435b04ebdec1e
Chris Howells <howells@kde.org>
2003-09-08 12:08:57 +0000

    Language cleanup, as discussed on kde-cvs
    svn path=/trunk/kdepim/; revision=249383

--- kmmainwin.cpp
+++ kmmainwin.cpp
@@ -194,7 +194,7 @@ bool KMMainWin::queryClose() {
 
   if (config->readBoolEntry("warn-before-expire", true)) {
     ret = KMessageBox::warningYesNo(KMainWindow::memberList->first(),
-			 str, i18n("Expire Old Messages?"), i18n("Expire"), i18n("Leave Without"));
+			 str, i18n("Expire Old Messages?"), i18n("Expire"), i18n("Don't Expire"));
     if (ret == KMessageBox::Continue) {
       kernel->setCanExpire(true);
     }
