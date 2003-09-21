1b3f85e7e454c35887a5546ec628ac69c481bc4d
Zack Rusin <zack@kde.org>
2003-09-21 04:39:36 +0000

    Removing license as requested by Marc. This of course leaves that file without a license, sure it's a violation of KDE policies but because radej and dijkema who are listed in the files annotate are no longer with us, there's no way to consult license of that file with them. Now here's a question: does the fact that KMail is GPL mean that this file is GPL or does the fact the this file doesn't have a license mean that KMail doesn't have one?
    svn path=/trunk/kdepim/; revision=252760

--- kmkernel.cpp
+++ kmkernel.cpp
@@ -1,32 +1,4 @@
-/*  -*- mode: C++; c-file-style: "gnu" -*-
- *
- *  This file is part of KMail, the KDE mail client.
- *  Copyright (c) 2003 KMail Developers
- *
- *  KMail is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License, version 2, as
- *  published by the Free Software Foundation.
- *
- *  KMail is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  In addition, as a special exception, the copyright holders give
- *  permission to link the code of this program with any edition of
- *  the Qt library by Trolltech AS, Norway (or with modified versions
- *  of Qt that use the same license as Qt), and distribute linked
- *  combinations including the two.  You must obey the GNU General
- *  Public License in all respects for all of the code used other than
- *  Qt.  If you modify this file, you may extend this exception to
- *  your version of the file, but you are not obligated to do so.  If
- *  you do not wish to do so, delete this exception statement from
- *  your version.
- */
+/*  -*- mode: C++; c-file-style: "gnu" -*- */
 #include "config.h"
 #include "kmkernel.h"
 
@@ -1433,7 +1405,7 @@ KMReaderWin* KMKernel::activeReaderWin()
   }
   if ( startupMode ) // return the first window which will probably get focus shortly
   {
-    KMMainWin *win = static_cast<KMMainWin*>(KMainWindow::memberList->first());  
+    KMMainWin *win = static_cast<KMMainWin*>(KMainWindow::memberList->first());
     if ( win && win->inherits("KMTopLevelWidget") && win->isA("KMMainWin") )
     {
       return win->mainKMWidget()->messageView();
