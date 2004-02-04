c92a229a8437f30808d94b05a209bfa95dae0a41
Ladislav Strojil <ladislav@strojil.cz>
2004-02-04 17:25:57 +0000

    Commiting a patch by Ingo Klöcker: The attached patch fixes the critical bug #71866.
    From http://bugs.kde.org/show_bug.cgi?id=71866#c2:
    "That's a prime example for why it's bad to store the value of an enum
    as a number in a config file. The reason for this bug is a reordering
    of the enum values in the code. This changed the meaning of the value
    which is stored in the config file.
    
    With the patch KMail stores the enum as a string representing the enum
    value. As a consequence all POP filters will be reset to NoAction. But
    there is no other fix possible because it's impossible to find out
    whether the pop filter configuration was saved with KMail 1.6 or with a
    previous version of KMail."
    
    svn path=/trunk/kdepim/; revision=285082

--- kmfilter.cpp
+++ kmfilter.cpp
@@ -147,9 +147,18 @@ void KMFilter::readConfig(KConfig* config)
   // that the pattern is purified.
   mPattern.readConfig(config);
 
-  if (bPopFilter)
+  if (bPopFilter) {
     // get the action description...
-    mAction = (KMPopFilterAction) config->readNumEntry( "action" );
+    QString action = config->readEntry( "action" );
+    if ( action == "down" )
+      mAction = Down;
+    else if ( action == "later" )
+      mAction = Later;
+    else if ( action == "delete" )
+      mAction = Delete;
+    else
+      mAction = NoAction;
+  }
   else {
     QStringList sets = config->readListEntry("apply-on");
     if ( sets.isEmpty() && !config->hasKey("apply-on") ) {
@@ -210,7 +219,19 @@ void KMFilter::writeConfig(KConfig* config) const
   mPattern.writeConfig(config);
 
   if (bPopFilter) {
-    config->writeEntry( "action", mAction );
+    switch ( mAction ) {
+    case Down:
+      config->writeEntry( "action", "down" );
+      break;
+    case Later:
+      config->writeEntry( "action", "later" );
+      break;
+    case Delete:
+      config->writeEntry( "action", "delete" );
+      break;
+    default:
+      config->writeEntry( "action", "" );
+    }
   } else {
     QStringList sets;
     if ( bApplyOnInbound )
