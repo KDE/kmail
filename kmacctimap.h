757f5e43de5e827d007bdd4b05a77536a8458745
David Faure <faure@kde.org>
2004-05-12 10:23:43 +0000

    removed useless forwarder
    svn path=/trunk/kdepim/; revision=310581

--- kmacctimap.h
+++ kmacctimap.h
@@ -72,15 +72,6 @@ public:
   virtual void setImapFolder(KMFolderImap *);
 
   /**
-   * Lists the directory starting from @p path
-   * All parameters (onlySubscribed, secondStep, parent) are included
-   * in the jobData
-   * connects to slotListResult and slotListEntries
-   */
-  void listDirectory(QString path, ListType subscription,
-      bool secondStep = FALSE, KMFolder* parent = NULL, bool reset = false);
-
-  /**
    * Starts the folderlisting for the root folder
    */
   virtual void listDirectory();
