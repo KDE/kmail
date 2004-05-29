abb5c02805f3794619e55a81156b55334e1c4415
Ingo Klcker <kloecker@kde.org>
2004-05-29 23:01:41 +0000

    use i18n(singular,plural,number)
    svn path=/trunk/kdepim/; revision=315973

--- kmheaders.cpp
+++ kmheaders.cpp
@@ -1710,11 +1710,10 @@ void KMHeaders::moveMsgToFolder ( KMFolder* destFolder, bool askForConfirmation 
   KMMessageList msgList = *selectedMsgs();
   if ( !destFolder && askForConfirmation &&    // messages shall be deleted
        KMessageBox::warningContinueCancel(this,
-         ( msgList.count() == 1 )
-         ? i18n("<qt>Do you really want to delete the selected message?<br>"
-                "Once deleted, it cannot be restored.</qt>")
-         : i18n("<qt>Do you really want to delete the %1 selected messages?<br>"
-                "Once deleted, they cannot be restored.</qt>").arg( msgList.count() ),
+         i18n("<qt>Do you really want to delete the selected message?<br>"
+              "Once deleted, it cannot be restored.</qt>",
+              "<qt>Do you really want to delete the %n selected messages?<br>"
+              "Once deleted, they cannot be restored.</qt>", msgList.count() ),
          i18n("Delete Messages"), i18n("De&lete"), "NoConfirmDelete") == KMessageBox::Cancel )
     return;  // user canceled the action
 
