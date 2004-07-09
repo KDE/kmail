4cce7942034fbe5087b996193519c59aa006a65a
David Faure <faure@kde.org>
2004-07-09 22:49:17 +0000

    CVS_SILENT copy over the comments
    svn path=/trunk/kdepim/; revision=327370

--- kmail-3.3-aegypten.pl
+++ kmail-3.3-aegypten.pl
@@ -17,6 +17,9 @@ while ( <> ) {
     next;
   } elsif ( $currentGroup eq "" and /^pgpType/ ) {
     my ($key,$value) = split /=/;
+    # 0 is auto -> delete key, i.e. use the default
+    # 1-4 map to the kpgp ones
+    # 5 is off -> off; since there's no such backend it won't set one
     if ( $value != 0 ) {
       my @newvalues = ("","Kpgp/gpg1","Kpgp/pgp v2","Kpgp/pgp v5","Kpgp/pgp v6","");
       print "OpenPGP=$newvalues[$value]\n";
