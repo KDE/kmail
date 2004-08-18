5358a3520b11d5e42b226fc1cd7105093105e684
Till Adam <adam@kde.org>
2004-08-18 12:54:57 +0000

    Backport of crash fix from head. Don't override reference counting of folders when switching the resource folder away from an account.
    svn path=/branches/proko2/kdepim/; revision=338828

--- kmailicalifaceimpl.cpp
+++ kmailicalifaceimpl.cpp
@@ -1583,7 +1583,7 @@ static void cleanupFolder( KMFolder* folder, KMailICalIfaceImpl* _this )
     folder->setType( "plain" );
     folder->setSystemFolder( false );
     folder->disconnect( _this );
-    folder->close( true );
+    folder->close();
   }
 }
 
