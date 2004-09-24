0d5e0a6808573db6e700f4e6d081d2c0906105a1
Till Adam <adam@kde.org>
2004-09-24 21:08:50 +0000

    Make sure the readerwindow is cleared when the current message goes away. This fixes proko2 issue420, but I'll add crash guards just in case.
    svn path=/trunk/kdepim/; revision=349149

--- kmreaderwin.cpp
+++ kmreaderwin.cpp
@@ -912,8 +912,6 @@ void KMReaderWin::setMsg(KMMessage* aMsg, bool force)
 //-----------------------------------------------------------------------------
 void KMReaderWin::clearCache()
 {
-  if (mLastSerNum > 0) // no risk for a dangling pointer
-    return;
   updateReaderWinTimer.stop();
   clear();
   mDelayedMarkTimer.stop();
