023e5f3cdbede6a2012b0ebee1512312d5c35c46
Daniel Molkentin <molkentin@kde.org>
2004-09-29 11:31:26 +0000

    everyone please welcome: the new journaling icon.
    svn path=/trunk/kdepim/; revision=350231

--- kmailicalifaceimpl.cpp
+++ kmailicalifaceimpl.cpp
@@ -1712,7 +1712,7 @@ void KMailICalIfaceImpl::loadPixmaps() const
     pixCalendar = new QPixmap( UserIcon("kmgroupware_folder_calendar"));
     pixNotes    = new QPixmap( UserIcon("kmgroupware_folder_notes"));
     pixTasks    = new QPixmap( UserIcon("kmgroupware_folder_tasks"));
-    // TODO: Find a pixmap for journals
+    pixJournals = new QPixmap( UserIcon("kmgroupware_folder_journals"));
   }
 }
 
@@ -1739,6 +1739,7 @@ QPixmap* KMailICalIfaceImpl::pixContacts;
 QPixmap* KMailICalIfaceImpl::pixCalendar;
 QPixmap* KMailICalIfaceImpl::pixNotes;
 QPixmap* KMailICalIfaceImpl::pixTasks;
+QPixmap* KMailICalIfaceImpl::pixJournals;
 
 static void reloadFolderTree()
 {
