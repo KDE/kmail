8c8040fba9ed7b3d9923e274ce3336a9b9ccc206
Till Adam <adam@kde.org>
2004-10-03 20:36:42 +0000

    And here as well.
    svn path=/branches/proko2/kdepim/; revision=351636

--- imapaccountbase.h
+++ imapaccountbase.h
@@ -378,6 +378,7 @@ namespace KMail {
     bool mACLSupport : 1;
     bool mAnnotationSupport : 1;
     bool mSlaveConnected : 1;
+    bool mSlaveConnectionError : 1;
 
 	// folders that should be checked for new mails
 	QValueList<QGuardedPtr<KMFolder> > mMailCheckFolders;
