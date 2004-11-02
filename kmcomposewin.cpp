9b56f07b9011ad6afe9f88c539f224edff4d4753
David Faure <faure@kde.org>
2004-11-02 12:09:28 +0000

    Backport fix for 88576 - thanks Sandro!
    svn path=/branches/KDE_3_3_BRANCH/kdepim/; revision=359819

--- kmcomposewin.cpp
+++ kmcomposewin.cpp
@@ -3882,10 +3882,12 @@ void KMAtmListViewItem::enableCryptoCBs(bool on)
   if( mCBEncrypt ) {
     mCBEncryptEnabled = on;
     mCBEncrypt->setEnabled( on );
+    mCBEncrypt->setShown( on );
   }
   if( mCBSign ) {
     mCBSignEnabled = on;
     mCBSign->setEnabled( on );
+    mCBSign->setShown( on );
   }
 }
 
