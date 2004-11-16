54c324f7b6a9b8e373e16cd99e016dde965ad0eb
Stephan Binner <binner@kde.org>
2004-11-16 18:58:12 +0000

    homebrewn i18n plural
    svn path=/trunk/kdepim/; revision=363491

--- kmheaders.cpp
+++ kmheaders.cpp
@@ -1985,7 +1985,7 @@ void KMHeaders::moveMsgToFolder ( KMFolder* destFolder, bool askForConfirmation 
               "Once deleted, it cannot be restored.</qt>",
               "<qt>Do you really want to delete the %n selected messages?<br>"
               "Once deleted, they cannot be restored.</qt>", msgList.count() ),
-	 i18n("Delete Message","Delete Messages",msgList.count()), KStdGuiItem::del(),
+	 msgList.count()>1 ? i18n("Delete Messages") : i18n("Delete Message"), KStdGuiItem::del(),
 	 "NoConfirmDelete") == KMessageBox::Cancel )
     return;  // user canceled the action
 
