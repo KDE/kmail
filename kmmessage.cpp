ee30ba875eea3f8adacbc6966505c4d3ede97b48
Till Adam <adam@kde.org>
2004-11-17 21:59:03 +0000

    Reallign with reality.
    svn path=/trunk/kdepim/; revision=363789

--- kmmessage.cpp
+++ kmmessage.cpp
@@ -4191,7 +4191,9 @@ void KMMessage::setMDNSentState( KMMsgMDNSentState status, int idx ) {
 //-----------------------------------------------------------------------------
 void KMMessage::link( const KMMessage *aMsg, KMMsgStatus aStatus )
 {
-  Q_ASSERT( aStatus == KMMsgStatusReplied || aStatus == KMMsgStatusForwarded );
+  Q_ASSERT( aStatus == KMMsgStatusReplied
+      || aStatus == KMMsgStatusForwarded
+      || aStatus == KMMsgStatusDeleted );
 
   QString message = headerField( "X-KMail-Link-Message" );
   if ( !message.isEmpty() )
