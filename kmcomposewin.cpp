6d763ab901e224e8c3e73432c1df5b42adcb59f3
David Faure <faure@kde.org>
2004-12-10 21:06:34 +0000

    Why do all kmail crashes happen in dumpDeadLetter()?
    svn path=/branches/KDE_3_3_BRANCH/kdepim/; revision=369715

--- kmcomposewin.cpp
+++ kmcomposewin.cpp
@@ -643,6 +643,8 @@ void KMComposeWin::deadLetter()
     return;
   }
   KMMessage *msg = mComposedMessages.first();
+  if ( !msg )
+    return;
   QCString msgStr = msg->asString();
   QCString fname = getenv("HOME");
   fname += "/dead.letter.tmp";
