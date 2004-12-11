42c2133acbaf2d6114da772250abfd7c1c38a8ad
David Faure <faure@kde.org>
2004-12-11 11:20:44 +0000

    [PATCH]Fix crash on exit with cyrus without specific vendor annotations From: Mario Teijeiro Otero <emeteo escomposlinux org> - thanks
    svn path=/branches/proko2/kdepim/; revision=369826

--- kmfoldercachedimap.cpp
+++ kmfoldercachedimap.cpp
@@ -1931,9 +1931,9 @@ KMFolderCachedImap::slotSetAnnotationResult(KIO::Job *job)
   bool cont = true;
   if ( job->error() ) {
     // Don't show error if the server doesn't support ANNOTATEMORE and this folder only contains mail
-    if ( job->error() == KIO::ERR_UNSUPPORTED_ACTION && contentsType() == ContentsTypeMail )
+    if ( job->error() == KIO::ERR_UNSUPPORTED_ACTION && contentsType() == ContentsTypeMail ) {
       if (mAccount->slave()) mAccount->removeJob(job);
-    else
+    } else
       cont = mAccount->handleJobError( job, i18n( "Error while setting annotation: " ) + '\n' );
   } else {
     if (mAccount->slave()) mAccount->removeJob(job);
