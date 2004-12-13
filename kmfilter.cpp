dd4b54270d54934700b5eaf80480ea4c47b31bee
Andreas Gungl <a.gungl@gmx.de>
2004-12-13 23:05:04 +0000

    If a filter action could not be applied in a clean way, a message is now written to the filter log viewer. This can help you to track down problems with non-existing folders. The only way I see to get them into kmailrc is to copy a configuration from somewhere else. KMail by itself doesn't allow to specify non-existing folders as targets for filter actions. So at least the user can find out that something's wrong.
    BUG: 95107
    
    svn path=/trunk/kdepim/; revision=370453

--- kmfilter.cpp
+++ kmfilter.cpp
@@ -98,9 +98,19 @@ KMFilter::ReturnCode KMFilter::execActions( KMMessage* msg, bool& stopIt ) const
 
     switch ( result ) {
     case KMFilterAction::CriticalError:
+      if ( FilterLog::instance()->isLogging() ) {
+        QString logText = QString( "<font color=#FF0000>%1</font>" )
+          .arg( i18n( "A critical error occured. Processing stops here." ) );
+        FilterLog::instance()->add( logText, FilterLog::appliedAction );
+      }
       // in case it's a critical error: return immediately!
       return CriticalError;
     case KMFilterAction::ErrorButGoOn:
+      if ( FilterLog::instance()->isLogging() ) {
+        QString logText = QString( "<font color=#FF0000>%1</font>" )
+          .arg( i18n( "A problem was found while applying this action!" ) );
+        FilterLog::instance()->add( logText, FilterLog::appliedAction );
+      }
     default:
       break;
     }
