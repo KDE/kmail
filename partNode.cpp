bbb18689309c25ab11bbb0c793cc943b71324cea
Carsten Burghardt <burghardt@kde.org>
2005-07-23 11:46:14 +0000

    Compare the IDs correctly with kasciistricmp. BUGS:109003
    svn path=/trunk/KDE/kdepim/; revision=437867

--- partNode.cpp
+++ partNode.cpp
@@ -36,6 +36,7 @@
 #include "kmmimeparttree.h"
 #include <mimelib/utility.h>
 #include <qregexp.h>
+#include <kasciistricmp.h>
 
 /*
   ===========================================================================
@@ -413,7 +414,7 @@ partNode* partNode::findType( int type, int subType, bool deep, bool wide )
 partNode* partNode::findNodeForDwPart( DwBodyPart* part )
 {
     partNode* found = 0;
-    if( dwPart()->partId() == part->partId() )
+    if( kasciistricmp( dwPart()->partId(), part->partId() ) == 0 )
         return this;
     if( mChild )
         found = mChild->findNodeForDwPart( part );
