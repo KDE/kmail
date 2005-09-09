edc69d53ca12baa46169ec1d30d511cd17284b64
Stephan Kulow <coolo@kde.org>
2005-09-09 05:09:08 +0000

    grrr, copyright years are just like version updates
    svn path=/branches/KDE/3.5/kdepim/; revision=458761

--- aboutdata.cpp
+++ aboutdata.cpp
@@ -210,7 +210,7 @@ namespace KMail {
   AboutData::AboutData()
     : KAboutData( "kmail", I18N_NOOP("KMail"),KMAIL_VERSION,
                   I18N_NOOP("KDE Email Client"), License_GPL,
-                  I18N_NOOP("(c) 1997-2004, The KMail developers"), 0,
+                  I18N_NOOP("(c) 1997-2005, The KMail developers"), 0,
                   "http://kmail.kde.org" )
   {
     using KMail::authors;
