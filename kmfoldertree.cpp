dcecc05c2d37baf2ada425bbe2b3c5a7e42fd9b8
Allen Winter <winter@kde.org>
2006-02-13 20:29:00 +0000

    Back by popular demand (votes), approved by Ingo and Till, we undo revision 450009.  The "Delete Folder" menu selection is now restored.
    Also added a separator above this selection so it stands out a little better,
    as suggested by Ellen on the usability list.
    
    BUGS: 120024
    
    
    svn path=/branches/KDE/3.5/kdepim/; revision=509156

--- kmfoldertree.cpp
+++ kmfoldertree.cpp
@@ -1020,6 +1020,10 @@ void KMFolderTree::slotContextMenuRequested( QListViewItem *lvi,
       mMainWidget->action("compact")->plug(folderMenu);
 
       folderMenu->insertSeparator();
+      if ( !fti->folder()->isSystemFolder() )
+        mMainWidget->action("delete_folder")->plug(folderMenu);
+
+      folderMenu->insertSeparator();
       mMainWidget->action("empty")->plug(folderMenu);
       folderMenu->insertSeparator();
     }
