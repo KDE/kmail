42611be407b551408a0d8b1f3a6c6cc069cf04bc
Allen Winter <winter@kde.org>
2006-09-07 23:46:23 +0000

    Fix old bug 66057: mails don't get marked (read etc.) Patch provided by Christian Zangl (Thanks!) and approved by Till and Ingo.
    BUGS: 66057
    
    svn path=/branches/KDE/3.5/kdepim/; revision=581934

--- kmreadermainwin.cpp
+++ kmreadermainwin.cpp
@@ -115,6 +115,7 @@ void KMReaderMainWin::showMsg( const QString & encoding, KMMessage *msg )
 {
   mReaderWin->setOverrideEncoding( encoding );
   mReaderWin->setMsg( msg, true );
+  mReaderWin->slotTouchMessage();
   setCaption( msg->subject() );
   mMsg = msg;
   menuBar()->show();
