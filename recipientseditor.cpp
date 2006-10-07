238d7c5579cb60a018b47f8b8c9068880458bcbf
Allen Winter <winter@kde.org>
2006-10-07 17:15:10 +0000

    forward port SVN commit 585468 by kling:
    Always initialize mLineHeight.
    
    
    svn path=/trunk/KDE/kdepim/; revision=593384

--- recipientseditor.cpp
+++ recipientseditor.cpp
@@ -328,7 +328,7 @@ void RecipientLine::setRemoveLineButtonEnabled( bool b )
 
 RecipientsView::RecipientsView( QWidget *parent )
   : Q3ScrollView( parent ), mCurDelLine( 0 ), mModified( false ),
-    mFirstColumnWidth( 0 )
+    mFirstColumnWidth( 0 ), mLineHeight( 0 )
 {
   setHScrollBarMode( AlwaysOff );
   setLineWidth( 0 );
