175a9e0c0c70b546ad0cdda300ae0b148e1652fe
Allen Winter <winter@kde.org>
2006-11-06 18:33:27 +0000

    forward port SVN commit 602743 by winterz:
    Patch from Thomas McGuire that fixes the bugs:
    + Wrong checkbox in the layout when adding attachments to the composer
    + attachment display in editor: "sort by size" sorts alphanumerically, not by size
    
    
    svn path=/trunk/KDE/kdepim/; revision=602755

--- kmatmlistview.h
+++ kmatmlistview.h
@@ -1,5 +1,5 @@
 /* -*- mode: C++; c-file-style: "gnu" -*-
- * KMComposeWin Header File
+ * KMAtmListViewItem Header File
  * Author: Markus Wuebben <markus.wuebben@kde.org>
  */
 #ifndef __KMAIL_KMATMLISTVIEW_H__
@@ -15,48 +15,59 @@ class QCheckBox;
 class KMAtmListViewItem : public QObject, public Q3ListViewItem
 {
   Q_OBJECT
-  friend class ::KMComposeWin;
-  friend class ::MessageComposer;
 
 public:
-  KMAtmListViewItem(Q3ListView * parent);
+  KMAtmListViewItem( Q3ListView *parent );
   virtual ~KMAtmListViewItem();
-  virtual void paintCell( QPainter * p, const QColorGroup & cg,
-                          int column, int width, int align );
 
-  void setUncompressedMimeType( const QByteArray & type, const QByteArray & subtype ) {
+  //A custom compare function is needed because the size column is
+  //human-readable and therefore doesn't sort correctly.
+  virtual int compare( Q3ListViewItem *i, int col, bool ascending ) const;
+
+  virtual void paintCell( QPainter *p, const QColorGroup &cg, int column,
+                          int width, int align );
+
+  void setUncompressedMimeType( const QByteArray &type, const QByteArray &subtype )
+  {
     mType = type; mSubtype = subtype;
   }
-  void uncompressedMimeType( QByteArray & type, QByteArray & subtype ) const {
+  void setAttachmentSize( int numBytes )
+  {
+    mAttachmentSize = numBytes;
+  }
+  void uncompressedMimeType( QByteArray & type, QByteArray & subtype ) const
+  {
     type = mType; subtype = mSubtype;
   }
   void setUncompressedCodec( const QByteArray & codec ) { mCodec = codec; }
   QByteArray uncompressedCodec() const { return mCodec; }
 
+  void enableCryptoCBs( bool on );
+  void setEncrypt( bool on );
+  bool isEncrypt();
+  void setSign( bool on );
+  bool isSign();
+  void setCompress( bool on );
+  bool isCompress();
+
 signals:
   void compress( int );
   void uncompress( int );
 
-protected:
-  void enableCryptoCBs(bool on);
-  void setEncrypt(bool on);
-  bool isEncrypt();
-  void setSign(bool on);
-  bool isSign();
-  void setCompress(bool on);
-  bool isCompress();
-
 private slots:
   void slotCompress();
+  void slotHeaderChange( int, int, int );
+  void slotHeaderClick( int );
 
 private:
-  Q3ListView* mListview;
-  QCheckBox* mCBEncrypt;
-  QCheckBox* mCBSign;
-  QCheckBox* mCBCompress;
-  bool mCBSignEnabled, mCBEncryptEnabled;
-  QByteArray mType, mSubtype, mCodec;
-};
+  void updateCheckBox( int headerSection, QCheckBox *cb );
+  void updateAllCheckBoxes();
 
+  QCheckBox *mCBEncrypt;
+  QCheckBox *mCBSign;
+  QCheckBox *mCBCompress;
+  QByteArray mType, mSubtype, mCodec;
+  int mAttachmentSize;
+};
 
 #endif // __KMAIL_KMATMLISTVIEW_H__
