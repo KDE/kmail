46f4404bca7f4d0eba709f2de137471679077b9c
Frank Osterfeld <frank.osterfeld@kdemail.net>
2007-02-07 11:24:38 +0000

    update version numbers
    svn path=/branches/kdepim/proko2/kdepim/; revision=631202

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.7 (proko2 branch after 2.1.5)"
+#define KMAIL_VERSION "1.7 (proko2 branch after 2.1.6)"
 
 #endif /*kmversion_h*/
