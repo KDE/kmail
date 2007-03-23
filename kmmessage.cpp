7b79526ba7c8cb198b892e4159e367b91b85a486
Pradeepto Bhattacharya <pradeepto@kde.org>
2007-03-23 06:43:35 +0000

    Forward port of ...
    SVN commit 645625 by pradeepto:
    
    kolab/issue1583 - Fixing OL compatibility with attachments that have umlauts in the filename. 
    Volker's patch worked really awesome. Thanks Volker. 
    
    
    svn path=/branches/KDE/3.5/kdepim/; revision=645628

--- kmmessage.cpp
+++ kmmessage.cpp
@@ -4140,11 +4140,23 @@ void KMMessage::updateAttachmentState( DwBodyPart* part )
     return;
   }
 
+  bool filenameEmpty = true;
+  if ( part->hasHeaders() ) {
+    if ( part->Headers().HasContentDisposition() ) {
+      DwDispositionType cd = part->Headers().ContentDisposition();
+      filenameEmpty = cd.Filename().empty();
+      if ( filenameEmpty ) {
+        // let's try if it is rfc 2231 encoded which mimelib can't handle
+        filenameEmpty = KMMsgBase::decodeRFC2231String( KMMsgBase::extractRFC2231HeaderField( cd.AsString().c_str(), "filename" ) ).isEmpty();
+      }
+    }
+  }
+
   if ( part->hasHeaders() &&
        ( ( part->Headers().HasContentDisposition() &&
            !part->Headers().ContentDisposition().Filename().empty() ) ||
          ( part->Headers().HasContentType() &&
-           !part->Headers().ContentType().Name().empty() ) ) )
+           !filenameEmpty ) ) )
   {
     // now blacklist certain ContentTypes
     if ( !part->Headers().HasContentType() ||
