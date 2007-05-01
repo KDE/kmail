c8a6ee1d6a3116f441817f6b31f5215757848d84
Allen Winter <winter@kde.org>
2007-05-01 15:39:56 +0000

    cleaning, scrubbing, and some minor Krazy fixes.
    svn path=/trunk/KDE/kdepim/; revision=660115

--- callback.cpp
+++ callback.cpp
@@ -1,47 +1,50 @@
 /*  -*- c++ -*-
-    callback.cpp
+  callback.cpp
 
-    This file is used by KMail's plugin interface.
-    Copyright (c) 2004 Bo Thorsen <bo@sonofthor.dk>
+  This file is used by KMail's plugin interface.
+  Copyright (c) 2004 Bo Thorsen <bo@sonofthor.dk>
 
-    KMail is free software; you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  KMail is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    KMail is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
+  KMail is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-    In addition, as a special exception, the copyright holders give
-    permission to link the code of this program with any edition of
-    the Qt library by Trolltech AS, Norway (or with modified versions
-    of Qt that use the same license as Qt), and distribute linked
-    combinations including the two.  You must obey the GNU General
-    Public License in all respects for all of the code used other than
-    Qt.  If you modify this file, you may extend this exception to
-    your version of the file, but you are not obligated to do so.  If
-    you do not wish to do so, delete this exception statement from
-    your version.
+  In addition, as a special exception, the copyright holders give
+  permission to link the code of this program with any edition of
+  the Qt library by Trolltech AS, Norway (or with modified versions
+  of Qt that use the same license as Qt), and distribute linked
+  combinations including the two.  You must obey the GNU General
+  Public License in all respects for all of the code used other than
+  Qt.  If you modify this file, you may extend this exception to
+  your version of the file, but you are not obligated to do so.  If
+  you do not wish to do so, delete this exception statement from
+  your version.
 */
 
-#include "config.h"
-
 #include "callback.h"
+
+#include <config.h>
+
 #include "kmkernel.h"
 #include "kmmessage.h"
-#include <kpimutils/email.h>
-#include <libkpimidentities/identity.h>
-#include <libkpimidentities/identitymanager.h>
 #include "kmmainwin.h"
 #include "composer.h"
 #include "kmreaderwin.h"
 #include "secondarywindow.h"
+
+#include <kpimutils/email.h>
+
+#include <libkpimidentities/identity.h>
+#include <libkpimidentities/identitymanager.h>
 
 #include <mimelib/enum.h>
 
@@ -52,14 +55,13 @@
 
 using namespace KMail;
 
-
-Callback::Callback( KMMessage* msg, KMReaderWin* readerWin )
+Callback::Callback( KMMessage *msg, KMReaderWin *readerWin )
   : mMsg( msg ), mReaderWin( readerWin ), mReceiverSet( false )
 {
 }
 
-bool Callback::mailICal( const QString& to, const QString iCal,
-                         const QString& subject ) const
+bool Callback::mailICal( const QString &to, const QString iCal,
+                         const QString &subject ) const
 {
   kDebug(5006) << "Mailing message:\n" << iCal << endl;
 
@@ -78,19 +80,20 @@ bool Callback::mailICal( const QString& to, const QString iCal,
   // Outlook will only understand the reply if the From: header is the
   // same as the To: header of the invitation message.
   KConfigGroup options( KMKernel::config(), "Groupware" );
-  if( !options.readEntry( "LegacyMangleFromToHeaders", true ) ) {
+  if ( !options.readEntry( "LegacyMangleFromToHeaders", true ) ) {
     // Try and match the receiver with an identity
-    const KPIM::Identity& identity =
+    const KPIM::Identity &identity =
       kmkernel->identityManager()->identityForAddress( receiver() );
-    if( identity != KPIM::Identity::null() )
+    if ( identity != KPIM::Identity::null() ) {
       // Identity found. Use this
       msg->setFrom( identity.fullEmailAddr() );
-      msg->setHeaderField("X-KMail-Identity", QString::number( identity.uoid() ));
-      // Remove BCC from identity on ical invitations (https://intevation.de/roundup/kolab/issue474)
-      msg->setBcc( "" );
+    }
+    msg->setHeaderField( "X-KMail-Identity", QString::number( identity.uoid() ) );
+    // Remove BCC from identity on ical invitations (https://intevation.de/roundup/kolab/issue474)
+    msg->setBcc( "" );
   }
 
-  KMail::Composer * cWin = KMail::makeComposer();
+  KMail::Composer *cWin = KMail::makeComposer();
   cWin->setMsg( msg, false /* mayAutoSign */ );
   // cWin->setCharset( "", true );
   cWin->slotWordWrapToggled( false );
@@ -128,7 +131,7 @@ QString Callback::receiver() const
 
   QStringList ccaddrs = KPIMUtils::splitAddressList( mMsg->cc() );
   for ( QStringList::Iterator it = ccaddrs.begin(); it != ccaddrs.end(); ++it ) {
-    if( kmkernel->identityManager()->identityForAddress( *it ) !=
+    if ( kmkernel->identityManager()->identityForAddress( *it ) !=
         KPIM::Identity::null() ) {
       // Ok, this could be us
       ++found;
@@ -164,7 +167,8 @@ QString Callback::receiver() const
 
 void Callback::closeIfSecondaryWindow() const
 {
-  KMail::SecondaryWindow *window = dynamic_cast<KMail::SecondaryWindow*>( mReaderWin->mainWindow() );
+  KMail::SecondaryWindow *window =
+    dynamic_cast<KMail::SecondaryWindow*>( mReaderWin->mainWindow() );
   if ( window ) {
     window->close();
   }
--- callback.h
+++ callback.h
@@ -1,33 +1,33 @@
 /*  -*- c++ -*-
-    callback.h
+  callback.h
 
-    This file is used by KMail's plugin interface.
-    Copyright (c) 2004 Bo Thorsen <bo@sonofthor.dk>
+  This file is used by KMail's plugin interface.
+  Copyright (c) 2004 Bo Thorsen <bo@sonofthor.dk>
 
-    KMail is free software; you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  KMail is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    KMail is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
+  KMail is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-    In addition, as a special exception, the copyright holders give
-    permission to link the code of this program with any edition of
-    the Qt library by Trolltech AS, Norway (or with modified versions
-    of Qt that use the same license as Qt), and distribute linked
-    combinations including the two.  You must obey the GNU General
-    Public License in all respects for all of the code used other than
-    Qt.  If you modify this file, you may extend this exception to
-    your version of the file, but you are not obligated to do so.  If
-    you do not wish to do so, delete this exception statement from
-    your version.
+  In addition, as a special exception, the copyright holders give
+  permission to link the code of this program with any edition of
+  the Qt library by Trolltech AS, Norway (or with modified versions
+  of Qt that use the same license as Qt), and distribute linked
+  combinations including the two.  You must obey the GNU General
+  Public License in all respects for all of the code used other than
+  Qt.  If you modify this file, you may extend this exception to
+  your version of the file, but you are not obligated to do so.  If
+  you do not wish to do so, delete this exception statement from
+  your version.
 */
 
 #ifndef CALLBACK_H
@@ -49,27 +49,36 @@ namespace KMail {
     a nicely formatted class, but simply include everything necessary
     for the plugins. */
 class KDE_EXPORT Callback {
-public:
-  Callback( KMMessage* msg, KMReaderWin* readerWin );
 
-  /** Get the full message */
-  KMMessage* getMsg() const { return mMsg; }
+  public:
+    Callback( KMMessage *msg, KMReaderWin *readerWin );
 
-  /** Mail a message */
-  bool mailICal( const QString& to, const QString iCal,
-                 const QString& subject ) const;
+    /*
+      Returns the full message.
+    */
+    KMMessage *getMsg() const { return mMsg; }
 
-  /** Get the receiver of the mail */
-  QString receiver() const;
+    /*
+      Mails a message.
+    */
+    bool mailICal( const QString &to, const QString iCal,
+                   const QString &subject ) const;
 
-  /** Close the main window showing this message, if it's a secondary window. */
-  void closeIfSecondaryWindow() const;
+    /*
+      Returns the receiver of the mail.
+    */
+    QString receiver() const;
 
-private:
-  KMMessage* mMsg;
-  KMReaderWin* mReaderWin;
-  mutable QString mReceiver;
-  mutable bool mReceiverSet;
+    /*
+      Closes the main window showing this message, if it's a secondary window.
+    */
+    void closeIfSecondaryWindow() const;
+
+  private:
+    KMMessage *mMsg;
+    KMReaderWin *mReaderWin;
+    mutable QString mReceiver;
+    mutable bool mReceiverSet;
 };
 
 }
