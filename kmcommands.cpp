537725a018206e4b15eba6ffcf559cf5eba2ce24
Pradeepto Bhattacharya <pradeepto@kde.org>
2007-05-29 04:05:52 +0000

    Merge ... SVN commit 666776 by savernik:
    SVN_SILENT don't use false if you actually mean NULL
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=669335

--- kmcommands.cpp
+++ kmcommands.cpp
@@ -1230,7 +1230,7 @@ KMCommand::Result KMForwardInlineCommand::execute()
     for ( KMMessage *msg = linklist.first(); msg; msg = linklist.next() ) {
       TemplateParser parser( fwdMsg, TemplateParser::Forward,
         msg->body(), false, false, false, false);
-        parser.process( msg, false, true );
+        parser.process( msg, 0, true );
 
       fwdMsg->link( msg, KMMsgStatusForwarded );
     }
@@ -1534,7 +1534,7 @@ KMCommand::Result KMCustomForwardCommand::execute()
     for ( KMMessage *msg = linklist.first(); msg; msg = linklist.next() ) {
       TemplateParser parser( fwdMsg, TemplateParser::Forward,
         msg->body(), false, false, false, false);
-        parser.process( msg, false, true );
+        parser.process( msg, 0, true );
 
       fwdMsg->link( msg, KMMsgStatusForwarded );
     }
