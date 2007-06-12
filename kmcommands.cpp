6db13f9aaae5f69c700aa0905a222a614f3f53b1
Jonathan Marten <jjm@keelhaul.me.uk>
2007-06-12 15:39:07 +0000

    Forward port of 674569 from 3.5 branch: Remove date and message-id from template message, so that new message will be assigned new up-to-date ones.
    BUG:146534 
    
    svn path=/trunk/KDE/kdepim/; revision=674571

--- kmcommands.cpp
+++ kmcommands.cpp
@@ -706,6 +706,10 @@ KMCommand::Result KMUseTemplateCommand::execute()
   KMMessage *newMsg = new KMMessage( new DwMessage( *msg->asDwMessage() ) );
   newMsg->setComplete( msg->isComplete() );
 
+  // these fields need to be regenerated for the new message
+  newMsg->removeHeaderField("Date");
+  newMsg->removeHeaderField("Message-ID");
+
   KMail::Composer *win = KMail::makeComposer();
   newMsg->setTransferInProgress( false ); // From here on on, the composer owns the message.
   win->setMsg( newMsg, false, true );
