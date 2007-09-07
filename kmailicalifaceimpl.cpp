6c33f2c4ae0c0ecf43144fb6e3247c6e8cc3c0ac
Thomas McGuire <mcguire@kde.org>
2007-09-07 13:28:16 +0000

    #warning -> Till todo
    SVN_SILENT
    
    svn path=/trunk/KDE/kdepim/; revision=709417

--- kmailicalifaceimpl.cpp
+++ kmailicalifaceimpl.cpp
@@ -596,9 +596,7 @@ void KMailICalIfaceImpl::slotMessageRetrieved( KMMessage* msg )
   quint32 sernum = msg->getMsgSerNum();
 
   // do we have an accumulator for this folder?
-#ifdef __GNUC__
-#warning Where is the value actually inserted??? --tmcguire
-#endif
+  // TODO (till): Remove this accumulator stuff, it is dead code.
   Accumulator *ac = mAccumulators.value( parent->location(), 0 );
   if( ac ) {
     QString s;
