8434d68aa90ee119f55fe5ed207543c1ba573f29
Volker Krause <vkrause@kde.org>
2007-09-21 08:48:19 +0000

    Fix encoding.
    Kolab issue 1983.
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=715112

--- objecttreeparser.cpp
+++ objecttreeparser.cpp
@@ -1573,7 +1573,7 @@ namespace KMail {
             if ( mReader->decryptMessage() )
               writePartIcon( &node->msgPart(), node->nodeId() );
             else
-              htmlWriter()->queue( decryptedData );
+              htmlWriter()->queue( QString::fromUtf8( decryptedData ) );
             htmlWriter()->queue( writeSigstatFooter( messagePart ) );
           }
         } else {
