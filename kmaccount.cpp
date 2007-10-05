2ecae0733029a2e905f5aa996c1904953853bcae
Volker Krause <vkrause@kde.org>
2007-10-05 07:41:06 +0000

    Forwardport SVN commit 720075 by vkrause from proko2 branch:
    KMKernel might be already deleted at this point.
    
    Kolab issue 1904.
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=721407

--- kmaccount.cpp
+++ kmaccount.cpp
@@ -105,7 +105,7 @@ void KMAccount::init() {
 //-----------------------------------------------------------------------------
 KMAccount::~KMAccount()
 {
-  if (!kmkernel->shuttingDown() && mFolder) mFolder->removeAccount(this);
+  if ( (kmkernel && !kmkernel->shuttingDown()) && mFolder ) mFolder->removeAccount(this);
   if (mTimer) deinstallTimer();
 }
 
