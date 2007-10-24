03fbffb360433a4330b28e7c22536e3a2fe5baee
Volker Krause <vkrause@kde.org>
2007-10-24 10:22:21 +0000

    Fix %1 substitution.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=728815

--- kmreaderwin.cpp
+++ kmreaderwin.cpp
@@ -2634,7 +2634,7 @@ void KMReaderWin::injectAttachments()
   if ( html.isEmpty() )
     return;
   if ( headerStyle() == HeaderStyle::fancy() )
-    html.prepend( "<div style=\"float:left;\">%1&nbsp;</div>" ).arg(i18n("Attachments:"));
+    html.prepend( QString::fromLatin1("<div style=\"float:left;\">%1&nbsp;</div>" ).arg(i18n("Attachments:")) );
   assert( injectionPoint.tagName() == "div" );
   static_cast<DOM::HTMLElement>( injectionPoint ).setInnerHTML( html );
 }
