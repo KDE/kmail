1b9179ca8f9c4aecbf37fac161d6c7042258ffd0
Till Adam <adam@kde.org>
2007-11-05 10:01:27 +0000

    This is in an anonymous namespace already, so avoid the warnings.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=733048

--- util.h
+++ util.h
@@ -199,7 +199,7 @@ namespace Util {
     };
 
     // return true if we should proceed, false if we should abort
-    static bool checkOverwrite( const KURL& url, QWidget* w )
+    bool checkOverwrite( const KURL& url, QWidget* w )
     {
         if ( KIO::NetAccess::exists( url, false /*dest*/, w ) ) {
             if ( KMessageBox::Cancel ==
