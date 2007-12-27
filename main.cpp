2587297f3c6861e7aed5a2f7d94c7b0d1e30c236
Jakob Petsovits <jpetso@gmx.at>
2007-12-27 12:14:26 +0000

    Er, oops. Don't assign KMail the address book icon.
    svn path=/trunk/KDE/kdepim/; revision=753418

--- main.cpp
+++ main.cpp
@@ -100,7 +100,7 @@ int main(int argc, char *argv[])
      return 0;
 
   KMailApplication app;
-  QApplication::setWindowIcon( KIcon( "office-address-book" ) );
+  QApplication::setWindowIcon( KIcon( "internet-mail" ) );
 
   // import i18n data and icons from libraries:
   KMail::insertLibraryCataloguesAndIcons();
