46080c3024879bfd815ea8d3f467c7ad8af76b4a
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-01-18 12:01:59 +0000

    SVN_SILENT version bump, this one is for real.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=763039

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.6 (enterprise 0.20080118.762989)"
+#define KMAIL_VERSION "1.9.6 (enterprise 0.20080118.763038)"
 
 #endif /*kmversion_h*/
