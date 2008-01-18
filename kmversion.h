b6def40b096f2e230246bd7856669e9dff012120
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-01-18 10:23:31 +0000

    SVN_SILENT Blocked revisions 689590 via svnmerge
    ........
      r689590 | vkrause | 2007-07-18 21:51:18 +0530 (Wed, 18 Jul 2007) | 4 lines
      
      Merge SVN commit 677090 by vkrause from enterprise branch:
      
      No more invalid reads while deleting/moving folders.
    ........
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=763016

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.6 (enterprise 0.20071204.744707)"
+#define KMAIL_VERSION "1.9.6 (enterprise 0.20080118.762989)"
 
 #endif /*kmversion_h*/
