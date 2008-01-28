84f372331f015cd8d8068c5b374491cc735d54ec
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-01-28 10:34:35 +0000

    - version bump
    svn path=/branches/kdepim/enterprise/kdepim/; revision=767557

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.6 (enterprise 0.20080118.763038)"
+#define KMAIL_VERSION "1.9.6 (enterprise 0.20080128.767555)"
 
 #endif /*kmversion_h*/
