2d4e89630884a7329c4935f6e1fa60d3c0436e77
Thomas Moenicke <tm@php-qt.org>
2008-02-01 23:09:44 +0000

    * merged revision 769706 from enterprise (license,copyright)
    svn path=/trunk/KDE/kdepim/; revision=769734

--- snippetitem.cpp
+++ snippetitem.cpp
@@ -1,10 +1,16 @@
-/*
- *  File : snippetitem.cpp
- *
- *  Author: Robert Gruber <rgruber@users.sourceforge.net>
- *
- *  Copyright: See COPYING file that comes with this distribution
- */
+/***************************************************************************
+ *   snippet feature from kdevelop/plugins/snippet/                        *
+ *                                                                         * 
+ *   Copyright (C) 2007 by Robert Gruber                                   *
+ *   rgruber@users.sourceforge.net                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "snippetitem.h"
 
 #include <kaction.h>
