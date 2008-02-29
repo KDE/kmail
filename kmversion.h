c02cbe3a857dda82ae4c4aaebca1ae2e13e480f8
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-02-29 12:08:49 +0000

    -- This version is officially renamed to "Keep Volker Happy" version :).
    svn path=/branches/kdepim/enterprise/kdepim/; revision=780530

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.9 (enterprise 0.20080229.780496)"
+#define KMAIL_VERSION "1.9.9 (enterprise 0.20080229.780520)"
 
 #endif /*kmversion_h*/
