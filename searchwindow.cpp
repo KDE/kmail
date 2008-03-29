34c62e2ed961fc2cbe66a1e4c9086d07792d4679
Eduardo Robles Elvira <edulix@gmail.com>
2008-03-29 12:05:46 +0000

    Remove checks for Qt 4.4 as its use is mandatory in trunk
    svn path=/trunk/KDE/kdepim/knode/; revision=791398

--- searchwindow.cpp
+++ searchwindow.cpp
@@ -348,11 +348,7 @@ SearchWindow::SearchWindow(KMMainWidget* w, KMFolder *curFolder):
 
   ac->addAssociatedWidget( this );
   foreach (QAction* action, ac->actions())
-#if QT_VERSION < KDE_MAKE_VERSION(4,4,0)
-    action->setShortcutContext(Qt::WidgetShortcut); // remove after Qt4.4 becomes mandatory
-#else
     action->setShortcutContext(Qt::WidgetWithChildrenShortcut);
-#endif
 }
 
 //-----------------------------------------------------------------------------
