9422d495cb25d1cc01e9a419968fec92f3c50a31
Volker Krause <vkrause@kde.org>
2008-03-29 15:18:54 +0000

    SVN_SILENT update version numbers
    svn path=/branches/kdepim/enterprise/kdepim/; revision=791442

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.9 (enterprise 0.20080321.788243)"
+#define KMAIL_VERSION "1.9.9 (enterprise 0.20080329.791441)"
 
 #endif /*kmversion_h*/
