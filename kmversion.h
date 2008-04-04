8d8c56e6b047478105068e0c8c20330d085ef708
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-04-04 10:16:13 +0000

    Update version numbers for - Happy Birthday Till Adam and Happy Anniversary Jesper release
    svn path=/branches/kdepim/enterprise/kdepim/; revision=793539

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.9 (enterprise 0.20080329.791441)"
+#define KMAIL_VERSION "1.9.9 (enterprise 0.20080404.793538)"
 
 #endif /*kmversion_h*/
