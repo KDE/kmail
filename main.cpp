1cce051d225d3ddda62252b0fbdb8f9a523f48e2
Friedrich W. H. Kossebau <kossebau@kde.org>
2008-05-14 17:28:18 +0000

    no need to set the program icon to the program name, already done by KApplication (in case it should be rather set by KAboutData::ProgramIconName these days anyway)
    svn path=/trunk/KDE/kdepim/; revision=807774

--- main.cpp
+++ main.cpp
@@ -101,7 +101,6 @@ int main(int argc, char *argv[])
      return 0;
 
   KMailApplication app;
-  QApplication::setWindowIcon( KIcon( "kmail" ) );
 
   // import i18n data and icons from libraries:
   KMail::insertLibraryCataloguesAndIcons();
