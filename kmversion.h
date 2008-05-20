4ab7af9c47a816cea1b308346918cda6d7ddc8de
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-05-20 13:05:40 +0000

    SVN_SILENT Ich bin ein Berliner Release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=810305

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.9 (enterprise 0.20080509.805825)"
+#define KMAIL_VERSION "1.9.9 (enterprise 0.20080520.810303)"
 
 #endif /*kmversion_h*/
