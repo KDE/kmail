2a30beefefc92d0102a6f9be77ac8335b14c3b36
Thomas McGuire <mcguire@kde.org>
2008-07-19 20:10:30 +0000

    Merged revisions 827244 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/trunk/KDE/kdepim
    ........
      r827244 | winterz | 2008-07-02 15:15:54 +0200 (Wed, 02 Jul 2008) | 2 lines
      
      add a comment with code to fix bug 164970 after the string freeze is lifted.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=834998

--- configuredialog.cpp
+++ configuredialog.cpp
@@ -5227,6 +5227,9 @@ MiscPageGroupwareTab::MiscPageGroupwareTab( QWidget* parent )
            this, SLOT( slotEmitChanged( void ) ) );
 
   mExchangeCompatibleInvitations = new QCheckBox( i18n( "Exchange compatible invitation naming" ), gBox );
+  //TODO: when string freeze is over, break up this tooltip into 3 or 4 lines
+  // see bug 164970
+  //  mExchangeCompatibleInvitations->setToolTip( i18n( "Microsoft Outlook, when used in combination with a Microsoft Exchange server,\nhas a problem understanding standards-compliant groupware e-mail.\nEnable this option on to send groupware invitations in a way that Microsoft Exchange understands." ) );
   mExchangeCompatibleInvitations->setToolTip( i18n( "Microsoft Outlook, when used in combination with a Microsoft Exchange server, has a problem understanding standards-compliant groupware e-mail. Turn this option on to send groupware invitations in a way that Microsoft Exchange understands." ) );
   mExchangeCompatibleInvitations->setWhatsThis( i18n( GlobalSettings::self()->
            exchangeCompatibleInvitationsItem()->whatsThis().toUtf8() ) );
