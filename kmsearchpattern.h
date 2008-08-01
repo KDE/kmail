15823df8a4074a5b8581938e6ba9b359573379d0
Allen Winter <winter@kde.org>
2008-08-01 20:39:23 +0000

    Remove the ability to search for messages with "Old" status. Till and I thought this wasn't necessary in the search. Especially since KMail cannot mark a message as "Old" (Till says some other mailers can, however)
    No need to backport.
    
    svn path=/trunk/KDE/kdepim/; revision=840791

--- kmsearchpattern.h
+++ kmsearchpattern.h
@@ -236,7 +236,6 @@ namespace KMail {
     { I18N_NOOP2( "message status", "New" ),                    "mail-unread-new"     },
     { I18N_NOOP2( "message status", "Unread" ),                 "mail-unread"         },
     { I18N_NOOP2( "message status", "Read" ),                   "mail-read"           },
-    { I18N_NOOP2( "message status", "Old" ),                    0                     },
     { I18N_NOOP2( "message status", "Deleted" ),                "mail-deleted"        },
     { I18N_NOOP2( "message status", "Replied" ),                "mail-replied"        },
     { I18N_NOOP2( "message status", "Forwarded" ),              "mail-forwarded"      },
@@ -255,18 +254,17 @@ namespace KMail {
     StatusNew = 1,
     StatusUnread = 2,
     StatusRead = 3,
-    StatusOld = 4,
-    StatusDeleted = 5,
-    StatusReplied = 6,
-    StatusForwarded = 7,
-    StatusQueued = 8,
-    StatusSent = 9,
-    StatusWatched = 10,
-    StatusIgnored = 11,
-    StatusSpam = 12,
-    StatusHam = 13,
-    StatusToAct = 14,
-    StatusHasAttachment = 15
+    StatusDeleted = 4,
+    StatusReplied = 5,
+    StatusForwarded = 6,
+    StatusQueued = 7,
+    StatusSent = 8,
+    StatusWatched = 9,
+    StatusIgnored = 10,
+    StatusSpam = 11,
+    StatusHam = 12,
+    StatusToAct = 13,
+    StatusHasAttachment = 14
   };
 
   static const int StatusValueCount =
