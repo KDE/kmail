787dd3e0e673ec57adc1c184f1ed5714f3714fcc
Olivier Goffart <ogoffart@kde.org>
2008-09-27 19:52:46 +0000

    I promise that next time i will compile *before* commiting :-s sorry for that
    svn path=/trunk/KDE/kdepim/; revision=865497

--- kmacctimap.cpp
+++ kmacctimap.cpp
@@ -209,7 +209,7 @@ void KMAcctImap::ignoreJobsForMessage( KMMessage* msg )
 {
   if (!msg) return;
   QList<ImapJob*>::const_iterator it;
-  for ( it = mImapJobList.contBegin(); it != mImapJobList.constEnd() && (*it); ++it )
+  for ( it = mImapJobList.constBegin(); it != mImapJobList.constEnd() && (*it); ++it )
   {
     ImapJob *job = (*it);
     if ( job->msgList().first() == msg )
