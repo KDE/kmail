3ad3195527dff6c7e2e7b3201ecc8af5062297e9
Thomas McGuire <mcguire@kde.org>
2008-11-30 16:03:36 +0000

    Backport r888157 by mleupold from trunk to the 4.1 branch:
    Remove an assertion that might actually crash KMail (eg. by issuing a DBus setFolder call). Passing 0 to setCurrentFolder is perfectly valid.
    
    
    svn path=/branches/KDE/4.1/kdepim/; revision=890891

--- kmkernel.cpp
+++ kmkernel.cpp
@@ -2119,7 +2119,6 @@ void KMKernel::selectFolder( const QString &folderPath )
     folder = kmkernel->imapFolderMgr()->getFolderByURL( folderPath );
   if ( !folder )
     folder = kmkernel->dimapFolderMgr()->getFolderByURL( folderPath );
-  Q_ASSERT( folder );
 
   KMMainWidget *widget = getKMMainWidget();
   Q_ASSERT( widget );
