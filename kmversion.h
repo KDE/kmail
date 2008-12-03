b373687f58801109f1835360aa8fc9ed21cbfa88
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-12-03 13:13:26 +0000

    -- Update version numbers for today's release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=892014

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20081121.887369)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20081203.892009)"
 
 #endif /*kmversion_h*/
