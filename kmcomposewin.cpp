9c450ca62cbf122eb91336a27c90b532c66c5262
Thomas McGuire <mcguire@kde.org>
2008-12-14 14:06:41 +0000

    Merged revisions 894576 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.1/kdepim
    ........
      r894576 | winterz | 2008-12-08 22:50:03 +0100 (Mon, 08 Dec 2008) | 5 lines
      
      backport SVN commit 889794 by winterz:
      
      don't test for attachments if the attachWordsList is empty.
      even if we are configured to check for forgotten attachments.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=896798

--- kmcomposewin.cpp
+++ kmcomposewin.cpp
@@ -1933,6 +1933,9 @@ bool KMComposeWin::userForgotAttachment()
   }
 
   QStringList attachWordsList = GlobalSettings::self()->attachmentKeywords();
+  if ( attachWordsList.isEmpty() ) {
+    return false;
+  }
 
   if ( attachWordsList.isEmpty() ) {
     // default value (FIXME: this is duplicated in configuredialog.cpp)
@@ -1954,8 +1957,7 @@ bool KMComposeWin::userForgotAttachment()
   // check whether the subject contains one of the attachment key words
   // unless the message is a reply or a forwarded message
   QString subj = subject();
-  gotMatch = ( KMMessage::stripOffPrefixes( subj ) == subj )
-    && ( rx.indexIn( subj ) >= 0 );
+  gotMatch = ( KMMessage::stripOffPrefixes( subj ) == subj ) && ( rx.indexIn( subj ) >= 0 );
 
   if ( !gotMatch ) {
     // check whether the non-quoted text contains one of the attachment key
