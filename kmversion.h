bfb5f5dfb8de0b4d50569345ad3a42b51ebc92e3
Pradeepto Bhattacharya <pradeepto@kde.org>
2009-01-20 18:48:51 +0000

    -- Updating version numbers again after another around of merges. SVN_SILENT
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=914276

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.10.1 (enterprise4 0.20081212.896160)"
+#define KMAIL_VERSION "1.10.1 (enterprise4 0.20090120.914273)"
 
 #endif /*kmversion_h*/
