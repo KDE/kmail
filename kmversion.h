0a1f780d99a37e7d33cbb9154b0c7f5df0c6bfa1
Pradeepto Bhattacharya <pradeepto@kde.org>
2009-01-30 18:54:04 +0000

    Update version numbers. SVN_SILENT
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=918783

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.10.1 (enterprise4 0.20090120.914273)"
+#define KMAIL_VERSION "1.10.1 (enterprise4 0.20090130.918775)"
 
 #endif /*kmversion_h*/
