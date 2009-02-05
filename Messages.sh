bfe2d74f2eab0980e00499c518cbe6b64803e9b6
Pino Toscano <pino@kde.org>
2009-02-05 13:50:40 +0000

    try to extract manually .ui, .rc and .kcfg files
    svn path=/trunk/KDE/kdepim/; revision=921674

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 $PREPARETIPS > tips.cpp
+$EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg'` >> rc.cpp || exit 11
 $XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/kmail.pot
 rm -f tips.cpp
+rm -f rc.cpp
