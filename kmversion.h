37d57d1b915bda86206b00c398d411c8dfe6fc04
Till Adam <adam@kde.org>
2009-02-06 16:24:27 +0000

    Update versions for today's drop.
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=922270

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.10.1 (enterprise4 0.20090130.918775)"
+#define KMAIL_VERSION "1.10.1 (enterprise4 0.20090206.922266)"
 
 #endif /*kmversion_h*/
