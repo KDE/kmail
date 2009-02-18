3f6e7a42a653422c58fe48fe80b33a1d27118f0f
Laurent Montel <montel@kde.org>
2009-02-18 14:04:28 +0000

    Don't use deprecated signal
    svn path=/trunk/KDE/kdepim/; revision=927893

--- kmmsgpartdlg.cpp
+++ kmmsgpartdlg.cpp
@@ -88,7 +88,7 @@ KMMsgPartDialog::KMMsgPartDialog( const QString & caption,
                                << QString::fromLatin1("application/octet-stream")
                                << QString::fromLatin1("application/x-gunzip")
                                << QString::fromLatin1("application/zip") );
-  connect( mMimeType, SIGNAL(textChanged(const QString&)),
+  connect( mMimeType, SIGNAL(editTextChanged(const QString&)),
            this, SLOT(slotMimeTypeChanged(const QString&)) );
   glay->addWidget( mMimeType, 0, 1 );
 
@@ -333,7 +333,7 @@ void KMMsgPartDialog::slotMimeTypeChanged( const QString & mimeType ) {
   if ( !mt.isNull() )
     mIcon->setPixmap( KIconLoader::global()->loadMimeTypeIcon( mt->iconName(),
                       KIconLoader::Desktop ) );
-  else 
+  else
     mIcon->setPixmap( DesktopIcon("unknown") );
 }
 
