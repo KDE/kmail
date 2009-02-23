2805a4c92b94c77d705054fc7b43ce5277c16c94
Davide Bettio <davide.bettio@kdemail.net>
2009-02-23 20:32:37 +0000

    Fixing crash: we musn't delete mValidator.
    svn path=/trunk/KDE/kdepim/; revision=930563

--- accountdialog.cpp
+++ accountdialog.cpp
@@ -95,7 +95,7 @@ AccountDialog::AccountDialog( const QString & caption, KMAccount *account,
 {
   setCaption( caption );
   setButtons( Ok|Cancel|Help );
-  mValidator = new QRegExpValidator( QRegExp( "[A-Za-z0-9-_:.]*" ), 0 );
+  mValidator = new QRegExpValidator( QRegExp( "[A-Za-z0-9-_:.]*" ), this );
   setHelp("receiving-mail");
 
   KAccount::Type accountType = mAccount->type();
@@ -133,8 +133,6 @@ AccountDialog::AccountDialog( const QString & caption, KMAccount *account,
 
 AccountDialog::~AccountDialog()
 {
-  delete mValidator;
-  mValidator = 0;
   delete mServerTest;
   mServerTest = 0;
 }
