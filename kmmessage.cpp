19018821a6cfa034dd5771a40f06fe530a270f44
Jaime Torres Amate <jtamate@gmail.com>
2009-03-28 18:04:56 +0000

    Fixed all the issues found by Ingo Klöcker in http://reviewboard.kde.org/r/403/#review666
    svn path=/trunk/KDE/kdepim/; revision=946113

--- kmmessage.cpp
+++ kmmessage.cpp
@@ -619,21 +619,21 @@ static bool flushPart(QString &msg, QStringList &part,
 static QString stripSignature ( const QString & msg, bool clearSigned ) {
   // Following RFC 3676, only > before --
   // I prefer to not delete a SB instead of delete good mail content.
-  QRegExp sbDelimiterSearch = clearSigned ?
-      QRegExp( "(^|\n)[> ]*--\\s?\n" ) : QRegExp( "(^|\n)[> ]*-- \n" );
+  const QRegExp sbDelimiterSearch = clearSigned ?
+        QRegExp( "(^|\n)[> ]*--\\s?\n" ) : QRegExp( "(^|\n)[> ]*-- \n" );
   // The regular expresion to look for prefix change
-  QRegExp commonReplySearch = QRegExp( "[>]" );
+  const QRegExp commonReplySearch = QRegExp( "[ ]*>" );
 
   QString res = msg;
-  QString prefix; // the current prefix
-  QString line; // the line to check if is part of the SB
-  int posDeletingStart = -1;
-  int posNewLine = -1;
-  int posSignatureBlock = -1;
+  int posDeletingStart = 1; // to start looking at 0
 
-  // While there are SB delimiters
-  while ( ( posDeletingStart = res.indexOf( sbDelimiterSearch ) ) >= 0 )
+  // While there are SB delimiters (start looking just before the deleted SB)
+  while ( ( posDeletingStart = res.indexOf( sbDelimiterSearch , posDeletingStart -1 ) ) >= 0 )
   {
+    QString prefix; // the current prefix
+    QString line; // the line to check if is part of the SB
+    int posNewLine = -1;
+    int posSignatureBlock = -1;
     // Look for the SB begining
     posSignatureBlock = res.indexOf( '-', posDeletingStart );
     // The prefix before "-- "$
@@ -644,14 +644,17 @@ static QString stripSignature ( const QString & msg, bool clearSigned ) {
     // now go to the end of the SB
     while ( posNewLine < res.size() && posNewLine > 0 )
     {
-      line = res.mid( posNewLine, res.indexOf( '\n', posNewLine ) - posNewLine );
+      // handle the undefined case for mid ( x , -n ) where n>1
+      int nextPosNewLine = res.indexOf( '\n', posNewLine );
+      if ( nextPosNewLine < 0 ) nextPosNewLine = posNewLine - 1;
+      line = res.mid( posNewLine, nextPosNewLine - posNewLine );
 
       // check when the SB ends:
       // * does not starts with prefix or
       // * starts with prefix+(any substring of prefix)
-      if ( ( prefix.isEmpty() && line.indexOf( commonReplySearch ) < 0) ||
+      if ( ( prefix.isEmpty() && line.indexOf( commonReplySearch ) < 0 ) ||
            ( !prefix.isEmpty() && line.startsWith( prefix ) &&
-             !prefix.contains( line.mid( prefix.size(), 1 ) ) ) )
+             line.mid( prefix.size() ).indexOf( commonReplySearch ) < 0 ) )
       {
         posNewLine = res.indexOf( '\n', posNewLine ) + 1;
       }
