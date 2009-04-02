3e00e4fac2e247daae9b2e7faf2e533d4a6b62ed
Laurent Montel <montel@kde.org>
2009-04-02 09:25:45 +0000

    Fix attach file even if it's relative path as for ksendmail
    svn path=/trunk/KDE/kdepim/; revision=948109

--- kmkernel.cpp
+++ kmkernel.cpp
@@ -265,12 +265,19 @@ bool KMKernel::handleCommandLine( bool noArgsOpensReader )
   }
 
   const QStringList attachList = args->getOptionList("attach");
-  if (!attachList.isEmpty())
-  {
+  if (!attachList.isEmpty()) {
      mailto = true;
-     for ( QStringList::ConstIterator it = attachList.constBegin() ; it != attachList.constEnd() ; ++it )
-       if ( !(*it).isEmpty() )
-         attachURLs += KUrl( *it );
+     for ( QStringList::ConstIterator it = attachList.constBegin() ; it != attachList.constEnd() ; ++it ) {
+         if ( !(*it).isEmpty() ) {
+             KUrl url( *it );
+             if ( url.protocol().isEmpty() ) {
+                 const QString newUrl =  QDir::currentPath () + QDir::separator () + url.fileName();
+                 attachURLs.append( KUrl( newUrl ) );
+             }
+             else
+               attachURLs.append( url );
+         }
+     }
   }
 
   customHeaders = args->getOptionList("header");
