50ed72f25c4fe59ba1a1e9eb45b4d3c3f388ace1
Allen Winter <winter@kde.org>
2009-04-27 15:28:59 +0000

    Merged revisions 957559 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r957559 | mlaurent | 2009-04-22 09:02:54 -0400 (Wed, 22 Apr 2009) | 4 lines
      
      Update menu action when we undo
      (for example "move all in trash" was not updated after an undo)
      Need to port to e4/4.2/trunk)
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=959938

--- kmmainwidget.cpp
+++ kmmainwidget.cpp
@@ -1788,6 +1788,7 @@ void KMMainWidget::slotUndo()
 {
     mHeaders->undo();
     updateMessageActions();
+    updateFolderMenu();
 }
 
 //-----------------------------------------------------------------------------
