9d72baa51636c31a9bd1561ba754edb3a280531f
Laurent Montel <montel@kde.org>
2009-04-29 10:14:59 +0000

    Minor optimization not send "emit changed()" if we don't add new string
    svn path=/branches/kdepim/enterprise/kdepim/; revision=960937

--- simplestringlisteditor.cpp
+++ simplestringlisteditor.cpp
@@ -207,9 +207,10 @@ void SimpleStringListEditor::slotAdd() {
 					    &ok, this );
   // let the user verify the string before adding
   emit aboutToAdd( newEntry );
-  if ( ok && !newEntry.isEmpty() )
-    mListBox->insertItem( newEntry );
-  emit changed();
+  if ( ok && !newEntry.isEmpty() ) {
+      mListBox->insertItem( newEntry );
+      emit changed();
+  }
 }
 
 void SimpleStringListEditor::slotRemove() {
