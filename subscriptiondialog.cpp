d785746cc5b48513c79f61d998b43f0a48b2083d
Thomas McGuire <mcguire@kde.org>
2009-05-06 13:13:00 +0000

    Backport r961649 by tmcguire from trunk to the 4.2 branch:
    Merged revisions 961010 via svnmerge from 
    svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepim
    
    ........
      r961010 | tmcguire | 2009-04-29 15:18:07 +0200 (Wed, 29 Apr 2009) | 5 lines
      
      Don't show a strange empty root item in the subscription dialog.
      
      Thank you trolls for changing the default behavior of split() between
      Qt3 and Qt4, those are quite fun bugs to find ;)
    ........
    
    
    svn path=/branches/KDE/4.2/kdepim/; revision=964279

--- subscriptiondialog.cpp
+++ subscriptiondialog.cpp
@@ -127,7 +127,7 @@ void SubscriptionDialogBase::createListViewItem( int i )
     // the parent is not available and it's no root-item
     // this happens when the folders do not arrive in hierarchical order
     // so we create each parent in advance
-    QStringList folders = parentPath.split(mDelimiter);
+    QStringList folders = parentPath.split( mDelimiter, QString::SkipEmptyParts );
     uint i = 0;
     for ( QStringList::Iterator it = folders.begin(); it != folders.end(); ++it )
     {
@@ -158,10 +158,12 @@ void SubscriptionDialogBase::createListViewItem( int i )
       // as these items are "dummies" we create them non-checkable
       if (!item)
       {
-        if (parent)
+        if (parent) {
           item = new GroupItem(parent, info, this, false);
-        else
+        }
+        else {
           item = new GroupItem(folderTree(), info, this, false);
+        }
         mItemDict.insert(info.path, item);
       }
 
@@ -179,10 +181,12 @@ void SubscriptionDialogBase::createListViewItem( int i )
   // only checkable when the folder is selectable
   bool checkable = ( mFolderMimeTypes[i] == "inode/directory" ) ? false : true;
   // create a new item
-  if (parent)
+  if ( parent ) {
     item = new GroupItem(parent, info, this, checkable);
-  else
+  }
+  else {
     item = new GroupItem(folderTree(), info, this, checkable);
+  }
 
   if (oldItem) // remove old item
     mItemDict.remove(info.path);
