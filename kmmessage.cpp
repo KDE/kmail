9d9a42693785bba017283aa783845219a80cb707
Thomas McGuire <mcguire@kde.org>
2009-07-15 22:20:11 +0000

    Backport r992136 by tmcguire from trunk to the 4.3 branch:
    Fix sending of UTF-16 messages: Don't try to read the header fields as UTF-16, which
    corrupts them.
    
    Also, make the source viewer really show the source, don't try to encode it.
    
    kolab/issue3707
    
    
    svn path=/branches/KDE/4.3/kdepim/; revision=997519

--- kmmessage.cpp
+++ kmmessage.cpp
@@ -3358,8 +3358,10 @@ const QStringList &KMMessage::preferredCharsets()
 #ifndef NDEBUG
 void KMMessage::dump( DwEntity *entity, int level )
 {
-  if ( !entity )
+  if ( !entity ) {
     entity = mMsg;
+    entity->Assemble();
+  }
 
   QString spaces;
   for ( int i = 1; i <= level; i++ )
