1ba623b76f6dc8700313a38110729726f84c14d9
Allen Winter <winter@kde.org>
2009-07-16 13:42:55 +0000

    Backport r991462 by pokrzywka from trunk to the 4.3 branch:
    fixed attachment saving on windows: use QUrl::toLocalFile() (which returns "C:/foo") instead of QUrl::path() (which returns "/C:/foo")
    
    svn path=/branches/KDE/4.3/kdepim/; revision=997783

--- kmcommands.cpp
+++ kmcommands.cpp
@@ -2607,6 +2607,7 @@ KMCommand::Result KMSaveAttachmentsCommand::saveItem( partNode *node,
       size_t size = cstr.size();
       if ( dataNode->msgPart().type() == DwMime::kTypeText ) {
         // convert CRLF to LF before writing text attachments to disk
+        // PENDING (romain) disable on Windows ?
         size = KMail::Util::crlf2lf( data.data(), size );
       }
       data.resize( size );
@@ -2618,7 +2619,7 @@ KMCommand::Result KMSaveAttachmentsCommand::saveItem( partNode *node,
   if ( url.isLocalFile() )
   {
     // save directly
-    file.setFileName( url.path() );
+    file.setFileName( url.toLocalFile() );
     if ( !file.open( QIODevice::WriteOnly ) )
     {
       KMessageBox::error( parentWidget(),
