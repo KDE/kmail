edaa771cf343852043f1f91d81bee5eefd65ff84
Thomas McGuire <mcguire@kde.org>
2009-07-26 22:35:50 +0000

    Also support multipart/alternative with text/plain and multipart/related as children, instead of the usual text/plain and text/html.
    This fixes that the content of some Newsletters with images was rendered twice, bot the HTML and
    the plain text part was shown.
    
    BUG: 187989
    
    svn path=/trunk/KDE/kdepim/; revision=1002770

--- objecttreeparser.cpp
+++ objecttreeparser.cpp
@@ -1267,6 +1267,13 @@ namespace KMail {
 
     partNode * dataHtml = child->findType( DwMime::kTypeText,
                                            DwMime::kSubtypeHtml, false, true );
+    if ( !dataHtml ) {
+      // If we didn't find the HTML part as the first child of the multipart/alternative, it might
+      // be that this is a HTML message with images, and text/plain and multipart/related are the
+      // immediate children of this multipart/alternative node.
+      // In this case, the HTML node is a child of multipart/related.
+      dataHtml = child->findType( DwMime::kTypeMultipart, DwMime::kSubtypeRelated, false, true );
+    }
     partNode * dataPlain = child->findType( DwMime::kTypeText,
                                             DwMime::kSubtypePlain, false, true );
 
