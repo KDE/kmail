1ea5492c4b8e51bc3e14024689d83d6320d84428
Martin Koller <kollix@aon.at>
2009-08-05 18:28:18 +0000

    fix small optical glitch with margin inside tab
    svn path=/trunk/KDE/kdepim/; revision=1007413

--- kmfilterdlg.cpp
+++ kmfilterdlg.cpp
@@ -168,14 +168,14 @@ KMFilterDlg::KMFilterDlg(QWidget* parent, bool popFilter, bool createDummyFilter
     hbl = new QHBoxLayout( page1 );
     hbl->setObjectName( "kmfd_hbl" );
     hbl->setSpacing( spacingHint() );
-    hbl->setMargin( 0 );
+    hbl->setMargin( marginHint() );
 
     page2 = new QWidget( tabWidget );
     tabWidget->addTab( page2, i18nc("Advanced mail filter settings.","Advanced") );
     vbl2 = new QVBoxLayout( page2 );
     vbl2->setObjectName( "kmfd_vbl2" );
     vbl2->setSpacing( spacingHint() );
-    vbl2->setMargin( 0 );
+    vbl2->setMargin( marginHint() );
   }
 
   QVBoxLayout *vbl = new QVBoxLayout();
