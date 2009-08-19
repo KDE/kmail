111364d955d1f014dd54223592cbbf39effd21ef
Thomas McGuire <mcguire@kde.org>
2009-08-19 13:50:00 +0000

    - Give the completion sources a weight as well - Use the weight of the completion source to correctly sort the results in the popup - Make sure completion weights are correctly updated when changing the order
    The code is not pretty, but hey, this is addresseelineedit, after all...
    
    All this should finally fix kolab/issue3814.
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1013301

--- kmlineeditspell.cpp
+++ kmlineeditspell.cpp
@@ -156,7 +156,6 @@ void KMLineEdit::editRecentAddresses()
 //-----------------------------------------------------------------------------
 void KMLineEdit::loadContacts()
 {
-  // was: KABC::AddressLineEdit::loadAddresses()
   AddresseeLineEdit::loadContacts();
 
   if ( GlobalSettings::self()->showRecentAddressesInComposer() ){
@@ -165,7 +164,8 @@ void KMLineEdit::loadContacts()
         KRecentAddress::RecentAddresses::self( KMKernel::config() )->addresses();
       QStringList::Iterator it = recent.begin();
       QString name, email;
-      int idx = addCompletionSource( i18n( "Recent Addresses" ) );
+      // FIXME: Make the 120 configureable. This is also hardcoded somewhere else!
+      int idx = addCompletionSource( i18n( "Recent Addresses" ), 120 );
       for ( ; it != recent.end(); ++it ) {
         KABC::Addressee addr;
         KPIM::getNameAndMail(*it, name, email);
