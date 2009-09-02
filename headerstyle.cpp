9d3d0f94e1374032e32836b86333fc771e685819
Allen Winter <winter@kde.org>
2009-09-02 13:59:00 +0000

    don't print "CC:" and "BCC:" in the enterprise header. MERGE: e4,trunk,4.3
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1018982

--- headerstyle.cpp
+++ headerstyle.cpp
@@ -794,10 +794,10 @@ namespace KMail {
 	    headerParts << KMMessage::emailAddrAsAnchor( message->to(), false, linkColor );
 
 	if ( strategy->showHeader( "cc" ) && !message->cc().isEmpty() )
-	    headerParts << i18n("CC: ") + KMMessage::emailAddrAsAnchor( message->cc(), true, linkColor );
+	    headerParts << KMMessage::emailAddrAsAnchor( message->cc(), true, linkColor );
 
 	if ( strategy->showHeader( "bcc" ) && !message->bcc().isEmpty() )
-	    headerParts << i18n("BCC: ") + KMMessage::emailAddrAsAnchor( message->bcc(), true, linkColor );
+	    headerParts << KMMessage::emailAddrAsAnchor( message->bcc(), true, linkColor );
 
 	// remove all empty (modulo whitespace) entries and joins them via ", \n"
 	QString headerPart = " " + headerParts.grep( QRegExp( "\\S" ) ).join( ", " );
