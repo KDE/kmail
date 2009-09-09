d02b36e12070dc7cb60f998c97eb14cb97e5f580
Thomas McGuire <mcguire@kde.org>
2009-09-09 16:34:35 +0000

    Merged revisions 1020261 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.3/kdepim
    ........
      r1020261 | mkoller | 2009-09-05 23:30:19 +0200 (Sat, 05 Sep 2009) | 6 lines
      
      backport of bugfix for #196053
      Fix crash in precommand execution (hopefully).
      The crash was probably due to the eventloop object got destroyed in its own loop.
      catch 0-pointer instead of assert. Happens when kmail exits while precommand runs
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1021604

--- kmaccount.cpp
+++ kmaccount.cpp
@@ -395,7 +395,6 @@ bool KMAccount::runPrecommand(const QString &precommand)
           SLOT(precommandExited(bool)));
 
   kDebug(5006) << "Running precommand" << precommand;
-  mPrecommandEventLoop = new QEventLoop();
   if ( !precommandProcess.start() )
     return false;
 
@@ -403,9 +402,10 @@ bool KMAccount::runPrecommand(const QString &precommand)
   // the precommand is running (which may take a while).
   // The exec call will block until the event loop is exited, which happens in
   // precommandExited().
-  if ( mPrecommandEventLoop ) { // yes it can be 0 due to races with precommandProcess.start()
-    mPrecommandEventLoop->exec();
-  }
+  mPrecommandEventLoop = new QEventLoop();
+  mPrecommandEventLoop->exec();
+  delete mPrecommandEventLoop;
+  mPrecommandEventLoop = 0;
 
   return mPrecommandSuccess;
 }
@@ -413,18 +413,12 @@ bool KMAccount::runPrecommand(const QString &precommand)
 //-----------------------------------------------------------------------------
 void KMAccount::precommandExited(bool success)
 {
-  Q_ASSERT( mPrecommandEventLoop != 0 );
   mPrecommandSuccess = success;
-
-  // Exit and delete the event loop. This makes sure the execution continues
-  // in runPrecommand(), where the event loop was entered.
-  mPrecommandEventLoop->exit();
-
-  // Use deleteLater, because we are called inside of this very eventloop
-  mPrecommandEventLoop->deleteLater();
-  mPrecommandEventLoop = 0;
+  if ( mPrecommandEventLoop )  // don't crash when kmail exits while mPrecommandEventLoop runs
+    mPrecommandEventLoop->exit();
 }
 
+//-----------------------------------------------------------------------------
 void KMAccount::slotIdentitiesChanged()
 {
   // Fall back to the default identity if the one used currently is invalid
