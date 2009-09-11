b089a9b4b4ff87aca4afc4fa0186e09dd1033885
Thomas McGuire <mcguire@kde.org>
2009-09-11 13:01:49 +0000

    SVN_SILENT Update version numbers for today's release.
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1022319

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.10.1 (enterprise4 0.20090907.1020969)"
+#define KMAIL_VERSION "1.10.1 (enterprise4 0.20090911.1022317)"
 
 #endif /*kmversion_h*/
