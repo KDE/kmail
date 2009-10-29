c10a0c29783da7be3f3704690b358489aa6febc2
Allen Winter <winter@kde.org>
2009-10-29 22:33:52 +0000

    in KMHandleAttachmentCommand::atmSave(), make sure to pass a real parent widget to the KMSaveAttachmentsCommand instance, which in turn creates a file saveas dialog... ensuring we keep a sane widget stacking order. kolab/issue3481
    MERGE: e4,trunk,4.3
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1042499

--- kmcommands.cpp
+++ kmcommands.cpp
@@ -3157,7 +3157,7 @@ void KMHandleAttachmentCommand::atmSave()
   parts.append( mNode );
   // save, do not leave encoded
   KMSaveAttachmentsCommand *command =
-    new KMSaveAttachmentsCommand( 0, parts, mMsg, false );
+    new KMSaveAttachmentsCommand( parentWidget(), parts, mMsg, false );
   command->start();
 }
 
