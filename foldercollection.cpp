d9657cd6e776c27644e40b2135e7883c25bdfde6
Laurent Montel <montel@kde.org>
2009-11-02 11:56:07 +0000

    Move code here for the future
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1043813

--- foldercollection.cpp
+++ foldercollection.cpp
@@ -26,6 +26,7 @@
 #include <akonadi/collectiondeletejob.h>
 #include <kio/jobuidelegate.h>
 #include "kmcommands.h"
+//#include "expirejob.h"
 
 FolderCollection::FolderCollection( const Akonadi::Collection & col, bool writeconfig )
   : mCollection( col ),
@@ -475,4 +476,12 @@ void FolderCollection::slotDeletionCollectionResult( KJob * job )
 void FolderCollection::expireOldMessages( bool immediate )
 {
   kDebug() << "AKONADI PORT: port it  " << Q_FUNC_INFO;
+#if 0
+  KMail::ScheduledExpireTask* task = new KMail::ScheduledExpireTask(this, immediate);
+  kmkernel->jobScheduler()->registerTask( task );
+  if ( immediate ) {
+    // #82259: compact after expiring.
+    compact( CompactLater );
+  }
+#endif
 }
