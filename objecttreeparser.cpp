72f8cf0aaeba5b8d074178444ae75c36cc2b8340
Thomas McGuire <mcguire@kde.org>
2009-11-06 23:58:06 +0000

    Merged revisions 1041973,1041976 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.3/kdepim
    ........
      r1041973 | tmcguire | 2009-10-28 23:06:07 +0100 (Wed, 28 Oct 2009) | 5 lines
      
      Backport r1041850 by tmcguire from trunk to the 4.3 branch:
      
      Deal with broken HTML mails produced by apple mail.
    ........
      r1041976 | tmcguire | 2009-10-28 23:13:30 +0100 (Wed, 28 Oct 2009) | 9 lines
      
      Backport r1041974 by tmcguire from trunk to the 4.3 branch:
      
      Upps, don't hide attachments for broken mails from Apple Mail when prefering
      plain text.
      Now you see both plain text and HTML code, but there is no easy workaround for
      that, for those broken mails.
      When prefering HTML, they are displayed fine.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1045947

--- objecttreeparser.cpp
+++ objecttreeparser.cpp
@@ -1279,6 +1279,15 @@ namespace KMail {
       // immediate children of this multipart/alternative node.
       // In this case, the HTML node is a child of multipart/related.
       dataHtml = child->findType( DwMime::kTypeMultipart, DwMime::kSubtypeRelated, false, true );
+
+      // Still not found? Stupid apple mail actually puts the attachments inside of the
+      // multipart/alternative, which is wrong. Therefore we also have to look for multipart/mixed
+      // here.
+      // Do this only when prefering HTML mail, though, since otherwise the attachments are hidden
+      // when displaying plain text.
+      if ( !dataHtml && mReader->htmlMail() ) {
+        dataHtml = child->findType( DwMime::kTypeMultipart, DwMime::kSubtypeMixed, false, true );
+      }
     }
     partNode * dataPlain = child->findType( DwMime::kTypeText,
                                             DwMime::kSubtypePlain, false, true );
