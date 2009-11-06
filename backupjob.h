f235ffab6de41ae4b6d19a7f6ee75f71f31d3b5f
Allen Winter <winter@kde.org>
2009-11-06 22:11:50 +0000

    Merged revisions 1045770 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r1045770 | tmcguire | 2009-11-06 13:44:02 -0500 (Fri, 06 Nov 2009) | 2 lines
      
      Change copyright from myself to KDAB.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1045875

--- backupjob.h
+++ backupjob.h
@@ -1,4 +1,4 @@
-/* Copyright 2009 Thomas McGuire <mcguire@kde.org>
+/* Copyright 2009 Klarälvdalens Datakonsult AB
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
