cf87e7497b006dc55c0fd6aca837da00240047e9
Thomas McGuire <mcguire@kde.org>
2009-11-13 14:16:30 +0000

    Fix assert here as well, in this case the default mode is ok.
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1048548

--- importarchivedialog.cpp
+++ importarchivedialog.cpp
@@ -61,7 +61,6 @@ ImportArchiveDialog::ImportArchiveDialog( QWidget *parent, Qt::WFlags flags )
   QLabel *fileNameLabel = new QLabel( i18n( "Archive File:" ), mainWidget );
   mainLayout->addWidget( fileNameLabel, row, 0 );
   mUrlRequester = new KUrlRequester( mainWidget );
-  mUrlRequester->setMode( KFile::LocalOnly );
   mUrlRequester->setFilter( "*.tar *.zip *.tar.gz *.tar.bz2" );
   mainLayout->addWidget( mUrlRequester, row, 1 );
   row++;
