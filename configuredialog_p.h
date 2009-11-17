8f6f28f9031f171e3f5e62b95244157ba964e59d
Laurent Montel <montel@kde.org>
2009-11-17 14:16:28 +0000

    Quick search didn't work even in kmail -4.4 => disable it
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1050515

--- configuredialog_p.h
+++ configuredialog_p.h
@@ -396,7 +396,9 @@ private: // data
   QButtonGroup  *mReaderWindowModeGroup;
   QGroupBox     *mReaderWindowModeGroupBox;
   QCheckBox     *mFavoriteFolderViewCB;
+#if 0
   QCheckBox     *mFolderQuickSearchCB;
+#endif
   QButtonGroup  *mFolderToolTipsGroup;
   QGroupBox     *mFolderToolTipsGroupBox;
 };
