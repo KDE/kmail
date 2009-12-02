5036ca8fc798c2d07c85e6d5466306322be3aa04
Allen Winter <winter@kde.org>
2009-12-02 14:54:12 +0000

    add "*.*" to the filter on the "Save As" dialog MERGE: e4,trunk
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1057477

--- kmcommands.cpp
+++ kmcommands.cpp
@@ -768,7 +768,7 @@ static KURL subjectToUrl( const QString & subject )
                                            .replace( QDir::separator(), '_' )
                                            .replace( ": ", "_" )
                                            .replace( ':', '_' ),
-                                  "*.mbox" );
+                                  "*.mbox\n*.*" );
 }
 
 KMSaveMsgCommand::KMSaveMsgCommand( QWidget *parent, KMMessage * msg )
