5bee39aac2e77d63542a1d76a8dfddcbebdbd006
Thomas McGuire <mcguire@kde.org>
2009-12-04 10:31:27 +0000

    SVN_MERGE Merged revisions 1055312 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepim
    ................
      r1055312 | winterz | 2009-11-27 21:32:15 +0100 (Fri, 27 Nov 2009) | 14 lines
      
      Merged revisions 1050682 via svnmerge from 
      https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
      
      ........
        r1050682 | tmcguire | 2009-11-17 16:49:45 -0500 (Tue, 17 Nov 2009) | 7 lines
        
        Revert r529696 - "hotfix for the messagelist corruption".
        No idea if that bug is still there, but this "hotfix" prevents the index from being
        written out. And an outdated index can lead to funny things, like messages that are
        empty and can not be deleted, as observed in kolab/issue3098.
        
        Fixes kolab/issue3098, together with the previous commit.
      ........
    ................
    
    svn path=/trunk/KDE/kdepim/; revision=1058342

--- kmfolderimap.cpp
+++ kmfolderimap.cpp
@@ -107,15 +107,6 @@ KMFolderImap::~KMFolderImap()
 //-----------------------------------------------------------------------------
 void KMFolderImap::reallyDoClose()
 {
-  if ( isSelected() ) {
-    kWarning() <<"Trying to close the selected folder" << label()
-                   << "- ignoring!";
-//                   << "- ignoring!" << kBacktrace();
-    mOpenCount++;
-    //abort();
-    return;
-  }
-
   int idx = count();
   while ( --idx >= 0 ) {
     if ( mMsgList[idx]->isMessage() ) {
