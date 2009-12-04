a3757247270690f98cf08c60a297ffc72c55ebb4
Thomas McGuire <mcguire@kde.org>
2009-12-04 10:51:13 +0000

    SVN_MERGE Merged revisions 1057190 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepim
    ................
      r1057190 | winterz | 2009-12-01 18:09:24 +0100 (Tue, 01 Dec 2009) | 12 lines
      
      Merged revisions 1057186 via svnmerge from 
      https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
      
      ........
        r1057186 | winterz | 2009-12-01 12:04:35 -0500 (Tue, 01 Dec 2009) | 5 lines
        
        use an information dialog with a "do-not-ask-again" option
        instead of an error dialog when notifying the user that
        the attachment will be un-editable.
        kolab/issue3971
      ........
    ................
    
    svn path=/trunk/KDE/kdepim/; revision=1058362

--- editorwatcher.cpp
+++ editorwatcher.cpp
@@ -156,10 +156,11 @@ void EditorWatcher::checkEditDone()
   // nobody can edit that fast, we seem to be unable to detect
   // when the editor will be closed
   if ( mEditTime.elapsed() <= 3000 ) {
-    KMessageBox::error( mParentWidget,
+    KMessageBox::information( mParentWidget,
                         i18n( "KMail is unable to detect when the chosen editor is closed. "
                               "To avoid data loss, editing the attachment will be aborted." ),
-                        i18n( "Unable to edit attachment" ) );
+                        i18n( "Unable to edit attachment" ),
+                        "UnableToEditAttachment" );
   }
 
   emit editDone( this );
