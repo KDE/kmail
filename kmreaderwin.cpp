KDE/kmail

commit 28195145fc4c98ee48f6ff31ccd2bf4a9a98ed40

Changed HTML status bar in message window to be clickable and toggle between HTML and plain text when available. Implemented as an action in order to also be available as a configurable shortcut. http://reviewboard.kde.org/r/2017/
svn path=/trunk/KDE/kdepim/; revision=1050216

kmreaderwin.cpp
@@ -365,6 +365,8 @@ void KMReaderWin::createWidgets() {
   mBox->setFrameStyle( mMimePartTree->frameStyle() );
   mColorBar = new HtmlStatusBar( mBox );
   mColorBar->setObjectName( "mColorBar" );
+  connect( mColorBar, SIGNAL( clicked() ),
+           this, SLOT( slotToggleHtmlMode() ) );
   mViewer = new KHTMLPart( mBox );
   mViewer->setObjectName( "mViewer" );
   // Remove the shortcut for the selectAll action from khtml part. It's redefined to
@@ -385,7 +387,7 @@ void KMReaderWin::createWidgets() {
   } else {
     kDebug() << "Failed to find khtml's find action to remove it's shortcut";
   }
- 
+
   mSplitter->setStretchFactor( mSplitter->indexOf(mMimePartTree), 0 );
   mSplitter->setOpaqueResize( KGlobalSettings::opaqueResize() );
 }
@@ -429,6 +431,7 @@ KMReaderWin::KMReaderWin(QWidget *aParent,
     mToggleMimePartTreeAction( 0 ),
     mSelectEncodingAction( 0 ),
     mToggleFixFontAction( 0 ),
+    mToggleDisplayModeAction( 0 ),
     mCanStartDrag( false ),
     mHtmlWriter( 0 ),
     mSavedRelativePosition( 0 ),
@@ -698,6 +701,16 @@ void KMReaderWin::createActions()
   ac->addAction( "scroll_down_more", mScrollDownMoreAction );
   connect( mScrollDownMoreAction, SIGNAL( triggered( bool ) ),
            this, SLOT( slotScrollNext() ) );
+
+  //
+  // Actions not in menu
+  //
+
+  // Toggle HTML display mode.
+  mToggleDisplayModeAction = new KToggleAction( i18n( "Toggle HTML Display Mode" ), this );
+  ac->addAction( "toggle_html_display_mode", mToggleDisplayModeAction );
+  connect( mToggleDisplayModeAction, SIGNAL( triggered( bool ) ), SLOT( slotToggleHtmlMode() ) );
+  mToggleDisplayModeAction->setHelpText( i18n( "Toggle display mode between HTML and plain text" ) );
 }
 
 void KMReaderWin::setUseFixedFont( bool useFixedFont )
@@ -1518,12 +1531,9 @@ void KMReaderWin::displayMessage() {
 
   removeTempFiles();
 
-  mColorBar->setNeutralMode();
+  mColorBar->setNormalMode();
 
   parseMsg(msg);
-
-  if( mColorBar->isNeutral() )
-    mColorBar->setNormalMode();
 
   htmlWriter()->queue("</body></html>");
   htmlWriter()->flush();
@@ -2140,6 +2150,13 @@ void KMReaderWin::slotHandleAttachment( int choice )
 }
 
 //-----------------------------------------------------------------------------
+void KMReaderWin::slotToggleHtmlMode()
+{
+  setHtmlOverride( !htmlMail() );
+  update( true );
+}
+
+//-----------------------------------------------------------------------------
 void KMReaderWin::slotFind()
 {
   mViewer->findText();
@@ -2499,6 +2516,9 @@ void KMReaderWin::setHtmlOverride( bool override )
   if ( message() ) {
     message()->setDecodeHTML( htmlMail() );
   }
+
+  // keep toggle display mode action state in sync.
+  mToggleDisplayModeAction->setChecked( htmlMail() );
 }
 
 //-----------------------------------------------------------------------------

htmlstatusbar.cpp
@@ -44,6 +44,7 @@
 #include <QColor>
 #include <QString>
 #include <QLabel>
+#include <QMouseEvent>
 
 KMail::HtmlStatusBar::HtmlStatusBar( QWidget * parent, const char * name, Qt::WFlags f )
   : QLabel( parent, f ),
@@ -65,6 +66,7 @@ void KMail::HtmlStatusBar::update() {
   pal.setColor( foregroundRole(), fgColor() );
   setPalette( pal );
   setText( message() );
+  setToolTip( toolTip() );
 }
 
 void KMail::HtmlStatusBar::setNormalMode() {
@@ -75,8 +77,14 @@ void KMail::HtmlStatusBar::setHtmlMode() {
   setMode( Html );
 }
 
-void KMail::HtmlStatusBar::setNeutralMode() {
-  setMode( Neutral );
+void KMail::HtmlStatusBar::setMultipartPlainMode()
+{
+  setMode( MultipartPlain );
+}
+
+void KMail::HtmlStatusBar::setMultipartHtmlMode()
+{
+  setMode( MultipartHtml );
 }
 
 void KMail::HtmlStatusBar::setMode( Mode m ) {
@@ -86,19 +94,46 @@ void KMail::HtmlStatusBar::setMode( Mode m ) {
   update();
 }
 
+void KMail::HtmlStatusBar::mousePressEvent( QMouseEvent * event )
+{
+  if ( event->button() == Qt::LeftButton )
+  {
+    emit clicked();
+  }
+}
+
 QString KMail::HtmlStatusBar::message() const {
   switch ( mode() ) {
   case Html: // bold: "HTML Message"
+  case MultipartHtml:
     return i18n( "<qt><b><br />H<br />T<br />M<br />L<br /> "
                  "<br />M<br />e<br />s<br />s<br />a<br />g<br />e</b></qt>" );
   case Normal: // normal: "No HTML Message"
     return i18n( "<qt><br />N<br />o<br /> "
                  "<br />H<br />T<br />M<br />L<br /> "
                  "<br />M<br />e<br />s<br />s<br />a<br />g<br />e</qt>" );
+  case MultipartPlain: // normal: "Plain Message"
+    return i18n( "<qt><br />P<br />l<br />a<br />i<br />n<br /> "
+                 "<br />M<br />e<br />s<br />s<br />a<br />g<br />e<br /></qt>" );
   default:
-  case Neutral:
     return QString();
   }
+}
+
+QString KMail::HtmlStatusBar::toolTip() const
+{
+  switch ( mode() )
+  {
+    case Html:
+    case MultipartHtml:
+    case MultipartPlain:
+      return i18n( "Click to toggle between HTML and plain text." );
+    default:
+    case Normal:
+      break;
+  }
+
+  return QString();
 }
 
 QColor KMail::HtmlStatusBar::fgColor() const {
@@ -106,6 +141,7 @@ QColor KMail::HtmlStatusBar::fgColor() const {
   QColor defaultColor, color;
   switch ( mode() ) {
   case Html:
+  case MultipartHtml:
     defaultColor = Qt::white;
     color = defaultColor;
     if ( !GlobalSettings::self()->useDefaultColors() ) {
@@ -113,6 +149,7 @@ QColor KMail::HtmlStatusBar::fgColor() const {
     }
     return color;
   case Normal:
+  case MultipartPlain:
     defaultColor = Qt::black;
     color = defaultColor;
     if ( !GlobalSettings::self()->useDefaultColors() ) {
@@ -120,7 +157,6 @@ QColor KMail::HtmlStatusBar::fgColor() const {
     }
     return color;
   default:
-  case Neutral:
     return Qt::black;
   }
 }
@@ -131,6 +167,7 @@ QColor KMail::HtmlStatusBar::bgColor() const {
   QColor defaultColor, color;
   switch ( mode() ) {
   case Html:
+  case MultipartHtml:
     defaultColor = Qt::black;
     color = defaultColor;
     if ( !GlobalSettings::self()->useDefaultColors() ) {
@@ -138,6 +175,7 @@ QColor KMail::HtmlStatusBar::bgColor() const {
     }
     return color;
   case Normal:
+  case MultipartPlain:
     defaultColor = Qt::lightGray;
     color = defaultColor;
     if ( !GlobalSettings::self()->useDefaultColors() ) {
@@ -145,7 +183,6 @@ QColor KMail::HtmlStatusBar::bgColor() const {
     }
     return color;
   default:
-  case Neutral:
     return Qt::white;
   }
 }

htmlstatusbar.h
@@ -36,6 +36,7 @@
 
 class QString;
 class QColor;
+class QMouseEvent;
 
 namespace KMail {
 
@@ -43,17 +44,19 @@ namespace KMail {
    * @short The HTML statusbar widget for use with the reader.
    *
    * The HTML status bar is a small widget that acts as an indicator
-   * for the message content. It can be in one of two modes:
+   * for the message content. It can be in one of four modes:
    *
    * <dl>
    * <dt><code>Normal</code></dt>
    * <dd>Default. No HTML.</dd>
-   * <dt><code>Neutral</code></dt>
-   * <dd>Temporary value. Used while the real mode is undetermined.</dd>
    * <dt><code>Html</code></dt>
    * <dd>HTML content is being shown. Since HTML mails can mimic all sorts
    *     of KMail markup in the reader, this provides out-of-band information
    *     about the presence of (rendered) HTML.</dd>
+   * <dt><code>MultipartPlain</code></dt>
+   * <dd>Viewed as plain text with HTML part also available.</dd>
+   * <dt><code>MultipartHtml</code></dt>
+   * <dd>Viewed as Html with plain text part also available.</dd>
    * </dl>
    *
    * @author Ingo Kloecker <kloecker@kde.org>, Marc Mutz <mutz@kde.org>
@@ -67,14 +70,16 @@ namespace KMail {
     enum Mode {
       Normal,
       Html,
-      Neutral
+      MultipartPlain,
+      MultipartHtml
     };
 
     /** @return current mode. */
     Mode mode() const { return mMode ; }
     bool isHtml() const { return mode() == Html ; }
     bool isNormal() const { return mode() == Normal ; }
-    bool isNeutral() const { return mode() == Neutral ; }
+    bool isMultipartHtml() const { return mode() == MultipartHtml; }
+    bool isMultipartPlain() const { return mode() == MultipartPlain; }
 
     // Update the status bar, for example when the color scheme changed.
     void update();
@@ -84,13 +89,23 @@ namespace KMail {
     void setHtmlMode();
     /** Switch to "normal mode". */
     void setNormalMode();
-    /** Switch to "neutral" mode (currently == normal mode). */
-    void setNeutralMode();
+    /** Switch to "multipart html mode". */
+    void setMultipartHtmlMode();
+    /** Switch to "multipart plain mode". */
+    void setMultipartPlainMode();
     /** Switch to mode @p m */
     void setMode( Mode m );
 
+  signals:
+    /** The user has clicked the status bar. */
+    void clicked();
+
+  protected:
+    void mousePressEvent( QMouseEvent * event );
+
   private:
     QString message() const;
+    QString toolTip() const;
     QColor bgColor() const;
     QColor fgColor() const;
 

kmreaderwin.h
@@ -253,6 +253,7 @@ public:
   // Action to reply to a message
   // but action( "some_name" ) some name could be used instead.
   KToggleAction *toggleFixFontAction() { return mToggleFixFontAction; }
+  KToggleAction *toggleDisplayModeAction() { return mToggleDisplayModeAction; }
   KAction *mailToComposeAction() { return mMailToComposeAction; }
   KAction *mailToReplyAction() { return mMailToReplyAction; }
   KAction *mailToForwardAction() { return mMailToForwardAction; }
@@ -471,6 +472,9 @@ public slots:
    */
   void slotHandleAttachment( int action );
 
+  /** Toggle display mode between HTML and plain text. */
+  void slotToggleHtmlMode();
+
 protected slots:
   void slotCycleHeaderStyles();
   void slotBriefHeaders();
@@ -624,7 +628,7 @@ private:
       *mScrollUpAction, *mScrollDownAction, *mScrollUpMoreAction, *mScrollDownMoreAction,
       *mToggleMimePartTreeAction;
   KSelectAction *mSelectEncodingAction;
-  KToggleAction *mToggleFixFontAction;
+  KToggleAction *mToggleFixFontAction, *mToggleDisplayModeAction;
 
   KUrl mHoveredUrl;
   KUrl mClickedUrl;

objecttreeparser.cpp
@@ -1323,6 +1323,8 @@ namespace KMail {
       if ( dataPlain )
         dataPlain->setProcessed( true, false );
       stdChildHandling( dataHtml );
+      if ( mReader )
+        mReader->mColorBar->setMultipartHtmlMode();
       return true;
     }
 
@@ -1330,6 +1332,8 @@ namespace KMail {
       if ( dataHtml )
         dataHtml->setProcessed( true, false );
       stdChildHandling( dataPlain );
+      if ( mReader )
+        mReader->mColorBar->setMultipartPlainMode();
       return true;
     }
 

commit 2f633d42e6bca97a97f23938264bd0bc27cfd4f3

Fix crash when printing introduced in r1050216. Thanks to Thiago for discovering and analysing this.
svn path=/trunk/KDE/kdepim/; revision=1059809

kmreaderwin.cpp
@@ -2518,7 +2518,8 @@ void KMReaderWin::setHtmlOverride( bool override )
   }
 
   // keep toggle display mode action state in sync.
-  mToggleDisplayModeAction->setChecked( htmlMail() );
+  if ( mToggleDisplayModeAction )
+    mToggleDisplayModeAction->setChecked( htmlMail() );
 }
 
 //-----------------------------------------------------------------------------