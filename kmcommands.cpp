9278a105bdecc9016c4232de50ed73b8bfecfd08
Thomas McGuire <mcguire@kde.org>
2010-01-07 21:42:25 +0000

    SVN_MERGE Merged revisions 1063259,1063461 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepim
    ................
      r1063259 | winterz | 2009-12-17 16:58:27 +0100 (Thu, 17 Dec 2009) | 23 lines
      
      Merged revisions 1063249-1063250,1063257 via svnmerge from 
      https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
      
      ........
        r1063249 | winterz | 2009-12-17 10:19:25 -0500 (Thu, 17 Dec 2009) | 5 lines
        
        for the saveAs dialog prompt, use "*" instead of "*.*" for the all file filter.
        also use pretty names.
        
        MERGE: e4,trunk
      ........
        r1063250 | winterz | 2009-12-17 10:26:14 -0500 (Thu, 17 Dec 2009) | 3 lines
        
        add the word "non-hidden" to the saveAs dialog filter.
        MERGE: e4,trunk
      ........
        r1063257 | winterz | 2009-12-17 10:47:29 -0500 (Thu, 17 Dec 2009) | 4 lines
        
        i18n the filter for the saveAs dialog.
        and don't use the word "non-hidden".
        MERGE: e4,4.5 (new i18n string)
      ........
    ................
      r1063461 | tmcguire | 2009-12-18 17:01:49 +0100 (Fri, 18 Dec 2009) | 12 lines
      
      SVN_MERGE
      Merged revisions 1063451 via svnmerge from 
      svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
      
      ........
        r1063451 | tmcguire | 2009-12-18 16:40:56 +0100 (Fri, 18 Dec 2009) | 4 lines
        
        When the subject starts with a dot, change it to an underscore, to avoid hidden files.
        
        kolab/issue3805
      ........
    ................
    
    svn path=/trunk/KDE/kdepim/; revision=1071346

--- kmcommands.cpp
+++ kmcommands.cpp
@@ -797,7 +797,8 @@ static KUrl subjectToUrl( const QString &subject )
   if ( !fileName.endsWith( ".mbox" ) )
     fileName += ".mbox";
 
-  return KFileDialog::getSaveUrl( KUrl::fromPath( fileName ), "*.mbox\n*.*" );
+  const QString filter = i18n( "*.mbox|email messages (*.mbox)\n*|all files (*)" );
+  return KFileDialog::getSaveUrl( KUrl::fromPath( fileName ), filter );
 }
 
 KMSaveMsgCommand::KMSaveMsgCommand( QWidget *parent, KMMessage *msg )
@@ -2652,6 +2653,11 @@ QString KMCommand::cleanFileName( const QString &name )
   // better not use a dir-delimiter in a filename
   fileName.replace( '/', '_' );
   fileName.replace( '\\', '_' );
+
+  // Avoid hidden files be replacing the first dot
+  if ( fileName.startsWith( "." ) ) {
+    fileName[0] = '_';
+  }
 
   return fileName;
 }
