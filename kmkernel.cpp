56bd7a73bbad693d1d29f4584dc0535352d53a22
Laurent Montel <montel@kde.org>
2010-01-14 17:22:51 +0000

    Add a license to this file.
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1074744

--- kmkernel.cpp
+++ kmkernel.cpp
@@ -1,5 +1,20 @@
-/*  -*- mode: C++; c-file-style: "gnu" -*- */
+/* -*- mode: C++; c-file-style: "gnu" -*-
+  This file is part of KMail, the KDE mail client.
+  Copyright (c) 1997-2010 kdepim team
 
+  KMail is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  KMail is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #include "kmkernel.h"
 
 #include <config-kmail.h>
