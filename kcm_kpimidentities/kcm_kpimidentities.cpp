735771fa6078ea5b75fbf4678bf0c20812ec2c55
Volker Krause <vkrause@kde.org>
2010-05-20 10:21:21 +0000

    actually make it a real KCM
    svn path=/trunk/KDE/kdepim/kmail/; revision=1128810

---
+++ kcm_kpimidentities/kcm_kpimidentities.cpp
@@ -0,0 +1,36 @@
+/*   -*- mode: C++; c-file-style: "gnu" -*-
+ *   kmail: KDE mail client
+ *   Copyright (C) 2000 Espen Sand, espen@kde.org
+ *   Copyright (C) 2001-2003 Marc Mutz, mutz@kde.org
+ *   Contains code segments and ideas from earlier kmail dialog code.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "identitypage.h"
+#include <kcomponentdata.h>
+
+extern "C"
+{
+  KDE_EXPORT KCModule *create_kcm_kpimidentities( QWidget *parent, const char* )
+  {
+    KComponentData instance( "kcmkmail_config_identity" ); // keep in sync with kmail for now to reuse kmail translations until after the string freeze
+    KMail::IdentityPage *page = new KMail::IdentityPage( instance, parent );
+    page->setObjectName( "kcm_kpimidentities" );
+    return page;
+  }
+}
+
