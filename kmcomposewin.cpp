91afd0bc32972fe98c55221ffc49a00c9b1acbe8
Volker Krause <vkrause@kde.org>
2010-06-23 07:34:03 +0000

    Remove the 'NG' everywhere to make tests and mobile apps build again.
    svn path=/branches/work/komo/kdepim/; revision=1141668

--- kmcomposewin.cpp
+++ kmcomposewin.cpp
@@ -273,7 +273,7 @@ KMComposeWin::KMComposeWin( const KMime::Message::Ptr &aMsg, Composer::TemplateC
   connect( mEdtReplyTo, SIGNAL(completionModeChanged(KGlobalSettings::Completion)),
           SLOT(slotCompletionModeChanged(KGlobalSettings::Completion)) );
 
-  MessageComposer::RecipientsEditorNG* recipientsEditor = new MessageComposer::RecipientsEditorNG( mHeadersArea );
+  MessageComposer::RecipientsEditor* recipientsEditor = new MessageComposer::RecipientsEditor( mHeadersArea );
   recipientsEditor->setRecentAddressConfig( MessageComposer::MessageComposerSettings::self()->config() );
   connect( recipientsEditor,
            SIGNAL( completionModeChanged( KGlobalSettings::Completion ) ),
