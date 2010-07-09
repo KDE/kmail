43e46180ac6cf583860f889d688526c0fae44ef4
Tobias Koenig <tokoe@kde.org>
2010-07-09 10:44:52 +0000

    Merged revisions 1147155,1147162 via svnmerge from svn+ssh://tokoe@svn.kde.org/home/kde/branches/KDE/4.5/kdepim
    ........
      r1147155 | tokoe | 2010-07-07 15:26:26 +0200 (Wed, 07 Jul 2010) | 2 lines
      
      Fix selecting of folders when invoking 'Goto next Unread' action
    ........
      r1147162 | tokoe | 2010-07-07 15:45:51 +0200 (Wed, 07 Jul 2010) | 2 lines
      
      Do clear and set of the selection in one go
    ........
    
    svn path=/trunk/KDE/kdepim/kmail/; revision=1147903

--- foldertreeview.cpp
+++ foldertreeview.cpp
@@ -306,12 +306,21 @@ void FolderTreeView::slotHeaderContextMenuChangeIconSize( bool )
   writeConfig();
 }
 
-void FolderTreeView::selectModelIndex( const QModelIndex & index )
+void FolderTreeView::setCurrentModelIndex( const QModelIndex & index )
 {
   if ( index.isValid() ) {
     clearSelection();
     scrollTo( index );
     selectionModel()->setCurrentIndex( index, QItemSelectionModel::Rows );
+  }
+}
+
+void FolderTreeView::selectModelIndex( const QModelIndex & index )
+{
+  if ( index.isValid() ) {
+    scrollTo( index );
+    selectionModel()->select( index, QItemSelectionModel::Rows | QItemSelectionModel::Select |
+                                     QItemSelectionModel::Current | QItemSelectionModel::Clear );
   }
 }
 
@@ -328,7 +337,7 @@ void FolderTreeView::slotFocusNextFolder()
 
   if ( nextFolder.isValid() ) {
     expand( nextFolder );
-    selectModelIndex( nextFolder );
+    setCurrentModelIndex( nextFolder );
   }
 }
 
@@ -354,7 +363,7 @@ void FolderTreeView::slotFocusPrevFolder()
   const QModelIndex current = currentIndex();
   if ( current.isValid() ) {
     QModelIndex above = indexAbove( current );
-    selectModelIndex( above );
+    setCurrentModelIndex( above );
   }
 }
 
@@ -389,9 +398,9 @@ bool FolderTreeView::isUnreadFolder( const QModelIndex & current, QModelIndex &i
       index = indexAbove( current );
 
     if ( index.isValid() ) {
-      Akonadi::Collection collection = index.model()->data( current, Akonadi::EntityTreeModel::CollectionRole ).value<Akonadi::Collection>();
+      const Akonadi::Collection collection = index.model()->data( current, Akonadi::EntityTreeModel::CollectionRole ).value<Akonadi::Collection>();
       if ( collection.isValid() ) {
-        if ( collection.statistics().unreadCount()>0 ) {
+        if ( collection.statistics().unreadCount() > 0 ) {
           if ( !confirm ) {
             selectModelIndex( current );
             return true;
