3ff758050e351849f33af3f153825b709e47543d
Laurent Montel <montel@kde.org>
2010-08-25 12:32:20 +0000

    Add todo
    svn path=/trunk/KDE/kdepim/kmail/; revision=1167824

--- kmkernel.cpp
+++ kmkernel.cpp
@@ -942,6 +942,7 @@ void KMKernel::checkMailOnStartup()
       type.synchronize();
     }
 
+    /* TODO should not assume true but notify or ask the user. other Akonadi clients could expect these resources to continue working */
     if ( group.readEntry( "OfflineOnShutdown", true ) ) {
       if ( !type.isOnline() )
         type.setIsOnline( true );
@@ -1939,6 +1940,7 @@ void KMKernel::stopAgentInstance()
   const Akonadi::AgentInstance::List lst = KMail::Util::agentInstances();
   foreach( Akonadi::AgentInstance type, lst ) {
     KConfigGroup group( KMKernel::config(), resourceGroupPattern.arg( type.identifier() ) );
+    /* TODO should not assume true but notify or ask the user. other Akonadi clients could expect these resources to continue working */
     if ( group.readEntry( "OfflineOnShutdown", true ) )
       type.setIsOnline( false );
   }
