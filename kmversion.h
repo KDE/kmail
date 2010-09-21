700435b67765eb9d9a458c2ad27f32e745022060
Allen Winter <winter@kde.org>
2010-09-21 16:47:12 +0000

    SVN_SILENT Update version numbers for today's release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1177910

--- kmversion.h
+++ kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20100903.1171281)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20100921.1177909)"
 
 #endif /*kmversion_h*/
