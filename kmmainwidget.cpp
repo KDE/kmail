d28afcb3abe16a0294f05b3b51f5a262c6a8c345
Tobias Koenig <tokoe@kde.org>
2010-12-12 15:38:23 +0000

    Show 'Manage Local Subscription' menu entry only for IMAP folders
    CCBUG: 223486
    
    svn path=/trunk/KDE/kdepim/kmail/; revision=1205800

--- kmmainwidget.cpp
+++ kmmainwidget.cpp
@@ -3846,7 +3846,9 @@ void KMMainWidget::updateFolderMenu()
   mPreferHtmlLoadExtAction->setChecked( mHtmlLoadExtPref ? !mFolderHtmlLoadExtPref : mFolderHtmlLoadExtPref );
   mShowFolderShortcutDialogAction->setEnabled( !multiFolder && folderWithContent );
 
-  actionlist << akonadiStandardAction( Akonadi::StandardActionManager::ManageLocalSubscriptions );
+  if ( mCurrentFolder && mCurrentFolder->collection().resource().contains( IMAP_RESOURCE_IDENTIFIER ) )
+    actionlist << akonadiStandardAction( Akonadi::StandardActionManager::ManageLocalSubscriptions );
+
   mGUIClient->unplugActionList( QLatin1String( "collectionview_actionlist" ) );
   mGUIClient->plugActionList( QLatin1String( "collectionview_actionlist" ), actionlist );
 
