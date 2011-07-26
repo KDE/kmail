a281547b2fcf7bbd1adb2a2e7d8c0e66c673d331
Montel Laurent <montel@kde.org>
2011-07-26 16:25:10 +0200

    Fix compile

--- kmkernel.cpp
+++ kmkernel.cpp
@@ -47,7 +47,7 @@ using KMail::MailServiceImpl;
 #include "messagecomposersettings.h"
 #include "messagecomposer/messagehelper.h"
 #include "messagecomposer/messagecomposersettings.h"
-#include "messagecomposer/custommimeheader.h"
+#include "custommimeheader.h"
 
 #include "templateparser/templateparser.h"
 #include "templateparser/globalsettings_base.h"
