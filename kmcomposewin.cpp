6c50a1bca3c9f3dd511f3de1b066c8299d36891f
Montel Laurent <montel@kde.org>
2011-11-21 17:34:45 +0100

    Fix typo

--- kmcomposewin.cpp
+++ kmcomposewin.cpp
@@ -1803,7 +1803,7 @@ void KMComposeWin::autoSaveMessage(bool force)
   if ( isComposerModified() || force ) {
     applyComposerSetting( mComposerBase );
     mComposerBase->autoSaveMessage();
-    if ( force ) {
+    if ( !force ) {
       mWasModified = true;
       changeModifiedState( false );
     }
