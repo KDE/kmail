f35ea2e04c6b42420e4ae6df7b099951889674c9
Montel Laurent <montel@kde.org>
2012-02-03 22:54:14 +0100

    Use showclearbutton

--- configuredialog.cpp
+++ configuredialog.cpp
@@ -2960,6 +2960,7 @@ ComposerPageHeadersTab::ComposerPageHeadersTab( QWidget * parent )
 
   // "name" and "value" line edits and labels:
   mTagNameEdit = new KLineEdit( this );
+  mTagNameEdit->setClearButtonShown(true);
   mTagNameEdit->setEnabled( false );
   mTagNameLabel = new QLabel( i18nc("@label:textbox Name of the mime header.","&Name:"), this );
   mTagNameLabel->setBuddy( mTagNameEdit );
@@ -2970,6 +2971,7 @@ ComposerPageHeadersTab::ComposerPageHeadersTab( QWidget * parent )
            this, SLOT(slotMimeHeaderNameChanged(QString)) );
 
   mTagValueEdit = new KLineEdit( this );
+  mTagValueEdit->setClearButtonShown(true);
   mTagValueEdit->setEnabled( false );
   mTagValueLabel = new QLabel( i18n("&Value:"), this );
   mTagValueLabel->setBuddy( mTagValueEdit );
