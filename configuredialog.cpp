124057dcf7a9b9b3ee014fdc0d556a7b34d13705
Montel Laurent <montel@kde.org>
2012-10-11 13:26:15 +0200

    Fix layout

--- configuredialog.cpp
+++ configuredialog.cpp
@@ -1718,7 +1718,7 @@ AppearancePageMessageTagTab::AppearancePageMessageTagTab( QWidget * parent )
     //Extra VBoxLayout for stretchers around settings
     QVBoxLayout *tagsettinggrid = new QVBoxLayout();
     maingrid->addLayout( tagsettinggrid );
-    tagsettinggrid->addStretch( 10 );
+    //tagsettinggrid->addStretch( 10 );
 
     //Groupbox frame
     mTagSettingGroupBox = new QGroupBox( i18n("Ta&g Settings"),
