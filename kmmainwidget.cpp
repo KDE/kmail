4003e551637e56cec771e55c8a304bff868556a9
Montel Laurent <montel@kde.org>
2013-01-07 19:37:03 +0100

    Fix Bug 311688 - The "Export KMail Data" action uses icon "backup-mail",
    which is unavailable
    
    We don't have icons for it. So Use kmail for 4.10
    We will ask for new icon
    
    FIXED-IN: 4.10
    BUG: 311688

--- kmmainwidget.cpp
+++ kmmainwidget.cpp
@@ -3673,7 +3673,7 @@ void KMMainWidget::setupActions()
   }
 
   {
-      KAction *action = new KAction(KIcon("backup-mail"), i18n("&Export KMail Data..."), this);
+      KAction *action = new KAction(KIcon("kmail"), i18n("&Export KMail Data..."), this);
       actionCollection()->addAction("kmail_export_data", action );
       connect(action, SIGNAL(triggered(bool)), this, SLOT(slotExportData()));
   }
