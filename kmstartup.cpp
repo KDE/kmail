352827dfc29dede393bf72bf937dcbdd01b8b11d
Montel Laurent <montel@kde.org>
2013-05-31 08:06:48 +0200

    Was never added

--- kmstartup.cpp
+++ kmstartup.cpp
@@ -73,7 +73,8 @@ void checkConfigUpdates() {
     "3.5.4",
     "3.5.7-imap-flag-migration",
     "4.0-misc",
-    "4.2"
+    "4.2",
+    "4.4-akonadi"
   };
   static const int numUpdates = sizeof updates / sizeof *updates;
   // Warning: do not remove entries in the above array, or the update-level check below will break
