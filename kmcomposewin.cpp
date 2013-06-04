ff709335b0c730763f0cfcfe5da9933955f1b9f3
Allen Winter <allen.winter@kdab.com>
2013-06-04 15:36:04 -0400

    Put a toggle button for Rich Text editing on the composer. Notice change of terminology from "HTML" to "Rich Text".
    REVIEW: 110793

--- kmcomposewin.cpp
+++ kmcomposewin.cpp
@@ -1301,8 +1301,10 @@ void KMComposeWin::setupActions( void )
   connect( mComposerBase->editor(), SIGNAL(externalEditorClosed()), this, SLOT(slotExternalEditorClosed()));
   connect( mComposerBase->editor(), SIGNAL(externalEditorStarted()), this, SLOT(slotExternalEditorStarted()));
   //these are checkable!!!
-  markupAction = new KToggleAction( i18n("Formatting (HTML)"), this );
-  markupAction->setIconText( i18n("HTML") );
+  markupAction = new KToggleAction( i18n("Rich Text Formatting"), this );
+  markupAction->setIcon( KIcon( "text-html" ) );
+  markupAction->setIconText( i18n("Rich Text") );
+  markupAction->setToolTip( i18n( "Toggle Rich Text editing mode" ) );
   actionCollection()->addAction( "html", markupAction );
   connect( markupAction, SIGNAL(triggered(bool)), SLOT(slotToggleMarkup()) );
 
