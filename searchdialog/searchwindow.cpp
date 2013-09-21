8ef6092e42d54ac28c3b63b00388ad381716843f
Montel Laurent <montel@kde.org>
2013-09-21 12:05:59 +0200

    Remove it too

--- searchdialog/searchwindow.cpp
+++ searchdialog/searchwindow.cpp
@@ -431,13 +431,8 @@ void SearchWindow::slotSearch()
     searchPattern.purify();
 
     MailCommon::SearchPattern::SparqlQueryError queryError = MailCommon::SearchPattern::NoError;
-#ifdef AKONADI_USE_STRIGI_SEARCH
-    mQuery = searchPattern.asXesamQuery();
-    const QString queryLanguage = QLatin1String("XESAM");
-#else
     queryError = searchPattern.asSparqlQuery(mQuery, urls);
     const QString queryLanguage = QLatin1String("SPARQL");
-#endif
 
     switch(queryError) {
     case MailCommon::SearchPattern::NoError:
