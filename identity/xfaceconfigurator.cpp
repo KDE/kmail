0b3b0509ea65bb463333eacf25b25a98a6f5c480
Montel Laurent <montel@kde.org>
2013-10-09 09:00:30 +0200

    addSearchSupport->setSearchSupport

--- identity/xfaceconfigurator.cpp
+++ identity/xfaceconfigurator.cpp
@@ -186,7 +186,7 @@ namespace KMail {
     mTextEdit->setWhatsThis( i18n( "Use this field to enter an arbitrary X-Face string." ) );
     mTextEdit->setFont( KGlobalSettings::fixedFont() );
     mTextEdit->setWordWrapMode( QTextOption::WrapAnywhere);
-    mTextEdit->addSearchSupport(false);
+    mTextEdit->setSearchSupport(false);
     label2 = new QLabel( i18n("Examples are available at <a "
             "href=\"http://ace.home.xs4all.nl/X-Faces/\">"
             "http://ace.home.xs4all.nl/X-Faces/</a>."), page );
