cd7fa828471238e19aaeea904959b2b945090688
Montel Laurent <montel@kde.org>
2014-02-19 22:36:41 +0100

    Bye bye nepomuk

--- searchdialog/kmailsearchpatternedit.cpp
+++ searchdialog/kmailsearchpatternedit.cpp
@@ -21,7 +21,7 @@ using namespace KMail;
 KMailSearchPatternEdit::KMailSearchPatternEdit(QWidget *parent)
     : MailCommon::SearchPatternEdit(parent,
                                     (MailCommon::SearchPatternEdit::SearchPatternEditOptions)(SearchPatternEdit::NotShowDate|SearchPatternEdit::AbsoluteDate),
-                                    NepomukMode)
+                                    BalooMode)
 {
 }
 
