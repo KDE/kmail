676553303c6344f7e85d2eeff461bf1f16fca8a4
Montel Laurent <montel@kde.org>
2014-03-21 10:10:23 +0100

    Add mailmerge test gui app

---
+++ mailmerge/tests/mailmergewidgettest_gui.cpp
@@ -0,0 +1,38 @@
+/*
+  Copyright (c) 2014 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "kmail/mailmerge/mailmergewidget.h"
+
+#include <kdebug.h>
+#include <kapplication.h>
+#include <KCmdLineArgs>
+#include <KLocalizedString>
+
+int main (int argc, char **argv)
+{
+    KCmdLineArgs::init(argc, argv, "mailmergewidget_gui", 0, ki18n("MailMergeWidgetTest_Gui"),
+                       "1.0", ki18n("Test for mailmerge widget"));
+
+    KApplication app;
+
+    MailMergeWidget *w = new MailMergeWidget();
+    w->resize(800, 600);
+    w->show();
+    app.exec();
+    delete w;
+    return 0;
+}
