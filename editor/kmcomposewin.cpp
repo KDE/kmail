2ba611473da9f58e2ad487710b0bce500aa4fdd0
Montel Laurent <montel@kde.org>
2014-04-28 16:12:23 +0200

    Fix bug found by david when we create a new composer from a composer. Template was not changed when we change identity

--- editor/kmcomposewin.cpp
+++ editor/kmcomposewin.cpp
@@ -2444,7 +2444,8 @@ void KMComposeWin::slotNewComposer()
     KMime::Message::Ptr msg( new KMime::Message );
 
     MessageHelper::initHeader( msg, KMKernel::self()->identityManager() );
-    win = new KMComposeWin( msg, false, false );
+    win = new KMComposeWin( msg, false, false, KMail::Composer::New );
+    win->setCollectionForNewMessage(mCollectionForNewMessage);
     win->show();
 }
 
