fd39f5813e0b67def6080674a1810d46222afd4f
Sandro Knauß <knauss@kolabsys.com>
2014-07-22 17:30:48 +0200

    Add paramenter to ensure the composer to open
    REVIEW: 119461

--- kmkernel.cpp
+++ kmkernel.cpp
@@ -643,6 +643,28 @@ int KMKernel::openComposer(const QString &to, const QString &cc,
     return 1;
 }
 
+int KMKernel::openComposer(const QString &to, const QString &cc,
+                           const QString &bcc, const QString &subject,
+                           const QString &body, bool hidden,
+                           const QString &attachName,
+                           const QByteArray &attachCte,
+                           const QByteArray &attachData,
+                           const QByteArray &attachType,
+                           const QByteArray &attachSubType,
+                           const QByteArray &attachParamAttr,
+                           const QString &attachParamValue,
+                           const QByteArray &attachContDisp,
+                           const QByteArray &attachCharset,
+                           unsigned int identity)
+{
+    return openComposer(to, cc, bcc,
+                        subject, body, hidden,
+                        attachName, attachCte, attachData,
+                        attachType, attachSubType, attachParamAttr, attachParamValue,
+                        attachContDisp, attachCharset, identity, true);
+}
+
+
 int KMKernel::openComposer (const QString &to, const QString &cc,
                             const QString &bcc, const QString &subject,
                             const QString &body, bool hidden,
@@ -655,7 +677,8 @@ int KMKernel::openComposer (const QString &to, const QString &cc,
                             const QString &attachParamValue,
                             const QByteArray &attachContDisp,
                             const QByteArray &attachCharset,
-                            unsigned int identity )
+                            unsigned int identity,
+                            bool allowDefaultSend)
 {
     kDebug();
     KMail::Composer::TemplateContext context = KMail::Composer::New;
@@ -740,7 +763,7 @@ int KMKernel::openComposer (const QString &to, const QString &cc,
         cWin->disableForgottenAttachmentsCheck();
     }
 
-    if ( !hidden && !iCalAutoSend ) {
+    if ( !hidden && !(allowDefaultSend && iCalAutoSend)  ) {
         cWin->show();
         // Activate window - doing this instead of KWin::activateWindow(cWin->winId());
         // so that it also works when called from KMailApplication::newInstance()
