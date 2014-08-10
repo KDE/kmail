0059324e77cd3122506d4703e435ecaf54e0ce93
Montel Laurent <montel@kde.org>
2014-08-10 10:32:36 +0200

    Port to new connect api

--- mailmerge/attachmentlistwidget.cpp
+++ mailmerge/attachmentlistwidget.cpp
@@ -75,8 +75,8 @@ SelectAttachmentDialog::SelectAttachmentDialog(QWidget *parent)
     QPushButton *okButton = buttonBox->button(QDialogButtonBox::Ok);
     okButton->setDefault(true);
     okButton->setShortcut(Qt::CTRL | Qt::Key_Return);
-    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
-    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
 
     QWidget *w = new QWidget;
     QVBoxLayout *vbox = new QVBoxLayout;
--- mailmerge/mailmergedialog.cpp
+++ mailmerge/mailmergedialog.cpp
@@ -35,8 +35,8 @@ MailMergeDialog::MailMergeDialog(QWidget *parent)
     QPushButton *okButton = buttonBox->button(QDialogButtonBox::Ok);
     okButton->setDefault(true);
     okButton->setShortcut(Qt::CTRL | Qt::Key_Return);
-    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
-    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &MailMergeDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &MailMergeDialog::reject);
     mainLayout->addWidget(buttonBox);
 }
 
