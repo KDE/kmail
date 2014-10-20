3097f3709d5f01dacb478c56f6ddbeacd21e9459
Montel Laurent <montel@kde.org>
2014-10-20 09:37:18 +0200

    Add class to create followupreminder

---
+++ followupreminder/followupremindercreatejob.cpp
@@ -0,0 +1,63 @@
+/*
+  Copyright (c) 2014 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "followupremindercreatejob.h"
+
+
+FollowupReminderCreateJob::FollowupReminderCreateJob(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+FollowupReminderCreateJob::~FollowupReminderCreateJob()
+{
+
+}
+
+void FollowupReminderCreateJob::setFollowUpReminderDate(const QDate &date)
+{
+
+}
+
+void FollowupReminderCreateJob::setOriginalMessageItemId(Akonadi::Entity::Id value)
+{
+
+}
+
+void FollowupReminderCreateJob::setMessageId(const QString &messageId)
+{
+
+}
+
+void FollowupReminderCreateJob::setTo(const QString &to)
+{
+
+}
+
+void FollowupReminderCreateJob::setFollowUpReminderDate(const QDateTime &followUpReminderDate)
+{
+
+}
+
+void FollowupReminderCreateJob::setSubject(const QString &subject)
+{
+
+}
+
+
+
