cdb93388475a694074f56a805ee710c0c32a2496
Montel Laurent <montel@kde.org>
2014-11-30 07:37:35 +0100

    It was renamed to autotests => adapt extract messages

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $PREPARETIPS > tips.cpp
 $EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' | grep -v '/tests/'` -o $podir/kmail.pot
+$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' | grep -v '/autotests/'` -o $podir/kmail.pot
 rm -f tips.cpp
 rm -f rc.cpp
