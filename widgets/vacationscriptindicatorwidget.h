95b704e4f3bd6822297c70681502dedf87f92fd9
Montel Laurent <montel@kde.org>
2014-12-31 17:58:05 +0100

    Update copyright

--- widgets/vacationscriptindicatorwidget.h
+++ widgets/vacationscriptindicatorwidget.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2013, 2014 Montel Laurent <montel@kde.org>
+  Copyright (c) 2013-2015 Montel Laurent <montel@kde.org>
 
   This program is free software; you can redistribute it and/or modify it
   under the terms of the GNU General Public License, version 2, as
