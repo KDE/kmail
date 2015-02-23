c93a1ceb5985654240106be941c5276d252dbd4a
Montel Laurent <montel@kde.org>
2015-02-23 07:24:54 +0100

    Use addressbookwidget

--- mailmerge/widgets/mailmergewidget.h
+++ mailmerge/widgets/mailmergewidget.h
@@ -27,6 +27,7 @@ class KUrlRequester;
 namespace MailMerge {
 class CsvWidget;
 class AttachmentListWidget;
+class AddressBookWidget;
 class MailMergeWidget : public QWidget
 {
     Q_OBJECT
@@ -50,6 +51,7 @@ private:
     QStackedWidget *mStackedWidget;
     AttachmentListWidget *mAttachment;
     MailMerge::CsvWidget *mCsvWidget;
+    MailMerge::AddressBookWidget *mAddressbookWidget;
 };
 }
 #endif // MAILMERGEWIDGET_H
