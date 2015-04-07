b8da033a49d89bb7449a3525a7a444643d8da49c
Montel Laurent <montel@kde.org>
2015-04-07 13:10:03 +0200

    Fix crash when list of tag is empty

--- kmmainwidget.cpp
+++ kmmainwidget.cpp
@@ -1916,9 +1916,10 @@ void KMMainWidget::slotSelectMoreMessageTagList()
     TagSelectDialog dlg( this, selectedMessages.count(), selectedMessages.first() );
     if ( dlg.exec() ) {
         const Akonadi::Tag::List lst = dlg.selectedTag();
-
-        KMCommand *command = new KMSetTagCommand( lst, selectedMessages, KMSetTagCommand::CleanExistingAndAddNew );
-        command->start();
+        if (!lst.isEmpty()) {
+            KMCommand *command = new KMSetTagCommand( lst, selectedMessages, KMSetTagCommand::CleanExistingAndAddNew );
+            command->start();
+        }
     }
 }
 
