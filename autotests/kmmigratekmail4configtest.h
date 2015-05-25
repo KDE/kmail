00dbab2dc0667fc886a0a358249fe1ee3e5c8d81
Montel Laurent <montel@kde.org>
2015-05-25 09:29:51 +0200

    Create autotests

---
+++ autotests/kmmigratekmail4configtest.h
@@ -0,0 +1,32 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef KMMIGRATEKMAIL4CONFIGTEST_H
+#define KMMIGRATEKMAIL4CONFIGTEST_H
+
+#include <QObject>
+
+class KMMigrateKMail4ConfigTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit KMMigrateKMail4ConfigTest(QObject *parent = Q_NULLPTR);
+    ~KMMigrateKMail4ConfigTest();
+
+};
+
+#endif // KMMIGRATEKMAIL4CONFIGTEST_H
