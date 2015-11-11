6002ca16db8fd4b3fbd7117a25632c53ae3e1791
Montel Laurent <montel@kde.org>
2015-11-11 15:33:20 +0100

    Make sure that we defined topLevelCollection

--- job/fetchrecursivecollectionsjob.cpp
+++ job/fetchrecursivecollectionsjob.cpp
@@ -35,6 +35,12 @@ FetchRecursiveCollectionsJob::~FetchRecursiveCollectionsJob()
 
 void FetchRecursiveCollectionsJob::start()
 {
+    if (!mTopCollection.isValid()) {
+        qCWarning(KMAIL_LOG) << "Any collection is defined";
+        Q_EMIT fetchCollectionFailed();
+        deleteLater();
+        return;
+    }
     Akonadi::CollectionFetchJob *job = new Akonadi::CollectionFetchJob(mTopCollection, Akonadi::CollectionFetchJob::Recursive);
     connect(job, &Akonadi::CollectionFetchJob::result, this, &FetchRecursiveCollectionsJob::slotInitialCollectionFetchingDone);
 
