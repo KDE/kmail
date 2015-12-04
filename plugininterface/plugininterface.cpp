74a9f87e6e72020418c4824dce5b606642e1e9d3
Montel Laurent <montel@kde.org>
2015-12-04 08:23:32 +0100

    astyle kdelibs

--- plugininterface/plugininterface.cpp
+++ plugininterface/plugininterface.cpp
@@ -15,7 +15,6 @@
   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-
 #include "plugininterface.h"
 
 PluginInterface::PluginInterface(QObject *parent)
--- plugininterface/plugininterface.h
+++ plugininterface/plugininterface.h
@@ -15,7 +15,6 @@
   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-
 #ifndef PLUGININTERFACE_H
 #define PLUGININTERFACE_H
 
