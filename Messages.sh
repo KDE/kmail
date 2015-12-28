72cd5613876d76fa1ea307b35b954c47c048266c
Montel Laurent <montel@kde.org>
2015-12-28 14:40:33 +0100

    Remove grantlee_strings_extractor.pyc

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 ../grantlee-extractor-pot-scripts/extract_strings_ki18n.py `find -name \*.html` >> html.cpp
 $EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
 $XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/kmail.pot
-rm -f rc.cpp
+rm -f rc.cpp ../grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
