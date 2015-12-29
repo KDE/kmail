69e24d5aba2538b9928b2748a0f9499d5fce0811
l10n daemon script <scripty@kde.org>
2015-12-29 08:17:36 +0000

    SVN_SILENT made messages (after extraction)

---
+++ html.cpp
@@ -0,0 +1,17 @@
+// i18n: file: about/introduction_kmail.html
+i18nc("%1 is version", "Welcome to KMail %1");
+// i18n: file: about/introduction_kmail.html
+i18n("KMail is the email client by KDE. It is designed to be fully compatible with Internet mailing standards including IMAP, POP3 and SMTP.");
+// i18n: file: about/introduction_kmail.html
+i18n("KMail has many powerful features that are described in the <a href='help:/kmail/index.html'>documentation</a>.");
+// i18n: file: about/introduction_kmail.html
+i18n("Important changes since last version:");
+// i18n: file: about/introduction_kmail.html
+i18n("Some of the new features in this release of KMail include:");
+// i18n: file: about/introduction_kmail.html
+i18n("Please take a moment to fill in the KMail configuration panel at Settings->Configure KMail. You need to create at least a default identity and incoming as well as outgoing mail account.");
+// i18n: file: about/introduction_kmail.html
+i18n("We hope that you will enjoy KMail.");
+// i18n: file: about/introduction_kmail.html
+i18n("Thank you,<br/>The KMail Team");
+
