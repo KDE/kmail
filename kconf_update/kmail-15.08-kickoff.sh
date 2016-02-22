2bd9176c462b73b8f048e4dfd2913cbfcbaacb1a
Montel Laurent <montel@kde.org>
2016-02-22 20:08:41 +0100

    Use qtpaths directly

--- kconf_update/kmail-15.08-kickoff.sh
+++ kconf_update/kmail-15.08-kickoff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kickoffrcname=`kf5-config --path config --locate kickoffrc`
+kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
 if [ -f "$kickoffrcname" ]; then
    sed -i "s/\/KMail2.desktop/\/org.kde.kmail.desktop/" $kickoffrcname
 fi
