0ef045ea4de0650664a335c7960f60d776109e2f
Montel Laurent <montel@kde.org>
2016-04-21 13:39:13 +0200

    Fix header

--- kmail-version.h.cmake
+++ kmail-version.h.cmake
@@ -27,8 +27,8 @@
   build against older kdelibs releases.
 */
 
-#ifndef KDEPIM_VERSION_H
-#define KDEPIM_VERSION_H
+#ifndef KMAIL_VERSION_H
+#define KMAIL_VERSION_H
 
 
 #define KDEPIM_VERSION "@KDEPIM_VERSION@ (@WEBENGINE_TYPE@)"
