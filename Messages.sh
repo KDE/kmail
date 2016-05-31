4ee3c2f98ac2363b089aa89dbf683502f88949d6
Montel Laurent <montel@kde.org>
2016-05-31 06:47:13 +0200

    Use $EXTRACT_GRANTLEE_TEMPLATE_STRINGS

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-./grantlee-extractor-pot-scripts/extract_strings_ki18n.py `find -name \*.html` >> html.cpp
+$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find -name \*.html` >> html.cpp
 $EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
 $XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/kmail.pot
 rm -f rc.cpp html.cpp ./grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
