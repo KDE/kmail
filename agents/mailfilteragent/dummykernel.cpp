c1c061d6add56ae00cf1319421082cfccabd0d99
Montel Laurent <montel@kde.org>
2016-08-02 07:22:18 +0200

    Use read only identitymanager

--- agents/mailfilteragent/dummykernel.cpp
+++ agents/mailfilteragent/dummykernel.cpp
@@ -13,7 +13,7 @@ DummyKernel::DummyKernel(QObject *parent)
     : QObject(parent)
 {
     mMessageSender = new MessageComposer::AkonadiSender(this);
-    mIdentityManager = new KIdentityManagement::IdentityManager(false, this);
+    mIdentityManager = new KIdentityManagement::IdentityManager(true, this);
     Akonadi::Session *session = new Akonadi::Session("MailFilter Kernel ETM", this);
 
     mFolderCollectionMonitor = new MailCommon::FolderCollectionMonitor(session, this);
