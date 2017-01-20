9ea784e0d56aaebe2b7fdd95a0429b3ccc885f18
Burkhard Lück <lueck@hube-lueck.de>
2017-01-20 20:45:17 +0100

    Avoid duplicates messages in catalogs of repo kmail
    move Messages.sh inside src folder and remove superfluous option -ktranslate
    REVIEW:129854

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 $EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find -name \*.html` >> html.cpp
 $EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/kmail.pot
+$XGETTEXT  `find -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/kmail.pot
 rm -f rc.cpp html.cpp ./grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
