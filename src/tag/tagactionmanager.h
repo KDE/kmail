d9b4aee6820e1f5c839969064c4c30f93f2b7300
Montel Laurent <montel@kde.org>
2017-02-22 08:16:30 +0100

    Update copyright

--- src/tag/tagactionmanager.h
+++ src/tag/tagactionmanager.h
@@ -1,6 +1,6 @@
 /* Copyright 2010 Thomas McGuire <mcguire@kde.org>
 
-   Copyright 2011-2016 Laurent Montel <montel@kde.org>
+   Copyright 2011-2017 Laurent Montel <montel@kde.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
