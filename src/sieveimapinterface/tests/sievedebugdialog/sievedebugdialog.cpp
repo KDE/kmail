6e3ad2a013f4a2bff493a436ee49ddb3a316f7bd
Montel Laurent <montel@kde.org>
2017-03-03 07:09:38 +0100

    Move script

---
+++ src/sieveimapinterface/tests/sievedebugdialog/sievedebugdialog.cpp
@@ -0,0 +1,35 @@
+/*
+    Copyright (c) 2010 Volker Krause <vkrause@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include <KSieveUi/SieveDebugDialog>
+
+#include <QApplication>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    app.setQuitOnLastWindowClosed(false);
+    QApplication::setApplicationName(QStringLiteral("sievedebugdialog"));
+    QApplication::setApplicationVersion(QStringLiteral("1.0"));
+
+    KSieveUi::SieveDebugDialog *dlg = new KSieveUi::SieveDebugDialog;
+    dlg->exec();
+    delete dlg;
+    return 0;
+}
---
+++ src/sieveimapinterface/tests/vacation/main.cpp
@@ -0,0 +1,55 @@
+/*
+  Copyright (c) 2013-2017 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <KLocalizedString>
+#include <qapplication.h>
+#include <QCommandLineParser>
+#include <kaboutdata.h>
+#include <QStandardPaths>
+
+#include <KSieveUi/MultiImapVacationDialog>
+#include <KSieveUi/MultiImapVacationManager>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    KAboutData aboutData(QStringLiteral("vacationmultiscripttest"),
+                         i18n("VacationMultiScriptTest_Gui"),
+                         QStringLiteral("1.0"));
+
+    KAboutData::setApplicationData(aboutData);
+
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+    aboutData.setupCommandLine(&parser);
+
+    parser.process(app);
+    aboutData.processCommandLine(&parser);
+
+    app.setQuitOnLastWindowClosed(true);
+
+    KSieveUi::MultiImapVacationManager manager;
+    KSieveUi::MultiImapVacationDialog dlg(&manager);
+    QObject::connect(&dlg, &KSieveUi::MultiImapVacationDialog::okClicked, &app, &QApplication::quit);
+    QObject::connect(&dlg, &KSieveUi::MultiImapVacationDialog::cancelClicked, &app, &QApplication::quit);
+
+    dlg.show();
+
+    return app.exec();
+}
