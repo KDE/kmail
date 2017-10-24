78651887c7fdd54cb64056a53a6cbc74dd9b6041
Montel Laurent <montel@kde.org>
2017-10-24 13:35:05 +0200

    Add more autotest

--- src/editor/warningwidgets/attachmentmissingwarning.cpp
+++ src/editor/warningwidgets/attachmentmissingwarning.cpp
@@ -32,10 +32,12 @@ AttachmentMissingWarning::AttachmentMissingWarning(QWidget *parent)
     setWordWrap(true);
 
     QAction *action = new QAction(QIcon::fromTheme(QStringLiteral("mail-attachment")), i18n("&Attach file"), this);
+    action->setObjectName(QStringLiteral("attachfileaction"));
     connect(action, &QAction::triggered, this, &AttachmentMissingWarning::slotAttachFile);
     addAction(action);
 
     action = new QAction(QIcon::fromTheme(QStringLiteral("window-close")), i18n("&Remind me later"), this);
+    action->setObjectName(QStringLiteral("remindmelater"));
     connect(action, &QAction::triggered, this, &AttachmentMissingWarning::explicitlyClosed);
     addAction(action);
 }
---
+++ src/editor/warningwidgets/autotests/externaleditorwarningtest.cpp
@@ -0,0 +1,46 @@
+/*
+   Copyright (C) 2017 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "externaleditorwarningtest.h"
+#include "../externaleditorwarning.h"
+#include <QHBoxLayout>
+#include <QTest>
+
+QTEST_MAIN(ExternalEditorWarningTest)
+
+ExternalEditorWarningTest::ExternalEditorWarningTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+void ExternalEditorWarningTest::shouldHaveDefaultValue()
+{
+    QWidget *wid = new QWidget;
+    QHBoxLayout *layout = new QHBoxLayout(wid);
+    ExternalEditorWarning w;
+    layout->addWidget(&w);
+    wid->show();
+    QVERIFY(!w.isVisible());
+    //QVERIFY(w.isCloseButtonVisible());
+    QCOMPARE(w.messageType(), KMessageWidget::Information);
+    QVERIFY(w.wordWrap());
+    QVERIFY(!w.text().isEmpty());
+}
