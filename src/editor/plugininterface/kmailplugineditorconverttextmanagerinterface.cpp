446b2c9cf17315c9b64e403e974036f279d3d4b4
Laurent Montel <montel@kde.org>
2018-03-01 14:00:02 +0100

    Add mangerinterface

---
+++ src/editor/plugininterface/kmailplugineditorconverttextmanagerinterface.cpp
@@ -0,0 +1,56 @@
+/*
+   Copyright (C) 2018 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "kmailplugineditorconverttextmanagerinterface.h"
+#include "kmail_debug.h"
+#include <MessageComposer/PluginEditorConvertText>
+#include <MessageComposer/PluginEditorConvertTextManager>
+#include <MessageComposer/PluginEditorConvertTextInterface>
+
+KMailPluginEditorConvertTextManagerInterface::KMailPluginEditorConvertTextManagerInterface(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+KMailPluginEditorConvertTextManagerInterface::~KMailPluginEditorConvertTextManagerInterface()
+{
+
+}
+
+QWidget *KMailPluginEditorConvertTextManagerInterface::parentWidget() const
+{
+    return mParentWidget;
+}
+
+void KMailPluginEditorConvertTextManagerInterface::setParentWidget(QWidget *parentWidget)
+{
+    mParentWidget = parentWidget;
+}
+
+KActionCollection *KMailPluginEditorConvertTextManagerInterface::actionCollection() const
+{
+    return mActionCollection;
+}
+
+void KMailPluginEditorConvertTextManagerInterface::setActionCollection(KActionCollection *actionCollection)
+{
+    mActionCollection = actionCollection;
+}
---
+++ src/editor/plugininterface/kmailplugineditorconverttextmanagerinterface.h
@@ -0,0 +1,48 @@
+/*
+   Copyright (C) 2018 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KMAILPLUGINEDITORCONVERTTEXTMANAGERINTERFACE_H
+#define KMAILPLUGINEDITORCONVERTTEXTMANAGERINTERFACE_H
+
+#include <QObject>
+class QWidget;
+namespace MessageComposer {
+class PluginEditorConvertTextInterface;
+}
+class KActionCollection;
+class KMailPluginEditorConvertTextManagerInterface : public QObject
+{
+    Q_OBJECT
+public:
+    explicit KMailPluginEditorConvertTextManagerInterface(QObject *parent = nullptr);
+    ~KMailPluginEditorConvertTextManagerInterface();
+    QWidget *parentWidget() const;
+    void setParentWidget(QWidget *parentWidget);
+
+    KActionCollection *actionCollection() const;
+    void setActionCollection(KActionCollection *actionCollection);
+
+private:
+    Q_DISABLE_COPY(KMailPluginEditorConvertTextManagerInterface)
+    QList<MessageComposer::PluginEditorConvertTextInterface *> mListPluginInterface;
+    QWidget *mParentWidget = nullptr;
+    KActionCollection *mActionCollection = nullptr;
+};
+
+#endif // KMAILPLUGINEDITORCONVERTTEXTMANAGERINTERFACE_H
