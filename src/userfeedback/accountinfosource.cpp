066569a0231a31056a85cd332bffef5a31261d9b
Laurent Montel <montel@kde.org>
2020-02-04 13:47:15 +0100

    Add account info source

---
+++ src/userfeedback/accountinfosource.cpp
@@ -0,0 +1,46 @@
+/*
+   Copyright (C) 2020 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "accountinfosource.h"
+#include <QVariant>
+#include <KUserFeedback/Provider>
+#include <KLocalizedString>
+
+AccountInfoSource::AccountInfoSource()
+    : KUserFeedback::AbstractDataSource(QStringLiteral("account"), KUserFeedback::Provider::DetailedSystemInformation)
+{
+
+}
+
+//TODO add account type
+QString AccountInfoSource::name() const
+{
+    //TODO
+    return {};
+}
+
+QString AccountInfoSource::description() const
+{
+    return i18n("Account configurated in KMail (reveiver and sender).");
+}
+
+QVariant AccountInfoSource::data()
+{
+    return i18n("Account information");
+}
---
+++ src/userfeedback/accountinfosource.h
@@ -0,0 +1,36 @@
+/*
+   Copyright (C) 2020 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#ifndef ACCOUNTINFOSOURCE_H
+#define ACCOUNTINFOSOURCE_H
+#include <KUserFeedback/AbstractDataSource>
+
+class AccountInfoSource : public KUserFeedback::AbstractDataSource
+{
+public:
+    AccountInfoSource();
+
+    Q_REQUIRED_RESULT QString name() const override;
+    Q_REQUIRED_RESULT QString description() const override;
+
+    Q_REQUIRED_RESULT QVariant data() override;
+};
+
+#endif // ACCOUNTINFOSOURCE_H
