981490f350930ef3e539e3a3b9480a601bf7da8d
Laurent Montel <montel@kde.org>
2020-09-16 07:50:29 +0200

    Remove unused includes

--- src/tag/tagactionmanager.cpp
+++ src/tag/tagactionmanager.cpp
@@ -25,10 +25,6 @@
 
 #include <QPointer>
 
-#include <AkonadiCore/TagFetchJob>
-#include <AkonadiCore/TagFetchScope>
-#include <AkonadiCore/TagAttribute>
-
 using namespace KMail;
 
 static int s_numberMaxTag = 10;
