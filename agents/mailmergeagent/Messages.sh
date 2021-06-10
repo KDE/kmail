05b748c5227a2b1cbb3e9d43491dcd95dedb5fd4
Laurent Montel <montel@kde.org>
2021-06-10 13:28:11 +0200

    We don't have ui files here

--- agents/mailmergeagent/Messages.sh
+++ agents/mailmergeagent/Messages.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 # SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
 # SPDX-License-Identifier: BSD-3-Clause
-$EXTRACTRC ui/*.ui >> rc.cpp
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/' ` -o $podir/akonadi_mailmerge_agent.pot
-rm -f rc.cpp
