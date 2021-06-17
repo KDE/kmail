f50b570c64b4b96261d646e2dcf181da9aa5d5f7
Laurent Montel <montel@kde.org>
2021-06-17 08:06:36 +0200

    Remove commented code

--- src/kcm_kmail.cpp
+++ src/kcm_kmail.cpp
@@ -2,7 +2,7 @@
  *   kmail: KDE mail client
  *   SPDX-FileCopyrightText: 2000 Espen Sand <espen@kde.org>
  *   SPDX-FileCopyrightText: 2001-2003 Marc Mutz <mutz@kde.org>
- *   Contains code segments and ideas from earlier kmail dialog code.
+ *   SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
  *
  *   SPDX-License-Identifier: GPL-2.0-or-later
  *
@@ -11,10 +11,6 @@
 #include "configuredialog/configureaccountpage.h"
 #include "configuredialog/configureappearancepage.h"
 #include "configuredialog/configurecomposerpage.h"
-#include "configuredialog/configuredialog.h"
-#include "configuredialog/configuredialog_p.h"
 #include "configuredialog/configuremiscpage.h"
 #include "configuredialog/configurepluginpage.h"
 #include "configuredialog/configuresecuritypage.h"
-//#include "identity/identitypage.h"
-//#include <KCModule>
