d01bc697b82eb55c361d6f8caee05612aabcfadd
Ahmad Samir <a.samirh78@gmail.com>
2021-06-26 14:36:23 +0200

    Adjust KShortcutsDialog::showDialog() usage due to API changes
    GIT_SILENT

--- ktnef/src/ktnefmain.cpp
+++ ktnef/src/ktnefmain.cpp
@@ -166,7 +166,7 @@ void KTNEFMain::slotConfigureKeys()
 #if KXMLGUI_VERSION < QT_VERSION_CHECK(5, 84, 0)
     KShortcutsDialog::configure(actionCollection(), KShortcutsEditor::LetterShortcutsAllowed, this);
 #else
-    KShortcutsDialog::showDialog(actionCollection(), KShortcutsEditor::LetterShortcutsAllowed, /*isModal*/ true, this);
+    KShortcutsDialog::showDialog(actionCollection(), KShortcutsEditor::LetterShortcutsAllowed, this);
 #endif
 }
 
--- src/editor/kmcomposerwin.cpp
+++ src/editor/kmcomposerwin.cpp
@@ -3403,7 +3403,7 @@ void KMComposerWin::slotEditKeys()
 #if KXMLGUI_VERSION < QT_VERSION_CHECK(5, 84, 0)
     KShortcutsDialog::configure(actionCollection(), KShortcutsEditor::LetterShortcutsDisallowed);
 #else
-    KShortcutsDialog::showDialog(actionCollection(), KShortcutsEditor::LetterShortcutsAllowed, /*isModal*/ true, this);
+    KShortcutsDialog::showDialog(actionCollection(), KShortcutsEditor::LetterShortcutsAllowed, this);
 #endif
 }
 
