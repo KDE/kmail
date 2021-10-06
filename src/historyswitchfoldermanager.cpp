af839c1d35e38421a878e275c4cf0d7ba3e960e5
Laurent Montel <montel@kde.org>
2021-10-06 13:58:23 +0200

    Start to implement history switch folder. So it will more easy to switch
    back

---
+++ src/historyswitchfoldermanager.cpp
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "historyswitchfoldermanager.h"
+
+HistorySwitchFolderManager::HistorySwitchFolderManager(QObject *parent)
+    : QObject{parent}
+{
+}
+
+HistorySwitchFolderManager::~HistorySwitchFolderManager()
+{
+}
---
+++ src/historyswitchfoldermanager.h
@@ -0,0 +1,17 @@
+/*
+   SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class HistorySwitchFolderManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit HistorySwitchFolderManager(QObject *parent = nullptr);
+    ~HistorySwitchFolderManager() override;
+    // Add static method
+};
