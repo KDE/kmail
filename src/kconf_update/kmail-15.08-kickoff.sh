22cb31a593379de6b6ce4f37acc352f49a36fa42
Laurent Montel <montel@kde.org>
2021-10-15 06:54:00 +0200

    GIT_SILENT: remove old kconf_update

--- src/kconf_update/kmail-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/KMail2.desktop/\/org.kde.kmail.desktop/" $kickoffrcname
-fi
