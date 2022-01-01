c0b1bb92bc4cd19a10659c8430beb8434064f8bf
Laurent Montel <montel@kde.org>
2022-01-01 16:13:03 +0100

    GIT_SILENT: happy new year

--- agents/mailmergeagent/Messages.sh
+++ agents/mailmergeagent/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-# SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+# SPDX-FileCopyrightText: 2021-2022 Laurent Montel <montel@kde.org>
 # SPDX-License-Identifier: BSD-3-Clause
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/' ` -o $podir/akonadi_mailmerge_agent.pot
