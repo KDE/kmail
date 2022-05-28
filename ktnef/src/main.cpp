d010dac623060e4585093b65fb14b6a2b6009513
Laurent Montel <montel@kde.org>
2022-05-28 23:37:14 +0200

    It's enabled by default in qt6

--- ktnef/src/main.cpp
+++ ktnef/src/main.cpp
@@ -28,8 +28,8 @@ int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
-#endif
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+#endif
     QApplication app(argc, argv);
     KLocalizedString::setApplicationDomain("ktnef");
     KCrash::initialize();
