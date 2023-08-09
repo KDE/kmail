4ce2d09c1bf8a8706bf6d9deaef91a8b91631127
Laurent Montel <montel@kde.org>
2023-08-09 21:10:33 +0200

    Add autotest

---
+++ agents/archivemailagent/autotests/archivemailrangewidgettest.h
@@ -0,0 +1,19 @@
+/*
+   SPDX-FileCopyrightText: 2012-2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class ArchiveMailRangeWidgetTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ArchiveMailRangeWidgetTest(QObject *parent = nullptr);
+    ~ArchiveMailRangeWidgetTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
