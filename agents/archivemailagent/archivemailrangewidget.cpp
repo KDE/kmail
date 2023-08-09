7a6ce81cfb4958d58dd0808d6527ff85b60a6f31
Laurent Montel <montel@kde.org>
2023-08-09 21:10:53 +0200

    continue to implement range widget

--- agents/archivemailagent/archivemailrangewidget.cpp
+++ agents/archivemailagent/archivemailrangewidget.cpp
@@ -5,20 +5,26 @@
 */
 
 #include "archivemailrangewidget.h"
+#include "archivemailagent_debug.h"
 #include <KLocalizedString>
 #include <KTimeComboBox>
+#include <QCheckBox>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QTime>
 
 ArchiveMailRangeWidget::ArchiveMailRangeWidget(QWidget *parent)
     : QWidget{parent}
     , mStartRange(new KTimeComboBox(this))
     , mEndRange(new KTimeComboBox(this))
+    , mEnabled(new QCheckBox(i18n("Use Range"), this))
 {
     auto mainLayout = new QHBoxLayout(this);
     mainLayout->setObjectName(QStringLiteral("mainLayout"));
     mainLayout->setContentsMargins({});
-    mainLayout->addWidget(new QLabel(i18n("Range:"), this));
+
+    mEnabled->setObjectName(QStringLiteral("mEnabled"));
+    mainLayout->addWidget(mEnabled);
 
     mStartRange->setObjectName(QStringLiteral("mStartRange"));
     mStartRange->setTimeListInterval(60);
@@ -28,6 +34,43 @@ ArchiveMailRangeWidget::ArchiveMailRangeWidget(QWidget *parent)
 
     mainLayout->addWidget(mStartRange);
     mainLayout->addWidget(mEndRange);
+
+    connect(mEnabled, &QCheckBox::toggled, this, [this](bool enabled) {
+        mStartRange->setEnabled(enabled);
+        mEndRange->setEnabled(enabled);
+    });
 }
 
 ArchiveMailRangeWidget::~ArchiveMailRangeWidget() = default;
+
+bool ArchiveMailRangeWidget::isEnabled() const
+{
+    return mEnabled->isChecked();
+}
+
+void ArchiveMailRangeWidget::setEnabled(bool isEnabled)
+{
+    mEnabled->setChecked(isEnabled);
+}
+
+QList<int> ArchiveMailRangeWidget::range() const
+{
+    QList<int> timeRange;
+    timeRange.append(mStartRange->time().hour());
+    timeRange.append(mEndRange->time().hour());
+    return timeRange;
+}
+
+void ArchiveMailRangeWidget::setRange(const QList<int> &hours)
+{
+    if (hours.count() != 2) {
+        qCWarning(ARCHIVEMAILAGENT_LOG) << "Ranges is invalid " << hours;
+    } else {
+        const QTime startTime{hours.at(0), 0, 0};
+        mStartRange->setTime(startTime);
+        const QTime endTime{hours.at(1), 0, 0};
+        mStartRange->setTime(endTime);
+    }
+}
+
+#include "moc_archivemailrangewidget.cpp"
