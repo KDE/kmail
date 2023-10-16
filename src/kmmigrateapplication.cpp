f1786c947abb9c2b9f221651d2d42e240aab3b1d
Laurent Montel <montel@kde.org>
2023-10-16 13:47:55 +0200

    Remove unused kmmigrateapplication

--- src/kmmigrateapplication.cpp
+++
@@ -1,7 +0,0 @@
-/*
-   SPDX-FileCopyrightText: 2015-2023 Laurent Montel <montel@kde.org>
-
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
-
-#include "kmmigrateapplication.h"
--- src/kmmigrateapplication.h
+++
@@ -1,9 +0,0 @@
-/*
-   SPDX-FileCopyrightText: 2015-2023 Laurent Montel <montel@kde.org>
-
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
-
-#pragma once
-#include <QtGlobal>
-// QT6 TODO remove it
