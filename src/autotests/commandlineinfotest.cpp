9bd02d917329312d45675dfde29e7adbcf73f326
Laurent Montel <montel@kde.org>
2023-11-12 21:25:14 +0100

    Improve autotest

--- src/autotests/commandlineinfotest.cpp
+++ src/autotests/commandlineinfotest.cpp
@@ -17,5 +17,20 @@ CommandLineInfoTest::CommandLineInfoTest(QObject *parent)
 void CommandLineInfoTest::shouldHaveDefaultValues()
 {
     CommandLineInfo w;
-    // TODO
+    QVERIFY(w.customHeaders().isEmpty());
+    QVERIFY(w.attachURLs().isEmpty());
+    QVERIFY(w.to().isEmpty());
+    QVERIFY(w.cc().isEmpty());
+    QVERIFY(w.bcc().isEmpty());
+    QVERIFY(w.subject().isEmpty());
+    QVERIFY(w.body().isEmpty());
+    QVERIFY(w.inReplyTo().isEmpty());
+    QVERIFY(w.replyTo().isEmpty());
+    QVERIFY(w.identity().isEmpty());
+    QVERIFY(w.messageFile().isEmpty());
+    QVERIFY(!w.startInTray());
+    QVERIFY(!w.mailto());
+    QVERIFY(!w.checkMail());
+    QVERIFY(!w.viewOnly());
+    QVERIFY(!w.calledWithSession());
 }
