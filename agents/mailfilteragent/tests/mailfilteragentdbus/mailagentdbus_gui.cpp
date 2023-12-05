474ea3841e120017f49d6c64a6af7ea55d942d5a
Laurent Montel <montel@kde.org>
2023-12-05 07:56:47 +0100

    Add mailagentdbus_gui

---
+++ agents/mailfilteragent/tests/mailfilteragentdbus/mailagentdbus_gui.cpp
@@ -0,0 +1,31 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include <MailCommon/FilterLog>
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QStandardPaths>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.process(app);
+
+    // auto log = MailCommon::FilterLog::instance();
+    // log->setLogging(true);
+    // for (int i = 0; i < 50; ++i) {
+    //     log->add(QStringLiteral("Test %1").arg(i), MailCommon::FilterLog::AppliedAction);
+    // }
+
+    // auto dialog = new FilterLogDialog(nullptr);
+    // dialog->exec();
+    // delete dialog;
+    return 0;
+}
