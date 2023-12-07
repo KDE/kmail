fcc196fd4f9c7c00b1b102006b7c724bebc03594
Carl Schwan <carl@carlschwan.eu>
2023-12-07 02:34:11 +0100

    Remove broken code to setup standard button for the configure dialog
    The same code is also present in kcmutils/src/kcmultidialog.cpp which we
    are inheriting from and calling it twice cause some issues where some
    buttons appears twice.

--- src/configuredialog/configuredialog.cpp
+++ src/configuredialog/configuredialog.cpp
@@ -21,20 +21,11 @@ ConfigureDialog::ConfigureDialog(QWidget *parent, bool modal)
     : KCMultiDialog(parent)
 {
     setFaceType(List);
-    setStandardButtons(QDialogButtonBox::Ok | QDialogButtonBox::Help | QDialogButtonBox::RestoreDefaults | QDialogButtonBox::Cancel | QDialogButtonBox::Apply
-                       | QDialogButtonBox::Reset);
     setModal(modal);
     const QList<KPluginMetaData> availablePlugins = KPluginMetaData::findPlugins(QStringLiteral("pim6/kcms/kmail"));
     for (const KPluginMetaData &metaData : availablePlugins) {
         addModule(metaData);
     }
-
-    connect(button(QDialogButtonBox::Ok), &QPushButton::clicked, this, &ConfigureDialog::slotOk);
-    connect(button(QDialogButtonBox::Apply), &QPushButton::clicked, this, &ConfigureDialog::slotApply);
-    connect(button(QDialogButtonBox::Help), &QPushButton::clicked, this, &ConfigureDialog::slotHelpClicked);
-    connect(button(QDialogButtonBox::Cancel), &QPushButton::clicked, this, &ConfigureDialog::slotUser1Clicked);
-    connect(button(QDialogButtonBox::RestoreDefaults), &QPushButton::clicked, this, &ConfigureDialog::slotDefaultClicked);
-    connect(button(QDialogButtonBox::Reset), &QPushButton::clicked, this, &ConfigureDialog::slotUser1Clicked);
 }
 
 QSize ConfigureDialog::sizeHint() const
