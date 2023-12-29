e3829bd0dc1f6918876a0121bdaa3662b89d7f2b
Laurent Montel <montel@kde.org>
2023-12-29 07:35:04 +0100

    Add addarchivemailwidget

---
+++ agents/archivemailagent/addarchivemailwidget.cpp
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "addarchivemailwidget.h"
+
+AddArchiveMailWidget::AddArchiveMailWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+AddArchiveMailWidget::~AddArchiveMailWidget() = default;
