0f97df7d292dcbe566e264817c61271f28a8d445
Laurent Montel <montel@kde.org>
2024-09-12 13:49:06 +0200

    Prepare to implement new identitytreengwidget which will support plasma activities

---
+++ src/identity/identitytreengwidget.cpp
@@ -0,0 +1,14 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-only
+*/
+
+#include "identitytreengwidget.h"
+
+IdentityTreeNgWidget::IdentityTreeNgWidget(QWidget *parent)
+    : KIdentityManagementWidgets::IdentityTreeView(parent)
+{
+}
+
+IdentityTreeNgWidget::~IdentityTreeNgWidget() = default;
---
+++ src/identity/identitytreengwidget.h
@@ -0,0 +1,18 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-only
+*/
+
+#pragma once
+
+#include <KIdentityManagementWidgets/IdentityTreeView>
+#include <QWidget>
+
+class IdentityTreeNgWidget : public KIdentityManagementWidgets::IdentityTreeView
+{
+    Q_OBJECT
+public:
+    explicit IdentityTreeNgWidget(QWidget *parent = nullptr);
+    ~IdentityTreeNgWidget() override;
+};
