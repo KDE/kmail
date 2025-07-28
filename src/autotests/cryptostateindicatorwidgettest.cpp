2e7d8f9eae55e5aa4ca9617e5446501475a41bef
Sune Vuorela <sune@vuorela.dk>
2025-07-28 14:40:00 +0200

    DRAFT: Revert "Test KDECI_CANNOT_CREATE_WINDOWS for disable this autotest"
    This reverts commit 0eb5fb6d5c8ab59db7689a43b82435a9aef88a5e.
    
    The new windows VM based CI should be able to handle it, so let's test
    it

--- src/autotests/cryptostateindicatorwidgettest.cpp
+++ src/autotests/cryptostateindicatorwidgettest.cpp
@@ -11,9 +11,6 @@
 CryptoStateIndicatorWidgetTest::CryptoStateIndicatorWidgetTest(QObject *parent)
     : QObject(parent)
 {
-    if (qEnvironmentVariableIntValue("KDECI_CANNOT_CREATE_WINDOWS")) {
-        QSKIP("KDE CI can't create a window on this platform, skipping some gui tests");
-    }
 }
 
 CryptoStateIndicatorWidgetTest::~CryptoStateIndicatorWidgetTest() = default;
