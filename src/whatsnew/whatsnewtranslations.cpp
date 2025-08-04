da81cd2672e7d77dbb8bf7d506b3a6813d5358b7
Laurent Montel <montel@kde.org>
2025-08-04 08:47:48 +0200

    Prepare 6.6.0

--- src/whatsnew/whatsnewtranslations.cpp
+++ src/whatsnew/whatsnewtranslations.cpp
@@ -13,7 +13,7 @@ WhatsNewTranslations::~WhatsNewTranslations() = default;
 // Use by newFeaturesMD5
 QList<KLazyLocalizedString> WhatsNewTranslations::lastNewFeatures() const
 {
-    const QList<KLazyLocalizedString> info{kli18n("Add new whatsnew widget."), kli18n("New plugin: show collection size.")};
+    const QList<KLazyLocalizedString> info{kli18n("Allow to export text in autogenerate editor plugins.")};
     return info;
 }
 
@@ -26,8 +26,18 @@ QList<PimCommon::WhatsNewInfo> WhatsNewTranslations::createWhatsNewInfo() const
         for (const KLazyLocalizedString &l : lastNewFeatures()) {
             lst += l.toString();
         }
-        info.setNewFeatures(lst);
+        info.setNewFeatures({i18n("Add new whatsnew widget."), i18n("New plugin: show collection size.")});
         info.setVersion(QStringLiteral("6.5.0"));
+        listInfo.append(std::move(info));
+    }
+    {
+        PimCommon::WhatsNewInfo info;
+        QStringList lst;
+        for (const KLazyLocalizedString &l : lastNewFeatures()) {
+            lst += l.toString();
+        }
+        info.setNewFeatures(lst);
+        info.setVersion(QStringLiteral("6.6.0"));
         listInfo.append(std::move(info));
     }
     return listInfo;
