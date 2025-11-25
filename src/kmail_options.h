a2bc1e55bbea9f71fd3c7506a2af8397e90f095e
Laurent Montel <montel@kde.org>
2025-11-25 13:37:19 +0100

    Allow to translate command line option

--- src/kmail_options.h
+++ src/kmail_options.h
@@ -14,28 +14,26 @@ static void kmail_options(QCommandLineParser *parser)
 
     options << QCommandLineOption(QStringList() << QStringLiteral("s") << QStringLiteral("subject"),
                                   i18nc("@info:shell", "Set subject of message"),
-                                  QStringLiteral("subject"))
+                                  i18n("Subject"))
             << QCommandLineOption(QStringList() << QStringLiteral("c") << QStringLiteral("cc"),
                                   i18n("Send CC: to 'address'. This can be repeated"),
-                                  QStringLiteral("address"))
+                                  i18n("Address"))
             << QCommandLineOption(QStringList() << QStringLiteral("b") << QStringLiteral("bcc"),
                                   i18n("Send BCC: to 'address'. This can be repeated"),
-                                  QStringLiteral("address"))
-            << QCommandLineOption(QStringList() << QStringLiteral("r") << QStringLiteral("replyTo"),
-                                  i18n("Set replyTo to 'address'"),
-                                  QStringLiteral("address"))
+                                  i18n("Address"))
+            << QCommandLineOption(QStringList() << QStringLiteral("r") << QStringLiteral("replyTo"), i18n("Set replyTo to 'address'"), i18n("Address"))
             << QCommandLineOption(QStringLiteral("header"),
                                   i18nc("@info:shell", "Add 'header' to message. This can be repeated"),
                                   QStringLiteral("header_name:header_value"))
-            << QCommandLineOption(QStringLiteral("msg"), i18nc("@info:shell", "Read message body from 'file'"), QStringLiteral("file"))
-            << QCommandLineOption(QStringLiteral("body"), i18nc("@info:shell", "Set body of message"), QStringLiteral("text"))
-            << QCommandLineOption(QStringLiteral("attach"), i18nc("@info:shell", "Add an attachment to the mail. This can be repeated"), QStringLiteral("url"))
+            << QCommandLineOption(QStringLiteral("msg"), i18nc("@info:shell", "Read message body from 'file'"), i18n("File"))
+            << QCommandLineOption(QStringLiteral("body"), i18nc("@info:shell", "Set body of message"), i18n("Text"))
+            << QCommandLineOption(QStringLiteral("attach"), i18nc("@info:shell", "Add an attachment to the mail. This can be repeated"), i18n("Attachment Url"))
             << QCommandLineOption(QStringLiteral("check"), i18nc("@info:shell", "Only check for new mail"))
             << QCommandLineOption(QStringLiteral("startintray"), i18nc("@info:shell", "Start minimized to tray"))
             << QCommandLineOption(QStringLiteral("composer"), i18nc("@info:shell", "Only open composer window"))
-            << QCommandLineOption(QStringLiteral("identity"), i18nc("@info:shell", "Set identity name"), QStringLiteral("identity"))
-            << QCommandLineOption(QStringLiteral("view"), i18nc("@info:shell", "View the given message file"), QStringLiteral("url"))
-            << QCommandLineOption(QStringLiteral("html"), i18nc("@info:shell", "Set body of message as html"), QStringLiteral("text"));
+            << QCommandLineOption(QStringLiteral("identity"), i18nc("@info:shell", "Set identity name"), i18n("Identity"))
+            << QCommandLineOption(QStringLiteral("view"), i18nc("@info:shell", "View the given message file"), i18n("Message Url"))
+            << QCommandLineOption(QStringLiteral("html"), i18nc("@info:shell", "Set body of message as html"), i18n("Body Message"));
 #if KMAIL_WITH_KUSERFEEDBACK
     parser->addOption(QCommandLineOption(QStringLiteral("feedback"), i18nc("@info:shell", "Lists the available options for user feedback")));
 #endif
