20559855fe4c717c583dddc8572b7457774b4953
Allen Winter <winter@kde.org>
2026-02-02 08:51:43 -0500

    Start adding WhatsNew for 6.7.0
    Mention changing the HTML color default option change

--- src/whatsnew/whatsnewtranslations.cpp
+++ src/whatsnew/whatsnewtranslations.cpp
@@ -13,7 +13,9 @@ WhatsNewTranslations::~WhatsNewTranslations() = default;
 // Use by newFeaturesMD5
 QList<KLazyLocalizedString> WhatsNewTranslations::lastNewFeatures() const
 {
-    const QList<KLazyLocalizedString> info{kli18n("Allow to export text in autogenerate editor plugins.")};
+    const QList<KLazyLocalizedString> info{
+        kli18n("HTML messages are shown with their intended colors by default. To override this, uncheck the \"Appearance->Colors->Do not change color from "
+               "HTML mail\" option")};
     return info;
 }
 
@@ -32,9 +34,20 @@ QList<TextAddonsWidgets::WhatsNewInfo> WhatsNewTranslations::createWhatsNewInfo(
         for (const KLazyLocalizedString &l : lastNewFeatures()) {
             lst += l.toString();
         }
-        info.setNewFeatures(lst);
+        info.setNewFeatures({i18n("Allow to export text in autogenerate editor plugins.")});
         info.setBugFixings({i18n("Fix text to speech support (enqueue messages)")});
         info.setVersion(QStringLiteral("6.6.0"));
+        listInfo.append(std::move(info));
+    }
+    {
+        TextAddonsWidgets::WhatsNewInfo info;
+        QStringList lst;
+        for (const KLazyLocalizedString &l : lastNewFeatures()) {
+            lst += l.toString();
+        }
+        info.setNewFeatures(lst);
+        info.setBugFixings({});
+        info.setVersion(QStringLiteral("6.7.0"));
         listInfo.append(std::move(info));
     }
     return listInfo;
